a17ac8d878446353eae71b9a04783b9911b84ec5
suocheng <suocheng@ecquaria.com>
2019-05-17 11:50:42 +0800

    add to git

---
+++ demo-app/src/main/webapp/deployed/IAIS/suochengTest/1/src/sop/webflow/rt/java/code/IAIS___suochengTest___1.java
@@ -0,0 +1,8 @@
+package sop.webflow.rt.java.code;
+
+import sop.webflow.rt.api.BaseProcessClass;
+import com.ecquaria.egp.core.forms.util.FormRuntimeUtil;
+import com.ecquaria.egp.core.forms.util.entity.FormButton;
+
+public class IAIS___suochengTest___1 extends BaseProcessClass {
+}
