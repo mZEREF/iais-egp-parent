019b0e558f89c75d4aaebf4ceeb30320f91713a1
jinhua <jinhua@ecquaria.com>
2019-06-06 16:17:07 +0800

    For Unit test

---
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/validation/ValidationResult.java
@@ -0,0 +1,20 @@
+package com.ecquaria.cloud.moh.iais.validation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationResult {
+    @Getter @Setter private boolean hasErrors;
+    private Map<String, String> errorMsg = new HashMap<String, String>();
+
+    public void addMessage(String key, String msg) {
+        errorMsg.put(key, msg);
+    }
+
+    public Map<String, String> retrieveAll() {
+        return errorMsg;
+    }
+}
