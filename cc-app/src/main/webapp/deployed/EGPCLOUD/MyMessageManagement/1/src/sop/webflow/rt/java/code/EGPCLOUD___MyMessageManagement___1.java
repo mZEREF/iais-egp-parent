98ef1799537098f857f1ff2898ee451409bb0f7a
chong_hua <chong_hua@ecquaria.com>
2019-07-03 13:59:16 +0800

    Resolve code specification issues

--- cc-app/src/main/webapp/deployed/EGPCLOUD/MyMessageManagement/1/src/sop/webflow/rt/java/code/EGPCLOUD___MyMessageManagement___1.java
+++ cc-app/src/main/webapp/deployed/EGPCLOUD/MyMessageManagement/1/src/sop/webflow/rt/java/code/EGPCLOUD___MyMessageManagement___1.java
@@ -13,6 +13,7 @@ package sop.webflow.rt.java.code;
 import sop.webflow.rt.api.BaseProcessClass;
 
 import com.ecquaria.cloud.helper.EngineHelper;
+import com.ecquaria.cloud.moh.iais.exception.IaisRuntimeException;
 
 public class EGPCLOUD___MyMessageManagement___1 extends BaseProcessClass {
 
@@ -25,27 +26,33 @@ public class EGPCLOUD___MyMessageManagement___1 extends BaseProcessClass {
 	private static final String MAIN_SEARCH = "mainSearch";
 
 
-	public void prepareData_OnStepProcess_0() throws Exception {
+	public void prepareData_OnStepProcess_0() {
+		throw new IaisRuntimeException(PREPARE_DATA);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, PREPARE_DATA, request);
 	}
 
-	public void search_OnStepProcess_0() throws Exception {
+	public void search_OnStepProcess_0()  {
+		throw new IaisRuntimeException(SEARCH);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, SEARCH, request);
 	}
 
-	public void step1_OnStepProcess_0() throws Exception {
+	public void step1_OnStepProcess_0() {
+		throw new IaisRuntimeException(START_PROCESS);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, START_PROCESS, request);
 	}
 
-	public void paging_OnStepProcess_0() throws Exception {
+	public void paging_OnStepProcess_0()  {
+		throw new IaisRuntimeException(CHANGE_PAGE);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, CHANGE_PAGE, request);
 	}
 
-	public void sorting_OnStepProcess_0() throws Exception {
+	public void sorting_OnStepProcess_0() {
+		throw new IaisRuntimeException(SORTING);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, SORTING, request);
 	}
 
-	public void mainSearch_OnStepProcess_0() throws Exception {
+	public void mainSearch_OnStepProcess_0() {
+		throw new IaisRuntimeException(MAIN_SEARCH);
 		EngineHelper.delegate(MY_MESSAGE_DELEGATOR, MAIN_SEARCH, request);
 	}
 	
