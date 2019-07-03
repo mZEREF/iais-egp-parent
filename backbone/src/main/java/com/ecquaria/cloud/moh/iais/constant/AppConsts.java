6fcf674f2375eb729edbc2e0536b0b9afb1c7f54
jinhua <jinhua@ecquaria.com>
2019-07-03 16:23:16 +0800

    For Unit test

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/constant/AppConsts.java
+++
@@ -1,8 +0,0 @@
-package com.ecquaria.cloud.moh.iais.constant;
-
-public final class AppConsts {
-    public static final String USER_DOMAIN_INTRANET              = "intranet";
-    public static final String USER_DOMAIN_INTERNET              = "internet";
-
-    private AppConsts() {}
-}
---
+++ backbone/src/test/java/com/ecquaria/cloud/moh/iais/aop/AuditFunctionAspectTest.java
@@ -0,0 +1,47 @@
+package com.ecquaria.cloud.moh.iais.aop;
+
+import com.ecquaria.cloud.moh.iais.constant.AppConsts;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.powermock.api.mockito.mockpolicies.Slf4jMockPolicy;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.client.RestTemplate;
+import sop.iwe.SessionManager;
+import sop.rbac.user.User;
+
+@RunWith(PowerMockRunner.class)
+@MockPolicy(Slf4jMockPolicy.class)
+@PrepareForTest({AuditFunctionAspect.class})
+public class AuditFunctionAspectTest {
+    @InjectMocks
+    private AuditFunctionAspect aspect;
+    @Mock
+    private RestTemplate restTemplate;
+    @Spy
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+
+
+    @Before
+    public void setup() {
+        aspect.setRequest(request);
+        aspect.setRestTemplate(restTemplate);
+        User user = new User();
+        user.setId("Test User");
+        user.setUserDomain(AppConsts.USER_DOMAIN_INTRANET);
+        request.getSession().setAttribute(SessionManager.SOP_LOGIN_INFO, user);
+        request.addHeader("User-Agent", "firefox");
+
+    }
+
+    @Test
+    public void testAspect() {
+
+    }
+}
