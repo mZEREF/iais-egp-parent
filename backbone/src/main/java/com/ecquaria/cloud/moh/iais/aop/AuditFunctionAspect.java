245ef69a17f21b9e9a9730fbbd581d0e542a85ca
Jinhua <jinhua@ecquaria.com>
2019-07-18 15:23:48 +0800

    Add oval validation framework

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/aop/AuditFunctionAspect.java
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/aop/AuditFunctionAspect.java
@@ -12,9 +12,10 @@
  */
 package com.ecquaria.cloud.moh.iais.aop;
 
-import com.ecquaria.cloud.moh.iais.annotation.FunctionTrack;
 import com.ecquaria.cloud.moh.iais.annotation.SearchTrack;
 import com.ecquaria.cloud.moh.iais.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -27,15 +28,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import sg.gov.moh.iais.common.utils.MiscUtil;
+import sg.gov.moh.iais.common.annotation.LogInfo;
 import sg.gov.moh.iais.web.logging.dto.AuditTrailDto;
-import sop.iwe.SessionManager;
-import sop.rbac.user.User;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.lang.reflect.Method;
 import java.util.*;
 
@@ -44,61 +39,74 @@ import java.util.*;
 public class AuditFunctionAspect {
 
     private RestTemplate restTemplate = new RestTemplate();
-    private HttpServletRequest request;
 
-    @Pointcut("@within(com.ecquaria.cloud.moh.iais.annotation.FunctionTrack)")
+    @Pointcut("@annotation(sg.gov.moh.iais.common.annotation.LogInfo)")
     public void auditFunction() {
+        throw new UnsupportedOperationException();
+    }
+    @Pointcut("@within(sg.gov.moh.iais.common.annotation.LogInfo)")
+    public void auditClass() {
         throw new UnsupportedOperationException();
     }
 
     @Around("auditFunction()")
     public Object auditAroundFunction(ProceedingJoinPoint point) throws Throwable {
         AuditTrailDto dto = new AuditTrailDto();
-        request = ((ServletRequestAttributes)
-                RequestContextHolder.getRequestAttributes()).getRequest();
-        User user = SessionManager.getInstance(request).getCurrentUser();
-        HttpSession session = request.getSession();
-        if (user != null) {
-            dto.setNricNumber(user.getId());
-            dto.setMohUserId(user.getId());
-            dto.setUserDomain(SessionManager.getInstance(request).getCurrentUserDomain());
-        }
-        dto.setSessionId(session.getId());
-        dto.setClientIp(MiscUtil.getClientIp(request));
-        dto.setUserAgent(request.getHeader("User-Agent"));
+        IaisEGPHelper.setAuditLoginUserInfo(dto);
+        return auditFunction(point, dto);
+    }
+
+    @Around("auditClass()")
+    public Object auditAroundClass(ProceedingJoinPoint point) throws Throwable {
+        AuditTrailDto dto = new AuditTrailDto();
+        IaisEGPHelper.setAuditLoginUserInfo(dto);
         Class clazz = point.getSignature().getDeclaringType();
-        if (clazz.isAnnotationPresent(FunctionTrack.class)) {
-            FunctionTrack logInfo = (FunctionTrack) clazz.getAnnotation(FunctionTrack.class);
+        if (clazz.isAnnotationPresent(LogInfo.class)) {
+            LogInfo logInfo = (LogInfo) clazz.getAnnotation(LogInfo.class);
             dto.setOperation(logInfo.auditType());
             dto.setFunctionName(logInfo.funcName());
             dto.setModule(logInfo.moduleName());
         }
+
+        return auditFunction(point, dto);
+    }
+
+    private Object auditFunction(ProceedingJoinPoint point, AuditTrailDto dto) throws Throwable {
         Method method = ((MethodSignature) point.getSignature()).getMethod();
-        if (method.isAnnotationPresent(FunctionTrack.class)) {
-            FunctionTrack logInfo = method.getAnnotation(FunctionTrack.class);
+        if (method.isAnnotationPresent(LogInfo.class)) {
+            LogInfo logInfo = method.getAnnotation(LogInfo.class);
             dto.setOperation(logInfo.auditType());
             dto.setFunctionName(logInfo.funcName());
             dto.setModule(logInfo.moduleName());
         }
         if (method.isAnnotationPresent(SearchTrack.class)) {
             Object[] args = point.getArgs();
-            if (args != null && args.length > 0) {
-                for (Object obj : args) {
-                    if (obj instanceof SearchParam) {
-                        SearchParam param = (SearchParam) obj;
-                        Map<String, Object> filters = param.getFilters();
-                        ObjectMapper mapper = new ObjectMapper();
-                        Map<String, Object> json = new HashMap<>();
-                        for (Map.Entry<String, Object> ent : filters.entrySet()) {
-                            json.put(ent.getKey(), ent.getValue());
-                        }
-                        dto.setViewParams(mapper.writeValueAsString(json));
-                        break;
+            keepSearchParam(args, dto);
+        }
+        callRestApi(dto);
+
+        return point.proceed();
+    }
+
+    private void keepSearchParam(Object[] args, AuditTrailDto dto) throws JsonProcessingException {
+        if (args != null && args.length > 0) {
+            for (Object obj : args) {
+                if (obj instanceof SearchParam) {
+                    SearchParam param = (SearchParam) obj;
+                    Map<String, Object> filters = param.getFilters();
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, Object> json = new HashMap<>();
+                    for (Map.Entry<String, Object> ent : filters.entrySet()) {
+                        json.put(ent.getKey(), ent.getValue());
                     }
+                    dto.setViewParams(mapper.writeValueAsString(json));
+                    break;
                 }
             }
         }
+    }
 
+    private void callRestApi(AuditTrailDto dto) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         List<AuditTrailDto> dtoList = new ArrayList<>();
@@ -106,8 +114,5 @@ public class AuditFunctionAspect {
         HttpEntity<Collection<AuditTrailDto>> jsonPart = new HttpEntity<>(dtoList, headers);
         restTemplate.exchange("http://localhost:8887/api/audittrail/cudTrail",
                 HttpMethod.POST, jsonPart, String.class);
-
-        return point.proceed();
     }
-
 }
