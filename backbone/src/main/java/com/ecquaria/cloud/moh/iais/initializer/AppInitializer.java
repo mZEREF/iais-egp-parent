17ce95712911a29f9249893e7c4aeebfa8e7d50b
Jinhua <jinhua@ecquaria.com>
2019-07-25 09:49:01 +0800

    Coding for query

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/initializer/AppInitializer.java
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/initializer/AppInitializer.java
@@ -13,12 +13,20 @@
 
 package com.ecquaria.cloud.moh.iais.initializer;
 
+import com.ecquaria.cloud.moh.iais.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.entity.MessageCode;
+import com.ecquaria.cloud.moh.iais.querydao.QueryDao;
 import com.ecquaria.cloud.moh.iais.sql.SqlMapLoader;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import sg.gov.moh.iais.common.utils.MessageUtil;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * AppInitializer
@@ -29,6 +37,9 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 @Slf4j
 public class AppInitializer implements ServletContextListener {
+    @Autowired
+    private QueryDao dao;
+
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         // Nothing to do
@@ -40,9 +51,22 @@ public class AppInitializer implements ServletContextListener {
         try {
             SqlMapLoader sqlMapLoader = new SqlMapLoader();
             sqlMapLoader.loadSqlMap();
+            initMessages();
         } catch (Exception ex) {
             log.error("Failed to initialize the application.", ex);
         }
         log.info("---------- Initialization done. ----------");
+    }
+
+    private void initMessages() {
+        SearchParam param = new SearchParam(MessageCode.class);
+        SearchResult<MessageCode> sr = dao.doQuery(param, "message", "retrieveAllMsg");
+        if (sr.getRowCount() > 0) {
+            Map<String, String> map = new HashMap<>();
+            for (MessageCode mc : sr.getRows()) {
+                map.put(mc.getCodeKey(), mc.getDescription());
+            }
+            MessageUtil.loadMessages(map);
+        }
     }
 }
