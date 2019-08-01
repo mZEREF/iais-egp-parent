b9852005384a1f5365cfbbcc171358e90b861126
suocheng <suocheng@ecquaria.com>
2019-08-01 10:34:17 +0800

    Unit test

---
+++ web-common/src/test/java/com/ecquaria/cloud/moh/iais/dto/MasterCodeDtoTest.java
@@ -0,0 +1,44 @@
+package com.ecquaria.cloud.moh.iais.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MasterCodeDto.class})
+public class MasterCodeDtoTest {
+
+    @Spy
+    MasterCodeDto entity = new MasterCodeDto();
+
+    @Test
+    public void testGetterSetter() {
+        entity.getMasterCodeId();
+        entity.setMasterCodeId(1);
+        entity.getCode();
+        entity.setCode(null);
+        entity.getCodeValue();
+        entity.setCodeValue(null);
+        entity.getCategory();
+        entity.setCategory(1);
+        entity.getDescription();
+        entity.setDescription(null);
+        entity.getFilterValue();
+        entity.setFilterValue(null);
+        entity.getSequence();
+        entity.setSequence(1);
+        entity.getStatus();
+        entity.setStatus(1);
+        entity.getVersion();
+        entity.setVersion(1);
+        entity.getEffectFrom();
+        entity.setEffectFrom(null);
+        entity.getEffectTo();
+        entity.setEffectTo(null);
+        assertNotNull(entity);
+    }
+}
