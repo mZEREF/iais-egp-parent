de57d2df0d7e78b949ef17379854594f08f71144
yichen <yichen_guo@ecquaria.com>
2019-11-01 15:09:44 +0800

    re add validate

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/base/BaseItem.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/base/BaseItem.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 public class BaseItem {
     private Integer id;
     private String rowguid;
-    private Integer configId;
+    private Integer dsads;
     private String module;
     private String type;
     private String sectionId;
