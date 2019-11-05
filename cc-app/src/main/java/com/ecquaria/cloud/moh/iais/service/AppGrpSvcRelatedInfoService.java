228b4a88e62a55c9f0ef57791cb399535c448d23
zixian <734735993@qq.com>
2019-11-05 10:30:11 +0800

    application new

---
+++ cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/AppGrpSvcRelatedInfoService.java
@@ -0,0 +1,23 @@
+package com.ecquaria.cloud.moh.iais.service;
+
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.HcsaSvcPersonnelDto;
+import com.ecquaria.cloud.moh.iais.dto.GovernanceOfficersDto;
+import com.ecquaria.cloud.moh.iais.dto.TestDto;
+
+import java.util.List;
+import java.util.Map;
+
+/****
+ *
+ *   @date 10/29/2019
+ *   @author zixian
+ */
+public interface AppGrpSvcRelatedInfoService {
+    List<HcsaSvcPersonnelDto> loadCGOBySvcIdAndPsnType(String serviceId, String psnType);
+    GovernanceOfficersDto loadGovernanceOfficerByCGOId(String cgoId);
+    List<TestDto> loadLaboratoryDisciplines(String str);
+    Map loadCGOByDisciplines(List disciplines);
+    List loadPO();
+    List saveLaboratoryDisciplines(List checkLists);
+
+}
