abff03a0f58380aa809f3fc23f26f0bee4dd876e
chong_hua <chong_hua@ecquaria.com>
2019-11-08 10:46:00 +0800

    Egp Inbox Module

--- cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxTabSericeImpl.java
+++ cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxTabSericeImpl.java
@@ -1,6 +1,8 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
-import com.ecquaria.cloud.moh.iais.common.dto.application.ApplicationDto;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.utils.RestApiUtil;
 import com.ecquaria.cloud.moh.iais.service.InboxTabService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +21,30 @@ public class InboxTabSericeImpl implements InboxTabService {
     private static final String URL="iais-application:8881/iais-application";
     @Override
     public List<ApplicationDto> selectAll() {
-        log.info("---InboxTabSericeImpl---");
+        log.info("The method is selectAll");
         ApplicationDto applicationDto = new ApplicationDto();
-        List<ApplicationDto> applicationDtos = RestApiUtil.getByList(URL,applicationDto,List.class);
-        return applicationDtos;
+        return RestApiUtil.getByList(URL,applicationDto,List.class);
     }
+
+    @Override
+    public List<ApplicationDto> searchByAppType(String type) {
+        log.info("The method is searchByAppType");
+        ApplicationDto applicationDto = new ApplicationDto();
+        return RestApiUtil.getByList(URL,applicationDto,List.class);
+    }
+
+    @Override
+    public SearchResult<ApplicationDto> doQuery(SearchParam param) {
+        return RestApiUtil.query(URL+"/param",param);
+    }
+
+    @Override
+    public List<ApplicationDto> searchByAppStatus(String status) {
+        log.info("The method is searchByAppType");
+        ApplicationDto applicationDto = new ApplicationDto();
+        return RestApiUtil.getByList(URL,applicationDto,List.class);
+    }
+
 
 }
 
