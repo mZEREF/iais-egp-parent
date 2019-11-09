5c8778a5963f45ef87ebbedbd01b25ee0d6ab0b1
Jinhua <jinhua@ecquaria.com>
2019-11-09 17:55:30 +0800

    Add Event Bus

--- cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmisionServiceImpl.java
+++ cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmisionServiceImpl.java
@@ -3,10 +3,8 @@ package com.ecquaria.cloud.moh.iais.service.impl;
 import com.ecquaria.cloud.moh.iais.common.dto.eventbus.SubmitReq;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppSubmissionDto;
 import com.ecquaria.cloud.moh.iais.common.utils.JsonUtil;
-import com.ecquaria.cloud.moh.iais.feign.EventBusClient;
+import com.ecquaria.cloud.moh.iais.common.utils.RestApiUtil;
 import com.ecquaria.cloud.moh.iais.service.AppSubmisionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,21 +15,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AppSubmisionServiceImpl implements AppSubmisionService {
-    @Autowired
-    private EventBusClient eventBusClient;
-
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
 
     @Override
     public void submit(AppSubmissionDto appSubmissionDto) {
         int timeoutSec = 300;
+        appSubmissionDto = new AppSubmissionDto();
+        appSubmissionDto.setEventRefNo("adreccssss");
         SubmitReq sr = new SubmitReq();
         sr.setProject("iais");
         sr.setService("appsubmit");
         sr.setData(JsonUtil.parseToJson(appSubmissionDto));
         sr.setWait(true);
         sr.setTotalWait(timeoutSec);
-        eventBusClient.submit(sr);
+        sr.setCallbackUrl("https://egp.sit.inter.iais.com/hcsaapplication/eservice/INTERNET/MohNewApplication");
+        sr.setUserId("internet");
+        sr.setProcess("MohNewApplication");
+        sr.setStep("1");
+        sr.setOperation("Application Submission");
+        RestApiUtil.callEventBusSubmit(sr);
     }
 }
