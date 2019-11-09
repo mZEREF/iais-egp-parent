048a7e97e42f0d771a13bd7b561ac478e18a68ad
wenkang <wenkang@ecquaria.com>
2019-11-09 14:04:13 +0800

    Merge branch 'egp_develop' of D:\iais-egp with conflicts.

--- cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmisionServiceImpl.java
+++ cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmisionServiceImpl.java
@@ -2,11 +2,10 @@ package com.ecquaria.cloud.moh.iais.service.impl;
 
 import com.ecquaria.cloud.moh.iais.common.dto.eventbus.SubmitReq;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppSubmissionDto;
+/*import com.ecquaria.cloud.moh.iais.common.feign.EventBusClient;*/
 import com.ecquaria.cloud.moh.iais.common.utils.JsonUtil;
-import com.ecquaria.cloud.moh.iais.feign.EventBusClient;
 import com.ecquaria.cloud.moh.iais.service.AppSubmisionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,21 +16,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AppSubmisionServiceImpl implements AppSubmisionService {
-    @Autowired
+    /*@Autowired
     private EventBusClient eventBusClient;
-
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
-
+*/
     @Override
     public void submit(AppSubmissionDto appSubmissionDto) {
-        int timeoutSec = 300;
+       /* int timeoutSec = 300;
         SubmitReq sr = new SubmitReq();
         sr.setProject("iais");
         sr.setService("appsubmit");
         sr.setData(JsonUtil.parseToJson(appSubmissionDto));
         sr.setWait(true);
         sr.setTotalWait(timeoutSec);
-        eventBusClient.submit(sr);
+        eventBusClient.submit(sr);*/
     }
 }
