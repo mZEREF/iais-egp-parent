6b532ef13bcfd23e113425f071d6c87389157fe1
ShiCheng_Xu <shicheng@ecquaria.com>
2019-11-15 14:45:57 +0800

    inspection

---
+++ hcsa-inspection/src/main/java/com/ecquaria/cloud/moh/iais/action/InspectionDelegtor.java
@@ -0,0 +1,14 @@
+package com.ecquaria.cloud.moh.iais.action;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Shicheng
+ * @date 2019/11/14 18:01
+ **/
+@Delegator("inspectionDelegator")
+@Slf4j
+public class InspectionDelegtor {
+
+}
