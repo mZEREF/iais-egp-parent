688d5e36a0941c31beaa303cd288369854090112
suocheng <suocheng@ecquaria.com>
2019-11-19 15:34:35 +0800

    RestApi

--- cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/AppSubmissionService.java
+++ cc-app/src/main/java/com/ecquaria/cloud/moh/iais/service/AppSubmissionService.java
@@ -1,6 +1,7 @@
 package com.ecquaria.cloud.moh.iais.service;
 
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppSubmissionDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.risksm.PreOrPostInspectionResultDto;
 
 /**
  * AppSubmissionService
@@ -12,5 +13,11 @@ public interface AppSubmissionService {
     public AppSubmissionDto submit(AppSubmissionDto appSubmissionDto);
 
     public AppSubmissionDto doSaveDraft(AppSubmissionDto appSubmissionDto);
+
+    public String getDraftNo(String appType);
+    public String getGroupNo(String appType);
+    public  Double getGroupAmount(AppSubmissionDto appSubmissionDto);
+    public PreOrPostInspectionResultDto judgeIsPreInspection(AppSubmissionDto appSubmissionDto);
+
 
 }
