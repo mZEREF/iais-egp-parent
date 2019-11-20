f4b14a5cc79a47bb3a93a4b05448f8b807cab93f
chong_hua <chong_hua@ecquaria.com>
2019-11-20 17:14:49 +0800

    Inter Inbox Mould

--- iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/constant/InboxConst.java
+++ iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/constant/InboxConst.java
@@ -9,16 +9,20 @@ public class InboxConst {
 
 
     //URL Info
+
     public static final String INBOX_URL            = "inter-inbox:8877/inter-inbox";
     public static final String APP_URL              = "hcsa-application:8883/iais-application";
-
     //SEARCH PARAM
+
     public static final String INBOX_RESULT         = "inboxResult";
     public static final String INBOX_PARAM          = "inboxParam";
-    public static final String APP_RESULT           = "AppResult";
-    public static final String APP_PARAM            = "AppParam";
+    public static final String APP_RESULT           = "appResult";
+    public static final String APP_PARAM            = "appParam";
 
-    public static final String ACTION_TYPE          = "crud_action_type";
+    public static final String SWITCH_ACTION        = "switch_action";
+
+    public static final String SEARCH_INBOX         = "seinbox";
+
 
 
 }
--- iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxServiceImpl.java
+++ iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxServiceImpl.java
@@ -2,15 +2,15 @@ package com.ecquaria.cloud.moh.iais.service.impl;
 
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
-import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxAppQueryDto;
+import com.ecquaria.cloud.moh.iais.common.dto.application.AppInboxQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxQueryDto;
-import com.ecquaria.cloud.moh.iais.common.dto.inbox.InterInboxDto;
 import com.ecquaria.cloud.moh.iais.common.utils.RestApiUtil;
 import com.ecquaria.cloud.moh.iais.constant.InboxConst;
 import com.ecquaria.cloud.moh.iais.service.InboxService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,13 +18,9 @@ import java.util.List;
 public class InboxServiceImpl implements InboxService {
 
     @Override
-    public List<InterInboxDto> selectAllInbox() {
-        return RestApiUtil.getList(InboxConst.INBOX_URL,List.class);
-    }
-
-    @Override
-    public SearchResult<InboxAppQueryDto> applicationDoQuery(SearchParam searchParam) {
-        return RestApiUtil.query(InboxConst.APP_URL+"/app-param",searchParam);
+    public List<AppInboxQueryDto> applicationDoQuery() {
+        List<AppInboxQueryDto> appInboxQueryDtoList = new ArrayList<>();
+        return RestApiUtil.postGetList(InboxConst.INBOX_URL+"/app-param",appInboxQueryDtoList,AppInboxQueryDto.class);
     }
 
     @Override
