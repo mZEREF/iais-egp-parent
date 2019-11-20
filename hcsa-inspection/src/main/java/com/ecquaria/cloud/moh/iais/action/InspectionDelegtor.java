bb1f3afcaa3ce7c391a85fc9dd11ac263758bf5b
ShiCheng_Xu <shicheng@ecquaria.com>
2019-11-20 10:56:06 +0800

    inspection

--- hcsa-inspection/src/main/java/com/ecquaria/cloud/moh/iais/action/InspectionDelegtor.java
+++ hcsa-inspection/src/main/java/com/ecquaria/cloud/moh/iais/action/InspectionDelegtor.java
@@ -1,9 +1,15 @@
 package com.ecquaria.cloud.moh.iais.action;
 
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import com.ecquaria.cloud.moh.iais.service.InspectionServie;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import sop.webflow.rt.api.BaseProcessClass;
 
 /**
+ * Process: MohInspectionAllotTaskInspector
+ *
  * @author Shicheng
  * @date 2019/11/14 18:01
  **/
@@ -11,4 +17,41 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class InspectionDelegtor {
 
+    @Autowired
+    private InspectionServie inspectionServie;
+
+    @Autowired
+    private InspectionDelegtor(InspectionServie inspectionServie){
+        this.inspectionServie = inspectionServie;
+    }
+
+    /**
+     * StartStep: InspectionPreInspectorStart
+     *
+     * @param bpc
+     * @throws
+     */
+    public void startStep(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the startStep start ...."));
+    }
+
+    /**
+     * StartStep: InspectionPreInspectorInit
+     *
+     * @param bpc
+     * @throws
+     */
+    public void initStep(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the initStep start ...."));
+    }
+
+    /**
+     * StartStep: InspectionPreInspectorInit
+     *
+     * @param bpc
+     * @throws
+     */
+    public void perpareStep(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the initStep start ...."));
+    }
 }
