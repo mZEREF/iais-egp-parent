a3d064aa8a23ad8e743d3c346d054edaabc8e07c
Jinhua <jinhua@ecquaria.com>
2019-11-21 21:24:27 +0800

    Change ids

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/action/AuditTrailDelegator.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/action/AuditTrailDelegator.java
@@ -7,7 +7,6 @@ package com.ecquaria.cloud.moh.iais.action;
  */
 
 import com.ecquaria.cloud.annotation.Delegator;
-import com.ecquaria.cloud.moh.iais.common.constant.AuditTrailConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.audit.AuditTrailConstants;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
@@ -27,14 +26,13 @@ import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
 import com.ecquaria.cloud.moh.iais.helper.QueryHelp;
 import com.ecquaria.cloud.moh.iais.helper.WebValidationHelper;
 import com.ecquaria.cloud.moh.iais.service.AuditTrailService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import sop.webflow.rt.api.BaseProcessClass;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import sop.webflow.rt.api.BaseProcessClass;
 
 @Delegator(value = "auditTrailDelegator")
 @Slf4j
@@ -127,26 +125,26 @@ public class AuditTrailDelegator {
         ParamUtil.setRequestAttr(request, "operationTypeSelect", operationTypeList);
 
         List<SelectOption> operationList =  new ArrayList<>();
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN), "Internet Login"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGOUT), "Internet Logout"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN_FAIL), "Internet Login Failure"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_VALIDATION_FAIL), "Data Validation Failure – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN), "Intranet Login"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGOUT), "Intranet Logout"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN_FAIL), "Intranet Login Failure"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_ALIDATION_FAIL), "Data Validation Failure – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INACTIVE_BATCHJOB), "Inactive Record – System Batch Job"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTERNET), "Unauthorised Attempts to Access Sources – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTRANET), "Unauthorised Attempts to Access Sources – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTERNET), "User account and password changes – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTRANET), "User account and password changes – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_USER_ACTIVITIES_INTERNET), "User Administrator Activities (Internet System Admin)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_SYSTEM_ACTIVITIES_INTRANET), "System Administrator Activities (Intranet System Admin)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_SUSPENDED), "User accounts suspended"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_LOCK), "User accounts Locked"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_INACTIVATED), "User accounts Inactivated"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTERNET), "Active Internet user accounts with its access rights"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTRANET), "Active Intranet user accounts with its access rights"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN), "Internet Login"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGOUT), "Internet Logout"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN_FAIL), "Internet Login Failure"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_VALIDATION_FAIL), "Data Validation Failure – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN), "Intranet Login"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGOUT), "Intranet Logout"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN_FAIL), "Intranet Login Failure"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_ALIDATION_FAIL), "Data Validation Failure – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INACTIVE_BATCHJOB), "Inactive Record – System Batch Job"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTERNET), "Unauthorised Attempts to Access Sources – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTRANET), "Unauthorised Attempts to Access Sources – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTERNET), "User account and password changes – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTRANET), "User account and password changes – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_USER_ACTIVITIES_INTERNET), "User Administrator Activities (Internet System Admin)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_SYSTEM_ACTIVITIES_INTRANET), "System Administrator Activities (Intranet System Admin)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_SUSPENDED), "User accounts suspended"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_LOCK), "User accounts Locked"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_INACTIVATED), "User accounts Inactivated"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTERNET), "Active Internet user accounts with its access rights"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTRANET), "Active Intranet user accounts with its access rights"));
 
         ParamUtil.setRequestAttr(request, "operationValueTypeSelect", operationList);
 
@@ -168,33 +166,33 @@ public class AuditTrailDelegator {
         ParamUtil.setRequestAttr(request, "operationTypeSelect", operationTypeList);
 
         List<SelectOption> operationList =  new ArrayList<>();
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN), "Internet Login"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGOUT), "Internet Logout"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN_FAIL), "Internet Login Failure"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_VALIDATION_FAIL), "Data Validation Failure – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN), "Intranet Login"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGOUT), "Intranet Logout"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN_FAIL), "Intranet Login Failure"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_ALIDATION_FAIL), "Data Validation Failure – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_VIEW_ALLUSER), "View Record (for all user)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INSERT_ALLUSER), "Insert Record (for all user)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UPDATE_ALLUSER), "Update Record (for all user)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_DELETE_ALLUSER), "Delete Record (for all user)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INSERT_BATCHJOB), "Insert Record – System Batch Job"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UPDATE_BATCHJOB), "Update Record – System Batch Job"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_DELETE_BATCHJOB), "Delete Record – System Batch Job"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INACTIVE_BATCHJOB), "Inactive Record – System Batch Job"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTERNET), "Unauthorised Attempts to Access Sources – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTRANET), "Unauthorised Attempts to Access Sources – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTERNET), "User account and password changes – Internet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTRANET), "User account and password changes – Intranet"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_USER_ACTIVITIES_INTERNET), "User Administrator Activities (Internet System Admin)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_SYSTEM_ACTIVITIES_INTRANET), "System Administrator Activities (Intranet System Admin)"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_SUSPENDED), "User accounts suspended"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_LOCK), "User accounts Locked"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_INACTIVATED), "User accounts Inactivated"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTERNET), "Active Internet user accounts with its access rights"));
-        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTRANET), "Active Intranet user accounts with its access rights"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN), "Internet Login"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGOUT), "Internet Logout"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_LOGIN_FAIL), "Internet Login Failure"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTERNET_VALIDATION_FAIL), "Data Validation Failure – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN), "Intranet Login"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGOUT), "Intranet Logout"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_LOGIN_FAIL), "Intranet Login Failure"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INTRANET_ALIDATION_FAIL), "Data Validation Failure – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_VIEW_ALLUSER), "View Record (for all user)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INSERT_ALLUSER), "Insert Record (for all user)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UPDATE_ALLUSER), "Update Record (for all user)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_DELETE_ALLUSER), "Delete Record (for all user)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INSERT_BATCHJOB), "Insert Record – System Batch Job"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UPDATE_BATCHJOB), "Update Record – System Batch Job"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_DELETE_BATCHJOB), "Delete Record – System Batch Job"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_INACTIVE_BATCHJOB), "Inactive Record – System Batch Job"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTERNET), "Unauthorised Attempts to Access Sources – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_UNAUTHORISED_INTRANET), "Unauthorised Attempts to Access Sources – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTERNET), "User account and password changes – Internet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_PASSWORD_CHANGE_INTRANET), "User account and password changes – Intranet"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_USER_ACTIVITIES_INTERNET), "User Administrator Activities (Internet System Admin)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_SYSTEM_ACTIVITIES_INTRANET), "System Administrator Activities (Intranet System Admin)"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_SUSPENDED), "User accounts suspended"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_LOCK), "User accounts Locked"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_INACTIVATED), "User accounts Inactivated"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTERNET), "Active Internet user accounts with its access rights"));
+//        operationList.add(new SelectOption(String.valueOf(AuditTrailConsts.OPERATION_ACCOUNT_ACTIVE_INTRANET), "Active Intranet user accounts with its access rights"));
 
         ParamUtil.setRequestAttr(request, "operationValueTypeSelect", operationList);
 
