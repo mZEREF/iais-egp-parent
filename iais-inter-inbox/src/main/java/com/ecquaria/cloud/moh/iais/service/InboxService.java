7e5db5748808e9943c5713f331aaf5771be9d689
chong_hua <chong_hua@ecquaria.com>
2019-11-23 17:55:30 +0800

    Inter Inbox Mould

--- iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/service/InboxService.java
+++ iais-inter-inbox/src/main/java/com/ecquaria/cloud/moh/iais/service/InboxService.java
@@ -3,6 +3,7 @@ package com.ecquaria.cloud.moh.iais.service;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
 import com.ecquaria.cloud.moh.iais.common.dto.application.AppInboxQueryDto;
+import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxAppQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxQueryDto;
 
 import java.util.List;
@@ -16,5 +17,6 @@ import java.util.List;
 public interface InboxService {
 
     List<AppInboxQueryDto> applicationDoQuery();
+    SearchResult<InboxAppQueryDto> appDoQuery(SearchParam searchParam);
     SearchResult<InboxQueryDto> inboxDoQuery(SearchParam searchParam);
 }
