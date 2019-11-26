57c24fd588eedd62fa5deabea86cc82e41760507
guyin <password$1>
2019-11-26 10:58:27 +0800

    hcsa-inspection

---
+++ hcsa-inspection/src/main/java/com/ecquaria/cloud/moh/iais/service/FillupChklistService.java
@@ -0,0 +1,32 @@
+package com.ecquaria.cloud.moh.iais.service;
+
+
+import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.checklist.CheckItemQueryDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.checklist.ChecklistConfigQueryDto;
+
+/**
+ * @author Guyin
+ * @date 2019/11/22 14:09
+ **/
+
+public interface FillupChklistService {
+    /**
+     * list checklist config by SearchParam
+     * backend method: listChecklistConfig
+     * @param searchParam
+     * @return
+     */
+    SearchResult<ChecklistConfigQueryDto> listChecklistConfig(SearchParam searchParam);
+
+
+    /**
+     * list checklist item by SearchParam
+     * backend method: listChecklistItem
+     * @param searchParam
+     * @return
+     */
+    SearchResult<CheckItemQueryDto> listChklItem(SearchParam searchParam);
+
+}
