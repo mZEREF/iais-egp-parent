495bfbe79205370a7c9fe25ed5691c647cc37b32
suocheng <suocheng@ecquaria.com>
2019-11-27 10:21:15 +0800

    Task routing

---
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/LicenceApproveBatchjob.java
@@ -0,0 +1,18 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * LicenceApproveBatchjob
+ *
+ * @author suocheng
+ * @date 11/26/2019
+ */
+@Delegator("licenceApproveBatchjob")
+@Slf4j
+public class LicenceApproveBatchjob {
+    public void doBatchJob(){
+       //
+    }
+}
---
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
@@ -0,0 +1,10 @@
+package com.ecquaria.cloud.moh.iais.service.client;
+
+/**
+ * ApplicationClientFallback
+ *
+ * @author suocheng
+ * @date 11/26/2019
+ */
+public class ApplicationClientFallback {
+}
