237fc48f175c93557a68eef8db313dbcdefe58be
suocheng <suocheng@ecquaria.com>
2019-11-28 19:06:37 +0800

    Task routing

--- hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicationGroupServiceImpl.java
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicationGroupServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
+import com.ecquaria.cloud.moh.iais.common.constant.rest.RestApiUrlConsts;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationGroupDto;
+import com.ecquaria.cloud.moh.iais.common.utils.RestApiUtil;
 import com.ecquaria.cloud.moh.iais.service.ApplicationGroupService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,11 @@ import org.springframework.stereotype.Service;
 public class ApplicationGroupServiceImpl implements ApplicationGroupService {
     @Override
     public ApplicationGroupDto getApplicationGroupDtoById(String appGroupId) {
-        return null;
+        return RestApiUtil.getByPathParam(RestApiUrlConsts.APPLICATION_GROUP_GROUPID,appGroupId,ApplicationGroupDto.class);
     }
 
     @Override
     public ApplicationGroupDto updateApplicationGroup(ApplicationGroupDto applicationGroupDto) {
-        return null;
+        return RestApiUtil.update(RestApiUrlConsts.IAIS_APPLICATION_GROUP_BE,applicationGroupDto,ApplicationGroupDto.class);
     }
 }
