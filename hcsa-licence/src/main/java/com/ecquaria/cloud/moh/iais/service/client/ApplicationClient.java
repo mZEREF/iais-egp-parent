793abb53c1f642857699bcab7eab4fe105323322
wenkang <wenkang@ecquaria.com>
2019-11-28 10:02:35 +0800

    upload and download

--- hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClient.java
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClient.java
@@ -1,7 +1,14 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.dto.application.ApplicationViewDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloudfeign.FeignConfiguration;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * ApplicationClient
@@ -12,5 +19,8 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 @FeignClient(name = "iais-application", configuration = FeignConfiguration.class,
         fallback = ApplicationClientFallback.class)
 public interface ApplicationClient {
-
+    @RequestMapping(path = "/iais-application-be/applicationview/{appNo}",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<ApplicationViewDto> getAppViewByNo(@PathVariable("appNo") String appNo);
+    @RequestMapping(path = "/iais-application-be/application/{appNo}",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<ApplicationDto> getAppByNo(@PathVariable("appNo") String appNo);
 }
--- hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
@@ -1,5 +1,11 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.dto.application.ApplicationViewDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PathVariable;
+
 /**
  * ApplicationClientFallback
  *
@@ -7,4 +13,16 @@ package com.ecquaria.cloud.moh.iais.service.client;
  * @date 11/26/2019
  */
 public class ApplicationClientFallback {
+    FeignResponseEntity<ApplicationViewDto> getAppViewByNo(@PathVariable("appNo") String appNo){
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+    FeignResponseEntity<ApplicationDto> getAppByNo(@PathVariable("appNo") String appNo){
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
 }
