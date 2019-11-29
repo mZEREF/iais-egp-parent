6c1a15885b0a2ceecae217e21bf4a5d0491a86e8
Jinhua <jinhua@ecquaria.com>
2019-11-29 10:18:50 +0800

    Add SG configuration profile

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/RestBridgeHelper.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/RestBridgeHelper.java
@@ -1,6 +1,5 @@
 package com.ecquaria.cloud.moh.iais.helper;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
