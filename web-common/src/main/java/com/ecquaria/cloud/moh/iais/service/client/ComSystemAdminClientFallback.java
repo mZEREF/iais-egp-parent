18be00bbfc34a50b6ea661bf462971ef575e0ff2
Jinhua <jinhua@ecquaria.com>
2019-12-03 17:32:08 +0800

    Add docker to licence

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ComSystemAdminClientFallback.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ComSystemAdminClientFallback.java
@@ -2,6 +2,7 @@ package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloud.moh.iais.common.dto.organization.OrgUserDto;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
+import java.util.List;
 import org.springframework.http.HttpHeaders;
 
 /**
@@ -12,6 +13,18 @@ import org.springframework.http.HttpHeaders;
  */
 public class ComSystemAdminClientFallback {
     public FeignResponseEntity<OrgUserDto> retrieveOrgUserAccount(String userId) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+    public FeignResponseEntity<List<String>> getWorkGrpsByUserId(String userId) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+    public FeignResponseEntity<List<String>> retrieveUserRoles(String userId) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
