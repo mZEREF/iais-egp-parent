c6ffe5cd92e0906e8924f8fb38c112a5df7e3446
suocheng <suocheng@ecquaria.com>
2019-12-05 11:47:50 +0800

    Licence

--- hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClient.java
+++ hcsa-licence/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClient.java
@@ -9,16 +9,14 @@ import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationLicenc
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationListFileDto;
 import com.ecquaria.cloudfeign.FeignConfiguration;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
+import java.util.List;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 /**
  * ApplicationClient
