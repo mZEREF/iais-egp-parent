d304ba56d3d5f24105f1dff788a4a4ba1cc7f836
caijing <caijing@ecquaria.com>
2019-12-05 19:06:22 +0800

    Licences Module

--- hcsa-licence/src/main/webapp/deployed/INTRANET/ApplicationView/1/src/sop/webflow/rt/java/code/INTRANET___ApplicationView___1.java
+++ hcsa-licence/src/main/webapp/deployed/INTRANET/ApplicationView/1/src/sop/webflow/rt/java/code/INTRANET___ApplicationView___1.java
@@ -18,96 +18,65 @@ import com.ecquaria.egp.core.application.controller.ApplicationViewController;
 public class INTRANET___ApplicationView___1 extends BaseProcessClass {
 
 	private static final String DELEGATOR ="hcsaApplicationDelegator";
+
 	public void prepareData_OnStepProcess_0() throws Exception {
 		EngineHelper.delegate(DELEGATOR, "prepareData", this);
 	}
-
-
-
-
 
 	public void aSO_OnStepProcess_0() throws Exception {
 	// 		ASO->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToASO", this);
 	}
 
-
 	public void iNS_OnStepProcess_0() throws Exception {
 	// 		INS->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToINS", this);
 	}
 
-
-
-	
 	public void aO2_OnStepProcess_0() throws Exception {
 	// 		AO2->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToAO2", this);
 	}
-
 
 	public void aO3_OnStepProcess_0() throws Exception {
 	// 		AO3->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToAO3", this);
 	}
 
-
 	public void aO1_OnStepProcess_0() throws Exception {
 	// 		AO1->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToAO1", this);
 	}
-
 
 	public void pSO_OnStepProcess_1() throws Exception {
 	// 		PSO->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "rontingTaskToPSO", this);
 	}
 
-
 	public void start_OnStepProcess_0() throws Exception {
 	// 		Start->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "doStart", this);
 	}
-
-
-
-
 
 	public void chooseStage_OnStepProcess_0() throws Exception {
 	// 		chooseStage->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "chooseStage", this);
 	}
 
-
-
-
-
 	public void approve_OnStepProcess_0() throws Exception {
 	// 		Approve->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "approve", this);
 	}
-
-
-
-
 
 	public void dMS_OnStepProcess_0() throws Exception {
 	// 		DMS->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "routeToDMS", this);
 	}
 
-
-
-
-
 	public void routeBack_OnStepProcess_0() throws Exception {
 	// 		RouteBack->OnStepProcess
 		EngineHelper.delegate(DELEGATOR, "routeBack", this);
 	}
-
-
-
-
 
 	public void internalEnquiry_OnStepProcess_0() throws Exception {
 	// 		InternalEnquiry->OnStepProcess
