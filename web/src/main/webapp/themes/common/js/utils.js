5f5d236e18da2e97865f7405c1e1e1cc17c57e94
Jinhua <jinhua@ecquaria.com>
2019-12-12 22:47:56 +0800

    Add validation

--- web/src/main/webapp/themes/common/js/utils.js
+++ web/src/main/webapp/themes/common/js/utils.js
@@ -9,12 +9,11 @@ function dismissWaiting() {
 }
 
 function doValidation(){
-    var baseContextPath = $("#baseContextPath").val();
     clearErrorMsg();
     $.ajax({
         async: false,
         type: 'POST',
-        url: baseContextPath + "/validation.do",
+        url: BASE_CONTEXT_PATH + "/validation.do",
         data:$('form').serialize(),
         success: function(data){
             doValidationParse(data);
@@ -22,7 +21,7 @@ function doValidation(){
         error: function(jqXHR, textStatus, errorThrown) {
             alert("error: " + textStatus);
             alert("error: " + errorThrown);
-            $("#emsErrorFlag").val('Error:Exception');
+            $("#iaisErrorFlag").val('Error:Exception');
         }
     });
 }
@@ -37,4 +36,41 @@ function formatHTMLEnCode(s1){
     s1 = s1.replace(/\"/g,"&quot;");
     s1 = s1.replace(/\'/g,"&acute;");
     return s1;
+}
+
+function clearErrorMsg(){
+    $("span[name='iaisErrorMsg']").each(function(){
+        $(this).html("");
+    });
+}
+
+function doValidationParse(data){
+    if(data != "[]"){
+        $("#iaisErrorFlag").val("BLOCK");
+        var results = jQuery.parseJSON(data);
+
+        for(var i= 0 ; i< results.length ; i ++){
+            for(var key in results[i]){
+                var error_key="error_" + key.replace(/\./g,'\\.');
+                if (document.getElementById(error_key)) {
+                    $("#"+error_key).show();
+                    if (error_key == 'error_topErrorDiv'
+                        || error_key.indexOf('noEscapeXml') > 0) {
+                        document.getElementById(error_key).innerHTML = results[i][key];
+                    } else {
+                        document.getElementById(error_key).innerHTML = formatHTMLEnCode(results[i][key]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function getErrorMsg(){
+    var msgStatus = false;
+    if($("#iaisErrorFlag").val() != null && $("#iaisErrorFlag").val() != ""){
+        msgStatus =  true;
+    }
+
+    return msgStatus;
 }
