089e2faa72d0ff269ce795f4f7cc3a1d29d4a511
Jinhua <jinhua@ecquaria.com>
2019-12-13 13:52:40 +0800

    Add validation

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/tags/DatePickerTag.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/tags/DatePickerTag.java
@@ -3,11 +3,9 @@ package com.ecquaria.cloud.moh.iais.tags;
 import com.ecquaria.cloud.moh.iais.common.utils.Formatter;
 import com.ecquaria.cloud.moh.iais.common.utils.MiscUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
-import com.ecquaria.cloud.moh.iais.helper.AccessUtil;
-
+import java.util.Date;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
-import java.util.Date;
 
 /**
  * Date Picker Tag
@@ -32,6 +30,7 @@ public final class DatePickerTag extends DivTagSupport {
     private String disableWeekDay; // 0 - 6
     private String startDate;
     private String endDate;
+    private boolean needErrorSpan;
     
     public DatePickerTag() {
         super();
@@ -54,6 +53,7 @@ public final class DatePickerTag extends DivTagSupport {
         title = null;
         startDate = null;
         endDate = null;
+        needErrorSpan = true;
     }
 
     // Releases any resources we may have (or inherit)
@@ -73,30 +73,17 @@ public final class DatePickerTag extends DivTagSupport {
     }
     
     public String generateHtml() {
-        boolean isBE = AccessUtil.isBackend();
         if (StringUtil.isEmpty(this.id)) {
             this.id = String.valueOf(MiscUtil.getDummyId());
         }
         StringBuilder html = new StringBuilder();
-        generateStartHtml(html,isBE);
+        generateStartHtml(html);
         generateFromToHtml(html);
-        generateEndHtml(html,isBE);
+        generateEndHtml(html);
         return html.toString();
     }
-    private void generateStartHtml(StringBuilder html,boolean isBE) {
-        if (isBE) {
-            html.append("<div class=\"input-group date form_date\"");
-            html.append(" id=\"").append(id).append("Gp\"");
-            if (!StringUtil.isEmpty(style)) {
-                html.append(" style=\"").append(style).append("\"");
-            }
-            html.append(">");
-        }
-        if (isBE) {
-            html.append("<input type=\"text\" class=\"form-control wxpicker").append(workingDay ? "-working" : "");
-        } else {
-            html.append("<input type=\"text\" class=\"date_picker form-control").append(workingDay ? "-working" : "");
-        }
+    private void generateStartHtml(StringBuilder html) {
+        html.append("<input type=\"text\" autocomplete=\"off\" class=\"date_picker form-control").append(workingDay ? "-working" : "");
         if (!StringUtil.isEmpty(cssClass)) {
             html.append(" ").append(cssClass);
         }
@@ -120,7 +107,7 @@ public final class DatePickerTag extends DivTagSupport {
             html.append(" data-date-end-date=\"").append(endDate).append("\"");
         }
     }
-    private void generateEndHtml(StringBuilder html,boolean isBE) {
+    private void generateEndHtml(StringBuilder html) {
         if (!StringUtil.isEmpty(disableWeekDay)) {
             html.append(" data-date-days-of-week-disabled=\"").append(disableWeekDay).append("\"");
         }
@@ -143,9 +130,9 @@ public final class DatePickerTag extends DivTagSupport {
             html.append(" title=\"").append(title).append("\"");
         }
         html.append(" placeholder=\"dd/mm/yyyy\" maxlength=\"10\"/>");
-        if (isBE) {
-            html.append("<span class=\"input-group-addon\">");
-            html.append("<button type=\"button\" style=\"margin-right:0px\" class=\"btn btn-primary date-set wxdate-set\"><i class=\"fa fa-calendar\"></i></button></span></div>");
+        if (needErrorSpan) {
+            html.append("<span id=\"error_").append(name).append("\"");
+            html.append(" name=\"iaisErrorMsg\" class=\"error-msg\"></span>");
         }
     }
     @Override
