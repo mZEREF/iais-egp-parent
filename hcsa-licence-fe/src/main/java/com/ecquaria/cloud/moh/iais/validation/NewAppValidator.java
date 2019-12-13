ab7ff5aa8fb6bede7b9daedab58b8e6ae7d01db4
Jinhua <jinhua@ecquaria.com>
2019-12-13 09:29:45 +0800

    Add validation

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/validation/NewAppValidator.java
@@ -0,0 +1,18 @@
+package com.ecquaria.cloud.moh.iais.validation;
+
+import com.ecquaria.cloud.moh.iais.common.validation.interfaces.CustomizeValidator;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * NewAppValidator
+ *
+ * @author Jinhua
+ * @date 2019/12/13 9:27
+ */
+public class NewAppValidator implements CustomizeValidator {
+    @Override
+    public Map<String, String> validate(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+}
