9a4566314e95d76168871730d93e555f08f0b41d
ShiCheng_Xu <shicheng@ecquaria.com>
2019-12-18 17:32:44 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/InspectionRectificationProService.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/InspectionRectificationProService.java
@@ -1,8 +1,30 @@
 package com.ecquaria.cloud.moh.iais.service;
 
+import com.ecquaria.cloud.moh.iais.common.dto.SelectOption;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesRoutingHistoryDto;
+
+import java.util.List;
+
 /**
  * @author Shicheng
  * @date 2019/12/18 14:26
  **/
 public interface InspectionRectificationProService {
+    /**
+      * @author: shicheng
+      * @Date 2019/12/18
+      * @Param: appNo, stageId
+      * @return: AppPremisesRoutingHistoryDto
+      * @Descripation: get ppPremisesRoutingHistoryDto By appNo
+      */
+    AppPremisesRoutingHistoryDto getAppHistoryByTask(String appNo, String stageId);
+
+    /**
+      * @author: shicheng
+      * @Date 2019/12/18
+      * @Param: null
+      * @return:
+      * @Descripation:
+      */
+    List<SelectOption> getProcessRecDecOption();
 }
--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionRectificationProImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionRectificationProImpl.java
@@ -1,11 +1,33 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
+import com.ecquaria.cloud.moh.iais.common.dto.SelectOption;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesRoutingHistoryDto;
+import com.ecquaria.cloud.moh.iais.service.InspectionRectificationProService;
+import com.ecquaria.cloud.moh.iais.service.client.AppPremisesRoutingHistoryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Shicheng
  * @date 2019/12/18 14:27
  **/
 @Service
-public class InspectionRectificationProImpl {
+public class InspectionRectificationProImpl implements InspectionRectificationProService {
+
+    @Autowired
+    private AppPremisesRoutingHistoryClient appPremisesRoutingHistoryClient;
+
+    @Override
+    public AppPremisesRoutingHistoryDto getAppHistoryByTask(String appId, String stageId) {
+        AppPremisesRoutingHistoryDto appPremisesRoutingHistoryDto = appPremisesRoutingHistoryClient.getAppPremisesRoutingHistorysByAppIdAndStageId(appId, stageId).getEntity();
+
+        return appPremisesRoutingHistoryDto;
+    }
+
+    @Override
+    public List<SelectOption> getProcessRecDecOption() {
+        return null;
+    }
 }
