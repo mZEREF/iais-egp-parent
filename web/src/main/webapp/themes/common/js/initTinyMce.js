6d144e251eb4292328193610779ac871cfcc8c12
chong_hua <chong_hua@ecquaria.com>
2019-12-25 18:26:49 +0800

    Tiny

--- web/src/main/webapp/themes/common/js/initTinyMce.js
+++ web/src/main/webapp/themes/common/js/initTinyMce.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     tinymce.init({
         selector: "#htmlEditroArea",  // change this value according to your HTML
@@ -6,7 +5,8 @@ $(document).ready(function(){
         plugins: ['print preview fullpage',
             'advlist autolink lists link image charmap print preview anchor',
             'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
+            'insertdatetime media table paste code help wordcount',
+            'noneditable'
         ],
         toolbar: 'undo redo | formatselect | ' +
             ' bold italic backcolor | alignleft aligncenter ' +
