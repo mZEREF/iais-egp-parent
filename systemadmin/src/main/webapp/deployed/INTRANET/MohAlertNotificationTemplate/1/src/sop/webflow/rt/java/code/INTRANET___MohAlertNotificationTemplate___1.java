e46e696441f8a347845e8a58dfb62da3c83ae95e
chong_hua <chong_hua@ecquaria.com>
2019-12-25 18:29:32 +0800

    Notification Templates

---
+++ systemadmin/src/main/webapp/deployed/INTRANET/MohAlertNotificationTemplate/1/src/sop/webflow/rt/java/code/INTRANET___MohAlertNotificationTemplate___1.java
@@ -0,0 +1,37 @@
+package sop.webflow.rt.java.code;
+import com.ecquaria.cloud.helper.EngineHelper;
+import sop.webflow.rt.api.BaseProcessClass;
+
+public class INTRANET___MohAlertNotificationTemplate___1 extends BaseProcessClass {
+
+	private static final String DELEGATOR ="templatesDelegator";
+	
+	public void prepareDate_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "prepareData", this);
+	}
+
+	public void prepareSwitch_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "prepareSwitch", this);
+	}
+
+	public void editTemplate_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "editTemplate", this);
+	}
+
+	public void searchTemplate_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "searchTemplate", this);
+	}
+
+	public void preViewTemplate_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "preView", this);
+	}
+
+	public void start_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "doStart", this);
+	}
+
+	public void doEdit_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "doEdit", this);
+	}
+
+}
