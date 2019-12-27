f69daa3d10545ce1521ead8fdd8a2a07954cedb2
zixian <zixian@ecquaria.com>
2019-12-27 13:12:25 +0800

    new

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/MasterCodeUtil.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/MasterCodeUtil.java
@@ -76,6 +76,7 @@ public final class MasterCodeUtil {
     public static final String CATE_ID_PROCESSING_DECISION         = "B8A4E683-F517-EA11-BE78-000C29D29DB0";
     public static final String CATE_ID_INSPECTION_STATUS           = "D4EA93FF-511A-EA11-BE78-000C29D29DB0";
     public static final String CATE_ID_SERVICE_STEP                = "E498C55D-9723-EA11-BE7D-000C29F371DC";
+    public static final String CATE_ID_SERVICE_PERSONNEL_PSN_TYPE  = "D44B5FF6-5028-EA11-BE78-000C29D29DB0";
 
     /**
      * @description: refresh the master codes into cache
