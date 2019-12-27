53ecfcf4e5b34e54d2ae6ed5d495457dcf695b16
ShiCheng_Xu <shicheng@ecquaria.com>
2019-12-27 16:15:12 +0800

    new code

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/InspecSaveBeRecByService.java
@@ -0,0 +1,49 @@
+package com.ecquaria.cloud.moh.iais.service;
+
+
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.common.dto.system.ProcessFileTrackDto;
+
+import java.util.List;
+
+/**
+ * @author Shicheng
+ * @date 2019/12/27 14:15
+ **/
+public interface InspecSaveBeRecByService {
+    /**
+      * @author: shicheng
+      * @Date 2019/12/27
+      * @Param: applicationStatusFeToBeRectification, commonStatusActive
+      * @return:
+      * @Descripation:
+      */
+    List<ProcessFileTrackDto> getFileTypeAndStatus(String applicationStatusFeToBeRectification, String commonStatusActive);
+
+    /**
+      * @author: shicheng
+      * @Date 2019/12/27
+      * @Param: null
+      * @return: void
+      * @Descripation: delete UnZip File
+      */
+    void deleteUnZipFile();
+
+    /**
+      * @author: shicheng
+      * @Date 2019/12/27
+      * @Param: null
+      * @return: void
+      * @Descripation: compress File
+      */
+    void compressFile();
+
+    /**
+      * @author: shicheng
+      * @Date 2019/12/27
+      * @Param: intranet
+      * @return: Boolean
+      * @Descripation: save Data
+      */
+    Boolean saveData(AuditTrailDto intranet);
+}
