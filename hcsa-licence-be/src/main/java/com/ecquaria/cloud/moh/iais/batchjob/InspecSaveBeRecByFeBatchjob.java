10e1e2e375e7f43446183cd3ce304d88cc80b3e0
ShiCheng_Xu <shicheng@ecquaria.com>
2019-12-27 16:46:14 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspecSaveBeRecByFeBatchjob.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspecSaveBeRecByFeBatchjob.java
@@ -1,9 +1,19 @@
 package com.ecquaria.cloud.moh.iais.batchjob;
 
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.constant.ApplicationConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.common.dto.system.ProcessFileTrackDto;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
+import com.ecquaria.cloud.moh.iais.service.InspecSaveBeRecByService;
+import com.ecquaria.cloud.moh.iais.service.TaskService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import sop.webflow.rt.api.BaseProcessClass;
+
+import java.util.List;
 
 /**
  * @author Shicheng
@@ -12,6 +22,13 @@ import sop.webflow.rt.api.BaseProcessClass;
 @Delegator("inspecSaveBeRecByFeDelegator")
 @Slf4j
 public class InspecSaveBeRecByFeBatchjob {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private InspecSaveBeRecByService inspecSaveBeRecByService;
+
     /**
      * StartStep: inspecSaveBeRecByFeStart
      *
@@ -30,6 +47,12 @@ public class InspecSaveBeRecByFeBatchjob {
      */
     public void inspecSaveBeRecByFePre(BaseProcessClass bpc){
         logAbout("inspecSaveBeRecByFePre");
+        List<ProcessFileTrackDto> processFileTrackDtos = inspecSaveBeRecByService.getFileTypeAndStatus(ApplicationConsts.APPLICATION_STATUS_FE_TO_BE_RECTIFICATION,
+                AppConsts.COMMON_STATUS_ACTIVE);
+        AuditTrailDto intranet = AuditTrailHelper.getBatchJobDto(AppConsts.DOMAIN_INTRANET);
+        inspecSaveBeRecByService.deleteUnZipFile();
+        inspecSaveBeRecByService.compressFile(processFileTrackDtos);
+        Boolean saveDataFlag = inspecSaveBeRecByService.saveData(intranet);
     }
 
     private void logAbout(String methodName){
