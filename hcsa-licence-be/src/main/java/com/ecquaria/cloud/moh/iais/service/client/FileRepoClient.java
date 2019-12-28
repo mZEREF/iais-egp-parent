2dd2c59a35f5f9baeeddd14e10dc10c2e412013d
wenkang <wenkang@ecquaria.com>
2019-12-28 14:57:19 +0800

    mess

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClient.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClient.java
@@ -2,25 +2,17 @@ package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloud.moh.iais.config.FeignMultipartConfig;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
-<<<<<<< Updated upstream
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-=======
-import io.swagger.annotations.ApiParam;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
-<<<<<<< Updated upstream
  * FileRepoClient
  *
  * @author Jinhua
@@ -35,18 +27,4 @@ public interface FileRepoClient {
                                           @RequestParam("audittrail") String auditTrailStr);
     @GetMapping(value = "/{guid}")
     FeignResponseEntity<byte[]> getFileFormDataBase(@PathVariable(name = "guid") String guid);
-=======
- * @author Wenkang
- * @date 2019/12/28 14:21
- */
-@FeignClient(value = "file-repository", configuration = {FeignMultipartConfig.class},
-        fallback = FileRepoClientFallback.class)
-public interface FileRepoClient {
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-FeignResponseEntity<String> saveFileToDataBase(
-        @RequestPart("selectedFile") MultipartFile files,
-        @ApiParam(name = "audittrail", required = true)
-        @RequestParam("audittrail") String auditTrailStr,
-        @RequestParam(value="respId", required = false) String respId);
->>>>>>> Stashed changes
 }
--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClientFallback.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClientFallback.java
@@ -1,7 +1,6 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloudfeign.FeignResponseEntity;
-<<<<<<< Updated upstream
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,31 +18,9 @@ public class FileRepoClientFallback {
         return entity;
     }
     public FeignResponseEntity<byte[]> getFileFormDataBase(String guid) {
-=======
-import io.swagger.annotations.ApiParam;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-/**
- * @author Wenkang
- * @date 2019/12/28 14:21
- */
-public class FileRepoClientFallback {
-    FeignResponseEntity<String> saveFileToDataBase(
-           MultipartFile files,
-            @ApiParam(name = "audittrail", required = true)
-            String auditTrailStr,
-             String respId){
->>>>>>> Stashed changes
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
         return entity;
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
     }
 }
