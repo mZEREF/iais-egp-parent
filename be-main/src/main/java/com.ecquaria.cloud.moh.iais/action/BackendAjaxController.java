c878c7562f94a3fb9bd7303157d720e4a4575580
ShiCheng_Xu <shicheng@ecquaria.com>
2020-01-10 15:07:20 +0800

    new code

--- be-main/src/main/java/com.ecquaria.cloud.moh.iais/action/BackendAjaxController.java
+++ be-main/src/main/java/com.ecquaria.cloud.moh.iais/action/BackendAjaxController.java
@@ -1,11 +1,13 @@
 package com.ecquaria.cloud.moh.iais.action;
 
+import com.ecquaria.cloud.moh.iais.common.constant.HcsaConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.inspection.InspectionConstants;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaServiceDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inspection.AppInspectionStatusDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inspection.InspectionAppInGroupQueryDto;
+import com.ecquaria.cloud.moh.iais.common.dto.task.TaskDto;
 import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
 import com.ecquaria.cloud.moh.iais.dto.LoginContext;
 import com.ecquaria.cloud.moh.iais.helper.MasterCodeUtil;
@@ -50,6 +52,7 @@ public class BackendAjaxController {
         String groupNo = request.getParameter("groupno");
         SearchParam searchParamAjax = (SearchParam) ParamUtil.getSessionAttr(request, "searchParamAjax");
         Map<String, String> appNoUrl = (Map<String, String>) ParamUtil.getSessionAttr(request, "appNoUrl");
+        Map<String, TaskDto> taskMap = (Map<String, TaskDto>) ParamUtil.getSessionAttr(request, "taskMap");
 
         Map<String, Object> map = new HashMap();
         if (groupNo != null) {
@@ -65,7 +68,8 @@ public class BackendAjaxController {
                 if(item.getHciCode()==null){
                     item.setHciCode("N/A");
                 }
-                String timeLimitWarningColour = getTimeLimitWarningColourByTask(item);
+                TaskDto taskDto = taskMap.get(item.getRefNo());
+                String timeLimitWarningColour = getTimeLimitWarningColourByTask(item, hcsaServiceDto, taskDto);
                 item.setTimeLimitWarning(timeLimitWarningColour);
             }
             map.put("serviceName", serviceNameMap);
@@ -78,9 +82,13 @@ public class BackendAjaxController {
         return map;
     }
 
-    private String getTimeLimitWarningColourByTask(InspectionAppInGroupQueryDto inspectionAppInGroupQueryDto) {
-        String subStage = getSubStageByInspectionStatus(inspectionAppInGroupQueryDto);
+    private String getTimeLimitWarningColourByTask(InspectionAppInGroupQueryDto inspectionAppInGroupQueryDto, HcsaServiceDto hcsaServiceDto, TaskDto taskDto) {
         String colour = "black";
+        if(taskDto.getTaskKey().equals(HcsaConsts.ROUTING_STAGE_INS)) {
+            String subStage = getSubStageByInspectionStatus(inspectionAppInGroupQueryDto);
+        } else {
+
+        }
         return colour;
     }
 
@@ -89,8 +97,18 @@ public class BackendAjaxController {
         String subStage = "";
         if(appInspectionStatusDto != null) {
             String status = appInspectionStatusDto.getStatus();
-            if (status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_AO1_RESULT)) {
-
+            if (status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_PRE)) {
+                subStage = HcsaConsts.ROUTING_STAGE_PRE;
+            } else if (status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_CHECKLIST_VERIFY) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_EMAIL_VERIFY) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_AO1_EMAIL_VERIFY) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_REVIEW_CHECKLIST_EMAIL) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_NC_RECTIFICATION_EMAIL) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_REVIEW_RECTIFICATION_NC)) {
+                subStage = HcsaConsts.ROUTING_STAGE_INP;
+            } else if (status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_PREPARE_REPORT) ||
+                    status.equals(InspectionConstants.INSPECTION_STATUS_PENDING_AO1_RESULT)) {
+                subStage = HcsaConsts.ROUTING_STAGE_POT;
             }
         }
         return subStage;
