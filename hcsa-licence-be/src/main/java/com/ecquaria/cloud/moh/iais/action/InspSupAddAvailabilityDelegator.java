d120b6a8f987a3117e39c02a2abc9af7f6aed4cd
ShiCheng_Xu <shicheng@ecquaria.com>
2020-01-13 14:11:43 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/InspSupAddAvailabilityDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/InspSupAddAvailabilityDelegator.java
@@ -8,9 +8,13 @@ package com.ecquaria.cloud.moh.iais.action;
  **/
 
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import com.ecquaria.cloud.moh.iais.helper.AccessUtil;
+import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
 import com.ecquaria.cloud.moh.iais.service.InspSupAddAvailabilityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import sop.webflow.rt.api.BaseProcessClass;
 
 @Delegator("inspSupAddAvailabilityDelegator")
 @Slf4j
@@ -24,5 +28,136 @@ public class InspSupAddAvailabilityDelegator {
         this.inspSupAddAvailabilityService = inspSupAddAvailabilityService;
     }
 
+    /**
+     * StartStep: inspSupAddAvailabilityStart
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityStart(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityStart start ...."));
+        AuditTrailHelper.auditFunction("Inspection Sup Add Availability", "Inspection Sup Add Availability");
+    }
 
+    /**
+     * StartStep: inspSupAddAvailabilityInit
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityInit(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityInit start ...."));
+        AccessUtil.initLoginUserInfo(bpc.request);
+
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityPre
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityPre(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityPre start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityStep
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityStep(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityStep start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilitySearch
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilitySearch(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilitySearch start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilitySort
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilitySort(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilitySort start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityPage
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityPage(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityPage start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityQuery
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityQuery(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityQuery start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityAdd
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityAdd(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityAdd start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityVali
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityVali(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityVali start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityConfirm
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityConfirm(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityConfirm start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityStep2
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityStep2(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityStep2 start ...."));
+    }
+
+    /**
+     * StartStep: inspSupAddAvailabilityDelete
+     *
+     * @param bpc
+     * @throws
+     */
+    public void inspSupAddAvailabilityDelete(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the inspSupAddAvailabilityDelete start ...."));
+    }
 }
