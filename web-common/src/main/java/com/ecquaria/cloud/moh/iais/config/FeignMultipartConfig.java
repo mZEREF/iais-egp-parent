5dad4731db66e8b3364b623c0b5ef0c7b3c6a9b7
Jinhua <jinhua@ecquaria.com>
2020-01-31 17:26:38 +0800

    rollback the upgrade of egp

---
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/config/FeignMultipartConfig.java
@@ -0,0 +1,29 @@
+package com.ecquaria.cloud.moh.iais.config;
+
+import com.ecquaria.cloudfeign.FeignConfiguration;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.cloud.netflix.feign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * FeignMultipartConfig
+ *
+ * @author Jinhua
+ * @date 2019/11/27 12:14
+ */
+@Configuration
+public class FeignMultipartConfig extends FeignConfiguration {
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+
+}
