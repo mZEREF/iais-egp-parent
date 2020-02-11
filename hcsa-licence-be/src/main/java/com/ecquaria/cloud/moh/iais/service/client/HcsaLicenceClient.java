7c2de7110b75b4bc3bd22dd4bc98fc1892490665
jiahao <jiahao_chen@ecquaria.com>
2020-02-11 19:16:59 +0800

    aa

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/HcsaLicenceClient.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/HcsaLicenceClient.java
@@ -10,8 +10,6 @@ import com.ecquaria.cloud.moh.iais.common.dto.hcsa.licence.LicenceGroupDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.licence.PremisesDto;
 import com.ecquaria.cloudfeign.FeignConfiguration;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
-import java.util.List;
-import java.util.Map;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Wenkang
