019ff112d9f1438cf9cf30c0e9ed93f3b6faeae1
ShiCheng_Xu <shicheng@ecquaria.com>
2020-02-18 12:41:12 +0800

    new code

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicantConfirmInspDateServiceImpl.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicantConfirmInspDateServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
 import com.ecquaria.cloud.moh.iais.common.dto.appointment.ApptFeConfirmDateDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesInspecApptDto;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.service.ApplicantConfirmInspDateService;
+import com.ecquaria.cloud.moh.iais.service.client.InspectionFeClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +16,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class ApplicantConfirmInspDateServiceImpl implements ApplicantConfirmInspDateService {
+
+    @Autowired
+    private InspectionFeClient inspectionFeClient;
     /**
      * Specific Date
      */
@@ -20,7 +26,15 @@ public class ApplicantConfirmInspDateServiceImpl implements ApplicantConfirmInsp
     public ApptFeConfirmDateDto getSpecificDateDto(String appPremCorrId) {
         ApptFeConfirmDateDto apptFeConfirmDateDto = new ApptFeConfirmDateDto();
         if(!StringUtil.isEmpty(appPremCorrId)){
+            AppPremisesInspecApptDto appPremisesInspecApptDto = inspectionFeClient.getSpecificDtoByAppPremCorrId(appPremCorrId).getEntity();
 
+            if(appPremisesInspecApptDto != null){
+                apptFeConfirmDateDto.setSpecificDate(appPremisesInspecApptDto.getSpecificInspDate());
+                apptFeConfirmDateDto.setApptStartDate(appPremisesInspecApptDto.getStartDate());
+                apptFeConfirmDateDto.setApptEndDate(appPremisesInspecApptDto.getEndDate());
+                apptFeConfirmDateDto.setAppPremCorrId(appPremCorrId);
+                apptFeConfirmDateDto.setApptRefNo(appPremisesInspecApptDto.getApptRefNo());
+            }
         }
         return apptFeConfirmDateDto;
     }
