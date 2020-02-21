c8ecf43cb6fb0d3bc656d666626b84958b4b0ef0
suocheng <suocheng@ecquaria.com>
2020-02-21 18:02:27 +0800

    FE

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BeEicGatewayClient.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BeEicGatewayClient.java
@@ -58,7 +58,7 @@ public interface BeEicGatewayClient {
             @RequestHeader("date-Secondary") String dateSec,
             @RequestHeader("authorization-Secondary") String authorizationSec);
 
-    @PostMapping(value = "/v1/app-request-for-info/",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/v1/app-req-for-info/",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<AppEditSelectDto> createAppEditSelectDto(@RequestBody AppEditSelectDto  appEditSelectDto,
                                                         @RequestHeader("date") String date,
                                                         @RequestHeader("authorization") String authorization,
