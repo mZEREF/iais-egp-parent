1437dc98f8d095bde67af0182ddce8249598862a
ShiCheng_Xu <shicheng@ecquaria.com>
2020-02-27 17:13:07 +0800

    new code

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClient.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClient.java
@@ -1,16 +1,21 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.dto.filerepo.FileRepoDto;
 import com.ecquaria.cloud.moh.iais.config.FeignMultipartConfig;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * FileRepoClient
@@ -28,5 +33,7 @@ public interface FileRepoClient {
     @GetMapping(value = "/{guid}")
     FeignResponseEntity<byte[]> getFileFormDataBase(@PathVariable(name = "guid") String guid);
 
-
+    @PostMapping(value = "/files", produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    FeignResponseEntity<List<FileRepoDto>> getFilesByIds(@RequestBody List<String> ids);
 }
--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClientFallback.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/FileRepoClientFallback.java
@@ -1,8 +1,11 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.dto.filerepo.FileRepoDto;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * FileRepoClientFallback
@@ -10,14 +13,26 @@ import org.springframework.web.multipart.MultipartFile;
  * @author Jinhua
  * @date 2019/11/26 12:29
  */
-public class FileRepoClientFallback {
+public class FileRepoClientFallback implements FileRepoClient {
+
+    @Override
     public FeignResponseEntity<String> saveFiles(MultipartFile file, String fileRepoStr) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
         return entity;
     }
+
+    @Override
     public FeignResponseEntity<byte[]> getFileFormDataBase(String guid) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<List<FileRepoDto>> getFilesByIds(List<String> ids) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
