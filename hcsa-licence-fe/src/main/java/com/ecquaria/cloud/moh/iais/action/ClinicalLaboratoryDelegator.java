90df835af89cd8c9185465e1554b9b83b3e51f2c
zixian <zixian@ecquaria.com>
2020-03-02 10:08:51 +0800

    new

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClinicalLaboratoryDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClinicalLaboratoryDelegator.java
@@ -412,61 +412,10 @@ public class ClinicalLaboratoryDelegator {
         String svcId = ParamUtil.getMaskedString(bpc.request, "svcId");
         AppSvcRelatedInfoDto appSvcRelatedInfoDto = getAppSvcRelatedInfo(bpc.request, svcId);
         ParamUtil.setSessionAttr(bpc.request, "currentPreviewSvcInfo", appSvcRelatedInfoDto);
-        List<AppSvcDisciplineAllocationDto> allocationDto = null;
-        if(appSvcRelatedInfoDto != null){
-            allocationDto = appSvcRelatedInfoDto.getAppSvcDisciplineAllocationDtoList();
-        }
-        List<AppGrpPremisesDto> appGrpPremisesDtoList = getAppSubmissionDto(bpc.request).getAppGrpPremisesDtoList();
-        Map<String,List<AppSvcDisciplineAllocationDto>> reloadDisciplineAllocationMap = new HashMap<>();
-        for(AppGrpPremisesDto appGrpPremisesDto:appGrpPremisesDtoList){
-            List<AppSvcDisciplineAllocationDto> reloadDisciplineAllocation = new ArrayList<>();
-            String hciName = "";
-            if(ApplicationConsts.PREMISES_TYPE_ON_SITE.equals(appGrpPremisesDto.getPremisesType())){
-                hciName = appGrpPremisesDto.getHciName();
-            }else if(ApplicationConsts.PREMISES_TYPE_CONVEYANCE.equals(appGrpPremisesDto.getPremisesType())){
-                hciName = appGrpPremisesDto.getConveyanceVehicleNo();
-            }
+        AppSubmissionDto appSubmissionDto = (AppSubmissionDto) ParamUtil.getSessionAttr(bpc.request,NewApplicationDelegator.APPSUBMISSIONDTO);
 
-           if(!StringUtil.isEmpty(hciName) && allocationDto !=null && allocationDto.size()>0 ){
-               for(AppSvcDisciplineAllocationDto appSvcDisciplineAllocationDto:allocationDto){
-                   List<AppSvcChckListDto> appSvcChckListDtoList = null;
-                   if(hciName.equals(appSvcDisciplineAllocationDto.getPremiseVal())){
-                       String chkLstId = appSvcDisciplineAllocationDto.getChkLstConfId();
-                       String idNo = appSvcDisciplineAllocationDto.getIdNo();
-                       //set chkLstName
-                       List<AppSvcLaboratoryDisciplinesDto> appSvcLaboratoryDisciplinesDtoList =appSvcRelatedInfoDto.getAppSvcLaboratoryDisciplinesDtoList();
-                       if(appSvcLaboratoryDisciplinesDtoList != null && appSvcLaboratoryDisciplinesDtoList.size()>0){
-                           for(AppSvcLaboratoryDisciplinesDto appSvcLaboratoryDisciplinesDto:appSvcLaboratoryDisciplinesDtoList){
-                               if(hciName.equals(appSvcLaboratoryDisciplinesDto.getPremiseVal())){
-                                   appSvcChckListDtoList = appSvcLaboratoryDisciplinesDto.getAppSvcChckListDtoList();
-                               }
-                           }
-                       }
-                       if(appSvcChckListDtoList != null && appSvcChckListDtoList.size()>0){
-                           for(AppSvcChckListDto appSvcChckListDto:appSvcChckListDtoList){
-                               if(chkLstId.equals(appSvcChckListDto.getChkLstConfId())){
-                                   appSvcDisciplineAllocationDto.setChkLstName(appSvcChckListDto.getChkName());
-                               }
-                           }
-                       }
-                       //set selCgoName
-                       List<AppSvcCgoDto> appSvcCgoDtoList = appSvcRelatedInfoDto.getAppSvcCgoDtoList();
-                       if(appSvcCgoDtoList != null && appSvcCgoDtoList.size()>0){
-                           for(AppSvcCgoDto appSvcCgoDto:appSvcCgoDtoList){
-                               if(idNo.equals(appSvcCgoDto.getIdNo())){
-                                   appSvcDisciplineAllocationDto.setCgoSelName(appSvcCgoDto.getName());
-                               }
-                           }
-                       }
-                       reloadDisciplineAllocation.add(appSvcDisciplineAllocationDto);
-                   }
-               }
-           }
-            reloadDisciplineAllocationMap.put(hciName, reloadDisciplineAllocation);
-        }
+        Map<String,List<AppSvcDisciplineAllocationDto>> reloadDisciplineAllocationMap= NewApplicationHelper.getDisciplineAllocationDtoList(appSubmissionDto,svcId);
         ParamUtil.setSessionAttr(bpc.request, "reloadDisciplineAllocationMap", (Serializable) reloadDisciplineAllocationMap);
-
-
 
         log.debug(StringUtil.changeForLog("the do prepareView end ...."));
     }
