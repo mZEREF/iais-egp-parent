b837573ba28918523320ae66a4893aa8e8d206de
chong_hua <chong_hua@ecquaria.com>
2020-03-12 14:38:54 +0800

    Internet Inbox

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EicGatewayFeMainClient.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EicGatewayFeMainClient.java
@@ -1,10 +1,7 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
 
-import com.ecquaria.cloudfeign.FeignConfiguration;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient(value = "eicgate", url="${iais.inter.gateway.url}", configuration = {FeignConfiguration.class},
-        fallback = EicGatewayFeMainClient.class)
+//@FeignClient(value = "eicgate", url="${iais.inter.gateway.url}", configuration = {FeignConfiguration.class},
+//        fallback = EicGatewayFeMainClient.class)
 public interface EicGatewayFeMainClient {
 }
