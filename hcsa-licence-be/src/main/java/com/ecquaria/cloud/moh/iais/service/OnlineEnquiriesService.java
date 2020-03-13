51bff1da407c265fd6bcfc15ad8a97b6dbdeea1a
junyu <junyu@ecquaria.com>
2020-03-13 15:20:15 +0800

    enquiries

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/OnlineEnquiriesService.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/OnlineEnquiriesService.java
@@ -2,12 +2,9 @@ package com.ecquaria.cloud.moh.iais.service;
 
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
-import com.ecquaria.cloud.moh.iais.common.dto.SelectOption;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaSvcQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.onlinenquiry.ProfessionalInformationQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.organization.LicenseeQueryDto;
-
-import java.util.List;
 
 /**
  * OnlineEnquiriesService
@@ -16,9 +13,8 @@ import java.util.List;
  * @date 2020/2/11
  */
 public interface OnlineEnquiriesService {
+
     SearchResult<LicenseeQueryDto> searchLicenseeIdsParam(SearchParam searchParam);
     SearchResult<HcsaSvcQueryDto> searchSvcNamesParam(SearchParam searchParam);
-    List<SelectOption> getLicSvcTypeOption();
-
     SearchResult<ProfessionalInformationQueryDto> searchProfessionalInformation(SearchParam searchParam);
 }
--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/OnlineEnquiriesServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/OnlineEnquiriesServiceImpl.java
@@ -1,9 +1,7 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
-import com.ecquaria.cloud.moh.iais.common.constant.ApplicationConsts;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
-import com.ecquaria.cloud.moh.iais.common.dto.SelectOption;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaSvcQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.onlinenquiry.ProfessionalInformationQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.organization.LicenseeQueryDto;
@@ -14,8 +12,6 @@ import com.ecquaria.cloud.moh.iais.service.client.OrganizationClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * OnlineEnquiriesServiceImpl
@@ -42,15 +38,6 @@ public class OnlineEnquiriesServiceImpl implements OnlineEnquiriesService {
     @Override
     public SearchResult<HcsaSvcQueryDto> searchSvcNamesParam(SearchParam searchParam) {
         return hcsaConfigClient.searchSvcNamesParam(searchParam).getEntity();
-    }
-
-    @Override
-    public List<SelectOption> getLicSvcTypeOption() {
-        List<String> serviceNames =hcsaConfigClient.getHcsaServiceNameByType(ApplicationConsts.SERVICE_CONFIG_TYPE_BASE).getEntity();
-        List<String> serviceNames1 =hcsaConfigClient.getHcsaServiceNameByType(ApplicationConsts.SERVICE_CONFIG_TYPE_SPECIFIED).getEntity();
-
-
-        return null;
     }
 
     @Override
