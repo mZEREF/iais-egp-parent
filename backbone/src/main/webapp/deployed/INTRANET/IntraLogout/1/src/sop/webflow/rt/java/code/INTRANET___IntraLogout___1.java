21e95bdc5a5acb6f45d2d8dfcc4eceffe096e4cf
suocheng <suocheng@ecquaria.com>
2020-03-18 15:53:36 +0800

    logout

--- backbone/src/main/webapp/deployed/INTRANET/IntraLogout/1/src/sop/webflow/rt/java/code/INTRANET___IntraLogout___1.java
+++ backbone/src/main/webapp/deployed/INTRANET/IntraLogout/1/src/sop/webflow/rt/java/code/INTRANET___IntraLogout___1.java
@@ -11,6 +11,7 @@
  */
 package sop.webflow.rt.java.code;
 import sop.webflow.rt.api.BaseProcessClass;
+import com.ecquaria.cloud.helper.EngineHelper;
 
 public class INTRANET___IntraLogout___1 extends BaseProcessClass {
     private static final String DELEGATOR ="iaisLogoutDelegate";
