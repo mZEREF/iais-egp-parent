b61c15d38f3d8964140be2ea448298b5ea2e9dee
HuaChong <chong_hua@ecquaria.com>
2020-03-24 14:01:25 +0800

    Application Withdrawal

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
@@ -445,6 +445,14 @@ public class ApplicationClientFallback implements ApplicationClient{
     }
 
     @Override
+    public FeignResponseEntity<Void> saveWithdrawn() {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
     public FeignResponseEntity<List<AppPremisesCorrelationDto>> getPremCorrDtoByAppGroupId(String appGroupId) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
