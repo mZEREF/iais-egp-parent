f9ffdcc40dabeca18fa330cd45eb503b716f919b
junyu <junyu@ecquaria.com>
2020-04-02 14:23:43 +0800

    egp-qa

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/helper/NewApplicationHelper.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/helper/NewApplicationHelper.java
@@ -46,6 +46,10 @@ public class NewApplicationHelper {
             /*   map.put("checkError","UC_CHKLMD001_ERR002");*/
         }else {
             for(int i=0;i<listDtos.size();i++){
+                if("27D8EB5B-1123-EA11-BE78-000C29D29DB0".equals(listDtos.get(i).getChkLstConfId())&&StringUtil.isEmpty(listDtos.get(i).getOtherScopeName()) ){
+                    map.put("pleaseIndicateError","ERR0009");
+                }
+
                 String parentName = listDtos.get(i).getParentName();
                 if(parentName==null){
                     count++;
