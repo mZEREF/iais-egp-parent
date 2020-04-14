c17e587b290f4da8a72d2c8198855866a235f719
GuYin <password$1>
2020-04-14 17:26:58 +0800

    change api

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BlastManagementListClient.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BlastManagementListClient.java
@@ -8,6 +8,9 @@ import com.ecquaria.cloudfeign.FeignConfiguration;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,14 +26,14 @@ public interface BlastManagementListClient {
     @PostMapping(value = "/iais-emails/getBlastManagementList",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<SearchResult<BlastManagementListDto>> getBlastManagementList(@RequestBody SearchParam searchParam);
 
-    @PostMapping(value = "/iais-emails/saveBlastList",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/iais-emails/blastList",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<BlastManagementDto> saveBlastList(@RequestBody BlastManagementDto blastManagementDto);
 
-    @PostMapping(value = "/iais-emails/deleteBlastList", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/iais-emails/blastList", consumes = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<Void> deleteBlastList(@RequestBody List<String> list);
 
-    @PostMapping(value = "/iais-emails/getBlastById", produces = MediaType.APPLICATION_JSON_VALUE)
-    FeignResponseEntity<BlastManagementDto> getBlastById(@RequestParam("id") String id);
+    @GetMapping(value = "/iais-emails/blastList/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<BlastManagementDto> getBlastById(@PathVariable("id") String id);
 
     @PostMapping(value = "/iais-emails/getBlastBySendTime", produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<List<BlastManagementDto>> getBlastBySendTime(@RequestParam("date") String date);
