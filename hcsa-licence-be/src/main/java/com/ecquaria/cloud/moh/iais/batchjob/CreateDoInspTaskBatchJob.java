21d4624a31eed8165e202b9e00523ad642dea7c7
ShiCheng_Xu <shicheng@ecquaria.com>
2020-04-19 12:19:08 +0800

    new code

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/CreateDoInspTaskBatchJob.java
@@ -0,0 +1,15 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Process MohCreateInspectionTask
+ *
+ * @author Shicheng
+ * @date 2020/4/19 12:08
+ **/
+@Delegator("createDoInspTaskBatchJob")
+@Slf4j
+public class CreateDoInspTaskBatchJob {
+}
