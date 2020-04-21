727e9341913b2dd13881c9c1a326ad84f3ff3193
yichen <yichen_guo@ecquaria.com>
2020-04-21 15:30:33 +0800

    message

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EicGatewayClient.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EicGatewayClient.java
@@ -1,6 +1,5 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
-import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.message.MessageDto;
 import com.ecquaria.cloud.moh.iais.common.dto.parameter.SystemParameterDto;
 import com.ecquaria.cloudfeign.FeignConfiguration;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 		fallback = EicGatewayClientFallBack.class)
 public interface EicGatewayClient {
 	@PostMapping(value = "/v1/sys-params",consumes = MediaType.APPLICATION_JSON_VALUE)
-	FeignResponseEntity<ApplicationDto> saveSystemParameterFe(@RequestBody SystemParameterDto systemParameterDto,
+	FeignResponseEntity<MessageDto> saveSystemParameterFe(@RequestBody SystemParameterDto systemParameterDto,
 	                                                      @RequestHeader("date") String date,
 	                                                      @RequestHeader("authorization") String authorization,
 	                                                      @RequestHeader("date-Secondary") String dateSec,
