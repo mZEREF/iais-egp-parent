93680fc387fe522f48a448054b0e937b588540c8
ShiCheng_Xu <shicheng@ecquaria.com>
2020-04-24 15:11:46 +0800

    new code

--- hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohInspecUserRectifiUpload/1/src/sop/webflow/rt/java/code/INTERNET___MohInspecUserRectifiUpload___1.java
+++ hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohInspecUserRectifiUpload/1/src/sop/webflow/rt/java/code/INTERNET___MohInspecUserRectifiUpload___1.java
@@ -11,6 +11,7 @@
  */
 package sop.webflow.rt.java.code;
 import com.ecquaria.cloud.helper.EngineHelper;
+
 import sop.webflow.rt.api.BaseProcessClass;
 
 public class INTERNET___MohInspecUserRectifiUpload___1 extends BaseProcessClass {
