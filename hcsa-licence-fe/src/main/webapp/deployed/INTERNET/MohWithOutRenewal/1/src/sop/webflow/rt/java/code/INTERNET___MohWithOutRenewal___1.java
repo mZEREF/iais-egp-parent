685efb90fdf55b8c342541618a0a1ab715956f32
zixian <zixian@ecquaria.com>
2020-04-27 19:12:49 +0800

    new

--- hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohWithOutRenewal/1/src/sop/webflow/rt/java/code/INTERNET___MohWithOutRenewal___1.java
+++ hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohWithOutRenewal/1/src/sop/webflow/rt/java/code/INTERNET___MohWithOutRenewal___1.java
@@ -11,7 +11,6 @@
  */
 package sop.webflow.rt.java.code;
 import com.ecquaria.cloud.helper.EngineHelper;
-
 import sop.webflow.rt.api.BaseProcessClass;
 
 public class INTERNET___MohWithOutRenewal___1 extends BaseProcessClass {
@@ -58,9 +57,11 @@ public class INTERNET___MohWithOutRenewal___1 extends BaseProcessClass {
 	}
 	public void toPrepareData_OnStepProcess_0() throws Exception {
 	// 		toPrepareData->OnStepProcess
+		EngineHelper.delegate(DELEGATOR, "toPrepareData", this);
 	}
 	public void prepareJump_OnStepProcess_0() throws Exception {
 	// 		prepareJump->OnStepProcess
+		EngineHelper.delegate(DELEGATOR, "prepareJump", this);
 	}
 	public void step2_OnStepProcess_0() throws Exception {
 	// 		controlSwitch->OnStepProcess
