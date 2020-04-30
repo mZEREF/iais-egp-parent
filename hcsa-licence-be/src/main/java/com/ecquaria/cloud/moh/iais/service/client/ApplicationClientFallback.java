40db8ed732f7790918075f25b1ff50c225859415
junyu <junyu@ecquaria.com>
2020-04-30 09:11:09 +0800

    enquiries

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
@@ -19,7 +19,6 @@ import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationLicenc
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationListFileDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.BroadcastApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.RequestInformationSubmitDto;
-import com.ecquaria.cloud.moh.iais.common.dto.hcsa.cessation.AppCessMiscDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inspection.AdCheckListShowDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inspection.RfiApplicationQueryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.system.ProcessFileTrackDto;
@@ -221,13 +220,7 @@ public class ApplicationClientFallback implements ApplicationClient{
         return entity;
     }
 
-    @Override
-    public FeignResponseEntity<List<AppCessMiscDto>> getAppCessMiscDtosByCorrIds(List<String> corrIds) {
-        FeignResponseEntity entity = new FeignResponseEntity<>();
-        HttpHeaders headers = new HttpHeaders();
-        entity.setHeaders(headers);
-        return entity;
-    }
+
 
     @Override
     public FeignResponseEntity<List<ApplicationDto>> getApplicationByStatus(String appStatus) {
