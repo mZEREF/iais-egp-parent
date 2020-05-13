9a3777de75570403e57aa8e84ad1a53dcbec7e32
Jinhua <jinhua@ecquaria.com>
2020-05-13 10:33:41 +0800

    Fix sonar issue

--- be-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/BelicationViewMainServiceImp.java
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/BelicationViewMainServiceImp.java
@@ -3,8 +3,6 @@ package com.ecquaria.cloud.moh.iais.service.impl;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloud.moh.iais.service.BelicationViewMainService;
 import com.ecquaria.cloud.moh.iais.service.client.BelicationClient;
-import com.ecquaria.cloud.moh.iais.service.client.HcsaConfigMainClient;
-import com.ecquaria.cloud.moh.iais.service.client.OrganizationMainClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,10 +12,6 @@ import org.springframework.stereotype.Service;
 public class BelicationViewMainServiceImp implements BelicationViewMainService {
     @Autowired
     private BelicationClient belicationClient;
-    @Autowired
-    private OrganizationMainClient organizationClient;
-    @Autowired
-    private HcsaConfigMainClient hcsaConfigClient;
 
     @Override
     public ApplicationDto updateApplicaiton(ApplicationDto applicationDto) {
