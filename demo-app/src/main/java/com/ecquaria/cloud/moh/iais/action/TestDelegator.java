7fc93b3115867f15defae2d6330d92e112c865bb
Jinhua <jinhua@ecquaria.com>
2020-05-13 13:37:04 +0800

    Fix sonar issue

--- demo-app/src/main/java/com/ecquaria/cloud/moh/iais/action/TestDelegator.java
+++ demo-app/src/main/java/com/ecquaria/cloud/moh/iais/action/TestDelegator.java
@@ -4,23 +4,21 @@ import com.ecquaria.cloud.annotation.Delegator;
 import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
 import com.ecquaria.egov.core.common.constants.AppConstants;
 import com.ecquaria.egp.core.forms.util.FormRuntimeUtil;
-import ecq.commons.exception.BaseException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import sop.i18n.MultiLangUtil;
 import sop.webflow.rt.api.BaseProcessClass;
 
 @Delegator
+@Slf4j
 public final class TestDelegator {
     public static final String PROJECT_NAME = "IAIS";
     public static final String PROCESS_NAME = "suochengTest";
     public static final String STEP_NAME = "SaveContinueURL";
     private static final String FORM_NAME = "ApplicationForm";
 
-    private static final Logger logger = LoggerFactory.getLogger(new Throwable().getStackTrace()[1].getClassName());
 
     public  void prepareData(BaseProcessClass base) {
-        logger.info("The prepareData start ... ");
+        log.info("The prepareData start ... ");
         FormRuntimeUtil.addButton("<i class=\"fa fa-floppy-o\"></i> " + MultiLangUtil.translate(base.request, AppConstants.KEY_TRANSLATION_MODULE_LABEL, "Save Draft"),
                 "draft", "btn btn-round-lg btn-lg btn-oblue2 btn-st", false);
         FormRuntimeUtil.addButton("<i class=\"fa fa-envelope-o\"></i> " + MultiLangUtil.translate(base.request, AppConstants.KEY_TRANSLATION_MODULE_LABEL, "Submit"),
@@ -29,14 +27,14 @@ public final class TestDelegator {
                 MultiLangUtil.translate(base.request, AppConstants.KEY_TRANSLATION_MODULE_LABEL, "Next")+" <i class=\"fa fa-arrow-circle-o-right\"></i>", "next");
         fb.setCssClass("saas-btn");
         FormRuntimeUtil.addButton(fb);*/
-        logger.info("The prepareData end ... ");
+        log.info("The prepareData end ... ");
     }
 
-    public  void validate(BaseProcessClass base) throws Exception {
+    public  void validate(BaseProcessClass base) {
 
     }
 
-    public void saveDraft(BaseProcessClass bpc) throws BaseException {
+    public void saveDraft(BaseProcessClass bpc) {
     }
     public void initSaveDraftData(BaseProcessClass bpc) {
         String caseid = bpc.request.getParameter("caseid");
@@ -45,24 +43,9 @@ public final class TestDelegator {
         String continueId = bpc.request.getParameter("continueId");
         bpc.getSession().setAttribute("continueId", continueId);
     }
-    public void bATStep0_OnApplicationCreate(BaseProcessClass bpc) throws BaseException{
+    public void bATStep0_OnApplicationCreate(BaseProcessClass bpc) {
 
     }
 
-//    public void test(BaseProcessClass bpc) throws Exception {
-//       String sql = "select *  from SINGPOST_ADDRESS";
-//        ResultSet result = DBToolService.getInstance().query(sql);
-//        int columnCount = 0;
-//        List<String> columnNames = IaisCommonUtils.genNewArrayList();
-//        if (result != null) {
-//            ResultSetMetaData resultSetMetaData = result.getMetaData();
-//            if (resultSetMetaData != null) {
-//                columnCount = resultSetMetaData.getColumnCount();
-//            }
-//            for (int i = 0; i < columnCount; i++) {
-//                columnNames.add(resultSetMetaData.getColumnName(i + 1));
-//            }
-//        }
-//    }
 
 }
