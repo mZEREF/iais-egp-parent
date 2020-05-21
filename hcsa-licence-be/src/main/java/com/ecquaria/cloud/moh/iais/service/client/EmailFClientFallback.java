8bd17aa14e35b71b59c9845d254e0adc8cdad159
weilu <weilu@ecquaria.com>
2020-05-21 16:25:34 +0800

    fix bug

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EmailFClientFallback.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EmailFClientFallback.java
@@ -1,9 +1,13 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloud.moh.iais.common.dto.emailsms.EmailDto;
+import com.ecquaria.cloud.moh.iais.common.dto.emailsms.SmsDto;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Wenkang
@@ -12,6 +16,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class EmailFClientFallback implements EmailClient {
     @Override
     public FeignResponseEntity<String> sendNotification( EmailDto email){
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<Map<String, String>> sendSMS(List<String> recipts, SmsDto sms, String reqRefNum) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
