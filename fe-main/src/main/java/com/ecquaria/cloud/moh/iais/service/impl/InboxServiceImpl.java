95540f2212afd04a849d7355fe1d4d041a1335be
zhilin <lzl19980518>
2020-05-21 09:55:53 +0800

    renew fe validate

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxServiceImpl.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InboxServiceImpl.java
@@ -169,7 +169,8 @@ public class InboxServiceImpl implements InboxService {
         for(ApplicationDto app : apps){
             if(ApplicationConsts.APPLICATION_TYPE_RENEWAL.equals(app.getApplicationType())
                     && !(ApplicationConsts.APPLICATION_STATUS_NOT_PAYMENT.equals(app.getStatus()))
-                    && !(ApplicationConsts.APPLICATION_STATUS_APPROVED.equals(app.getStatus()))){
+                    && !(ApplicationConsts.APPLICATION_STATUS_APPROVED.equals(app.getStatus()))
+                    && !(ApplicationConsts.APPLICATION_STATUS_REJECTED.equals(app.getStatus()))){
                 flag = false;
                 errorMap.put("errorMessage1","This application is performing the renew process");
             }
