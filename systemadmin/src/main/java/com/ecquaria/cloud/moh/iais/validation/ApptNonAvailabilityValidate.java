a1970f21932d538477a9402b94819ef0aba1f895
ShiCheng_Xu <shicheng@ecquaria.com>
2020-05-26 14:12:05 +0800

    new code

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/validation/ApptNonAvailabilityValidate.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/validation/ApptNonAvailabilityValidate.java
@@ -1,19 +1,25 @@
 package com.ecquaria.cloud.moh.iais.validation;
 
+import com.ecquaria.cloud.moh.iais.common.constant.appointment.AppointmentConstants;
 import com.ecquaria.cloud.moh.iais.common.dto.appointment.ApptNonAvailabilityDateDto;
+import com.ecquaria.cloud.moh.iais.common.dto.appointment.ApptUserCalendarDto;
+import com.ecquaria.cloud.moh.iais.common.utils.Formatter;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.MiscUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
 import com.ecquaria.cloud.moh.iais.common.validation.interfaces.CustomizeValidator;
-import com.ecquaria.cloud.moh.iais.service.client.OrganizationClient;
+import com.ecquaria.cloud.moh.iais.service.client.AppointmentClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Shicheng
@@ -23,26 +29,44 @@ import java.util.Map;
 public class ApptNonAvailabilityValidate implements CustomizeValidator {
 
     @Autowired
-    private OrganizationClient organizationClient;
+    private AppointmentClient appointmentClient;
 
     @Override
     public Map<String, String> validate(HttpServletRequest request) {
         ApptNonAvailabilityDateDto apptNonAvailabilityDateDto = (ApptNonAvailabilityDateDto) ParamUtil.getSessionAttr(request, "inspNonAvailabilityDto");
-        List<Date> inspectionDate = IaisCommonUtils.genNewArrayList();
+        ApptUserCalendarDto apptUserCalendarDto = appointmentClient.getCalenderBySysUserCorrIdAndStatus(apptNonAvailabilityDateDto.getUserCorrId(), AppointmentConstants.CALENDAR_STATUS_RESERVED).getEntity();
         Map<String, String> errMap = IaisCommonUtils.genNewHashMap();
         SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
         List<Date> nonAvaDate = MiscUtil.getDateInPeriodByRecurrence(apptNonAvailabilityDateDto.getBlockOutStart(),
                         apptNonAvailabilityDateDto.getBlockOutEnd(), apptNonAvailabilityDateDto.getRecurrence());
-
-        for(Date date : nonAvaDate){
-            for(Date inspDate : inspectionDate){
-                String nonDate = sdf2.format(date);
-                String inspecDate = sdf2.format(inspDate);
-                if(nonDate.equals(inspecDate)){
-                    errMap.put("nonAvaStartDate", "UC_INSP_ERR0004");
+        List<String> inspectionDate = getInspDateByCalendar(apptUserCalendarDto);
+        if(!IaisCommonUtils.isEmpty(inspectionDate)) {
+            for (Date date : nonAvaDate) {
+                for (String inspDate : inspectionDate) {
+                    String nonDate = sdf2.format(date);
+                    if (nonDate.equals(inspDate)) {
+                        errMap.put("nonAvaStartDate", "UC_INSP_ERR0004");
+                    }
                 }
             }
         }
         return errMap;
+    }
+
+    private List<String> getInspDateByCalendar(ApptUserCalendarDto apptUserCalendarDto) {
+        if(apptUserCalendarDto != null){
+            List<Date> timeSlots = apptUserCalendarDto.getStartSlot();
+            if(!IaisCommonUtils.isEmpty(timeSlots)) {
+                List<String> inspDates = IaisCommonUtils.genNewArrayList();
+                for (Date date : timeSlots) {
+                    String inspDate = Formatter.formatDateTime(date, "yyyy-MM-dd");
+                    inspDates.add(inspDate);
+                }
+                Set<String> inspDateSet = new HashSet<>(inspDates);
+                inspDates = new ArrayList<>(inspDateSet);
+                return inspDates;
+            }
+        }
+        return null;
     }
 }
