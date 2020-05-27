b2efcd4e819d4702d8cc69fcd8be80fc0f8f4170
weilu <weilu@ecquaria.com>
2020-05-27 17:58:16 +0800

    fix bug

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ApplicationClientFallback.java
@@ -564,6 +564,14 @@ public class ApplicationClientFallback implements ApplicationClient{
     }
 
     @Override
+    public FeignResponseEntity<List<ApplicationLicenceDto>> getCessGroup(List<String> groupIds) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
     public FeignResponseEntity<ApplicationDto> getApplicationBytaskId(String taskid) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
