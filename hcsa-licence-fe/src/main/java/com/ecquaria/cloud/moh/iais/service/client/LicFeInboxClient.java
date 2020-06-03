c7a3029a11d081be813bdd3dcfd6012851a90865
ShiCheng_Xu <shicheng@ecquaria.com>
2020-06-03 16:49:22 +0800

    new code

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/LicFeInboxClient.java
@@ -0,0 +1,35 @@
+package com.ecquaria.cloud.moh.iais.service.client;
+
+
+import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxMsgMaskDto;
+import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxQueryDto;
+import com.ecquaria.cloudfeign.FeignConfiguration;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "inter-inbox",configuration = FeignConfiguration.class,fallback = LicFeInboxFallback.class)
+public interface LicFeInboxClient {
+    @PostMapping(path = "/iais-inter-inbox/inbox-param", consumes = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<SearchResult<InboxQueryDto>> searchInbox(SearchParam searchParam);
+
+    @GetMapping(path = "/iais-inter-inbox/inbox/unread-unresponse-num", consumes = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<Integer> searchUnreadAndUnresponseNum(String userId);
+
+    @PutMapping(path = "/iais-inter-inbox/archive", consumes = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<Boolean> updateMsgStatusToArchive(String[] msgIds);
+
+    @PutMapping(path = "/iais-inter-inbox/message-status")
+    FeignResponseEntity<Void> updateMsgStatusTo(@RequestParam(value = "msgId") String msgId, @RequestParam(value = "msgStatus") String msgStatus);
+
+    @GetMapping(value = "/iais-inter-inbox/inbox/mask")
+    FeignResponseEntity<List<InboxMsgMaskDto>> getInboxMsgMask(@RequestParam(name = "msgId") String msgId);
+}
---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/LicFeInboxFallback.java
@@ -0,0 +1,59 @@
+package com.ecquaria.cloud.moh.iais.service.client;
+
+import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxMsgMaskDto;
+import com.ecquaria.cloud.moh.iais.common.dto.inbox.InboxQueryDto;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author: Hc
+ * @Program: iais-egp
+ * @Create: 2019-11-26 09:57
+ **/
+@Component
+public class LicFeInboxFallback implements LicFeInboxClient {
+    @Override
+    public FeignResponseEntity<SearchResult<InboxQueryDto>> searchInbox(SearchParam searchParam) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<Integer> searchUnreadAndUnresponseNum(String userId) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<Boolean> updateMsgStatusToArchive(String[] msgIds) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<Void> updateMsgStatusTo(String msgId, String msgStatus) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<List<InboxMsgMaskDto>> getInboxMsgMask(String msgId) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+}
