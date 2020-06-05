561f99c395a2d742f003e1b7befc73691b52e5fa
junyu <junyu@ecquaria.com>
2020-06-05 16:18:37 +0800

    enquiries

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/InspectReviseNcEmailDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/InspectReviseNcEmailDelegator.java
@@ -461,7 +461,7 @@ public class InspectReviseNcEmailDelegator {
         inspectionEmailTemplateDto.setApplicantName(licenseeName);
         inspectionEmailTemplateDto.setApplicationNumber(appNo);
         inspectionEmailTemplateDto.setHciCode(applicationViewDto.getHciCode());
-        inspectionEmailTemplateDto.setHciNameOrAddress(applicationViewDto.getHciAddress());
+        inspectionEmailTemplateDto.setHciNameOrAddress(applicationViewDto.getHciName()+"/"+applicationViewDto.getHciAddress());
         HcsaServiceDto hcsaServiceDto=inspectionService.getHcsaServiceDtoByServiceId(applicationViewDto.getApplicationDto().getServiceId());
         inspectionEmailTemplateDto.setServiceName(hcsaServiceDto.getSvcName());
         List<NcAnswerDto> ncAnswerDtos=insepctionNcCheckListService.getNcAnswerDtoList(correlationId);
