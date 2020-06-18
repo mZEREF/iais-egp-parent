e54ea86ff9a31441da244f252a924420e7f898b5
ShiCheng_Xu <shicheng@ecquaria.com>
2020-06-18 14:46:09 +0800

    new code

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ApptConfirmReSchDateDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ApptConfirmReSchDateDelegator.java
@@ -1,7 +1,12 @@
 package com.ecquaria.cloud.moh.iais.action;
 
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
+import com.ecquaria.cloud.moh.iais.service.ApptConfirmReSchDateService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import sop.webflow.rt.api.BaseProcessClass;
 
 /**
  * @Process MohApptUserChooseDate
@@ -12,5 +17,23 @@ import lombok.extern.slf4j.Slf4j;
 @Delegator(value = "apptConfirmReSchDateDelegator")
 @Slf4j
 public class ApptConfirmReSchDateDelegator {
+    @Autowired
+    private ApptConfirmReSchDateService apptConfirmReSchDateService;
 
+    @Autowired
+    private ApptConfirmReSchDateDelegator(ApptConfirmReSchDateService apptConfirmReSchDateService){
+        this.apptConfirmReSchDateService = apptConfirmReSchDateService;
+    }
+
+    /**
+     * StartStep: userConfirmInspDateStart
+     *
+     * @param bpc
+     * @throws
+     */
+    public void userConfirmInspDateStart(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("the userConfirmInspDateStart start ...."));
+
+        AuditTrailHelper.auditFunction("Appointment Re-Scheduling Confirm date", "Appointment Re-Scheduling Confirm date");
+    }
 }
