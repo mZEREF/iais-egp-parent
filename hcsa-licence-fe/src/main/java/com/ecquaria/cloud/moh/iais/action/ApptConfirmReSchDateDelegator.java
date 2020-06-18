48e07a68a2efbb1422de557b888bc451578b1f40
ShiCheng_Xu <shicheng@ecquaria.com>
2020-06-18 14:24:15 +0800

    new code

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ApptConfirmReSchDateDelegator.java
@@ -0,0 +1,16 @@
+package com.ecquaria.cloud.moh.iais.action;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Process MohApptUserChooseDate
+ *
+ * @author Shicheng
+ * @date 2020/6/18 13:43
+ **/
+@Delegator(value = "apptConfirmReSchDateDelegator")
+@Slf4j
+public class ApptConfirmReSchDateDelegator {
+
+}
