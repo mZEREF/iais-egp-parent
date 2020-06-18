422853638f36f055ea775206900fd814302e0130
ShiCheng_Xu <shicheng@ecquaria.com>
2020-06-18 14:39:27 +0800

    new code

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/ApptConfirmReSchDateService.java
@@ -0,0 +1,8 @@
+package com.ecquaria.cloud.moh.iais.service;
+
+/**
+ * @author Shicheng
+ * @date 2020/6/18 14:38
+ **/
+public interface ApptConfirmReSchDateService {
+}
---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApptConfirmReSchDateServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ecquaria.cloud.moh.iais.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Shicheng
+ * @date 2020/6/18 14:38
+ **/
+@Service
+@Slf4j
+public class ApptConfirmReSchDateServiceImpl {
+}
