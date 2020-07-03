db725d0c5a358a9e2b4494c4be834323d4b217cb
Jinhua <jinhua@ecquaria.com>
2020-07-03 16:01:38 +0800

    Change EIC to rest template

--- be-main/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BeEicGatewayMainClient.java
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/service/client/BeEicGatewayMainClient.java
@@ -2,12 +2,11 @@ package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.inbox.InterMessageDto;
-import com.ecquaria.cloudfeign.FeignConfiguration;
+import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,21 +16,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author suocheng
  * @date 2019/12/14 17:33
  */
-@FeignClient(value = "eicgate", url="${iais.intra.gateway.url}", configuration = {FeignConfiguration.class},
-        fallback = BeEicGatewayMainClientFallback.class)
-public interface BeEicGatewayMainClient {
-     @RequestMapping(value = "/v1/iais-application/",method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE)
-    FeignResponseEntity<ApplicationDto> updateApplication(@RequestBody ApplicationDto applicationDto,
-                                                          @RequestHeader("date") String date,
-                                                          @RequestHeader("authorization") String authorization,
-                                                          @RequestHeader("date-Secondary") String dateSec,
-                                                          @RequestHeader("authorization-Secondary") String authorizationSec);
+public class BeEicGatewayMainClient {
+    @Value("${iais.intra.gateway.url}")
+    private String gateWayUrl;
+
+    public FeignResponseEntity<ApplicationDto> updateApplication(ApplicationDto applicationDto,
+                                                          String date, String authorization, String dateSec,
+                                                          String authorizationSec) {
+         return IaisEGPHelper.callEicGateway(gateWayUrl + "/v1/iais-application/", HttpMethod.PUT, applicationDto,
+                 MediaType.APPLICATION_JSON, date, authorization, dateSec, authorizationSec, ApplicationDto.class);
+     }
 
     @RequestMapping(value = "/v1/iais-inter-inbox-message/",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
-    FeignResponseEntity<InterMessageDto> saveInboxMessage(@RequestBody InterMessageDto interInboxDto,
-                                                          @RequestHeader("date") String date,
-                                                          @RequestHeader("authorization") String authorization,
-                                                          @RequestHeader("date-Secondary") String dateSec,
-                                                          @RequestHeader("authorization-Secondary") String authorizationSec);
+    public FeignResponseEntity<InterMessageDto> saveInboxMessage(InterMessageDto interInboxDto,
+                                                          String date, String authorization, String dateSec,
+                                                          String authorizationSec) {
+        return IaisEGPHelper.callEicGateway(gateWayUrl + "/v1/iais-inter-inbox-message/", HttpMethod.POST, interInboxDto,
+                MediaType.APPLICATION_JSON, date, authorization, dateSec, authorizationSec, InterMessageDto.class);
+    }
 
 }
