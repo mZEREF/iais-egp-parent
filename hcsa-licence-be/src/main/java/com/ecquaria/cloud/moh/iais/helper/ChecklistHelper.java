b8373ec494bc96ba210660d5555d91669d54b994
yichen <yichen_guo@ecquaria.com>
2020-07-06 13:48:27 +0800

    fix(65792):

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/helper/ChecklistHelper.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/helper/ChecklistHelper.java
@@ -1,29 +1,47 @@
 package com.ecquaria.cloud.moh.iais.helper;
 
+import com.ecquaria.cloud.helper.SpringContextHelper;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.constant.HcsaConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.checklist.HcsaChecklistConstants;
+import com.ecquaria.cloud.moh.iais.common.constant.systemadmin.MsgTemplateConstants;
+import com.ecquaria.cloud.moh.iais.common.dto.emailsms.EmailDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.checklist.ChecklistConfigDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.checklist.ConfigExcelItemDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaSvcStageWorkingGroupDto;
 import com.ecquaria.cloud.moh.iais.common.dto.message.ErrorMsgContent;
+import com.ecquaria.cloud.moh.iais.common.dto.organization.OrgUserDto;
+import com.ecquaria.cloud.moh.iais.common.dto.templates.MsgTemplateDto;
 import com.ecquaria.cloud.moh.iais.common.exception.IaisRuntimeException;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.constant.ChecklistConstant;
 import com.ecquaria.cloud.moh.iais.constant.IaisEGPConstant;
+import com.ecquaria.cloud.moh.iais.service.client.EmailClient;
+import com.ecquaria.cloud.moh.iais.service.client.HcsaConfigClient;
+import com.ecquaria.cloud.moh.iais.service.client.OrganizationClient;
+import com.ecquaria.sz.commons.util.MsgUtil;
+import freemarker.template.TemplateException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author yi chen
  * @Date:2020/6/9
  **/
 
+@Slf4j
 public final class ChecklistHelper {
-
     private ChecklistHelper(){}
 
     public static boolean validateFile(HttpServletRequest request, MultipartFile file){
@@ -117,6 +135,55 @@ public final class ChecklistHelper {
             for(String error : errorMsgContent.getErrorMsgList()){
                 String msg = MessageUtil.getMessageDesc(error);
                 errorMsgContent.getErrorMsgList().set(idx++, msg);
+            }
+        }
+    }
+
+    public static void sendModifiedChecklistEmailToAOStage(String serviceId, String applicationType, String mailSender, String licenseeId){
+        EmailHelper emailHelper = SpringContextHelper.getContext().getBean(EmailHelper.class);
+        EmailClient emailClient = SpringContextHelper.getContext().getBean(EmailClient.class);
+        HcsaConfigClient hcsaConfigClient = SpringContextHelper.getContext().getBean(HcsaConfigClient.class);
+        OrganizationClient organizationClient = SpringContextHelper.getContext().getBean(OrganizationClient.class);
+
+        if (emailHelper == null || emailClient == null || hcsaConfigClient == null || organizationClient == null){
+            log.info("can not find context bean");
+            return;
+        }
+
+        HcsaSvcStageWorkingGroupDto hcsaSvcStageWorkingGroupDto = new HcsaSvcStageWorkingGroupDto();
+        hcsaSvcStageWorkingGroupDto.setServiceId(serviceId);
+        hcsaSvcStageWorkingGroupDto.setStageId(HcsaConsts.ROUTING_STAGE_INS);
+        hcsaSvcStageWorkingGroupDto.setOrder(2);
+        hcsaSvcStageWorkingGroupDto.setType(applicationType);
+        HcsaSvcStageWorkingGroupDto dto = hcsaConfigClient.getHcsaSvcStageWorkingGroupDto(hcsaSvcStageWorkingGroupDto).getEntity();
+
+        if (dto != null){
+            List<OrgUserDto> orgUserDtos = organizationClient.getUsersByWorkGroupName(dto.getId(), AppConsts.COMMON_STATUS_ACTIVE).getEntity();
+            if (!IaisCommonUtils.isEmpty(orgUserDtos)){
+                List<String> receiveEmail = orgUserDtos.stream().map(OrgUserDto::getEmail).collect(Collectors.toList());
+
+                log.info("=====address====>>>", receiveEmail);
+
+                try {
+                    Map<String,Object> map = new HashMap(1);
+                    map.put("MOH_NAME", AppConsts.MOH_AGENCY_NAME);
+
+                    MsgTemplateDto msgTemplate = emailHelper.getMsgTemplate(MsgTemplateConstants.MSG_TEMPLATE_INSPECTOR_MODIFIED_CHECKLIST);
+                    String subject = msgTemplate.getTemplateName();
+                    String messageContent = msgTemplate.getMessageContent();
+                    String templateMessageByContent = MsgUtil.getTemplateMessageByContent(messageContent, map);
+
+                    EmailDto emailDto=new EmailDto();
+                    emailDto.setContent(templateMessageByContent);
+                    emailDto.setSubject(subject);
+                    emailDto.setSender(mailSender);
+                    emailDto.setReceipts(receiveEmail);
+                    emailClient.sendNotification(emailDto).getEntity();
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                } catch (TemplateException e) {
+                    log.error(e.getMessage(), e);
+                }
             }
         }
     }
