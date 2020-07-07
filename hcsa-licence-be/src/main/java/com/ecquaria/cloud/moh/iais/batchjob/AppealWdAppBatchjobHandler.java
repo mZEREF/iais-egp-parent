67749540454590e7ee70e3c5a90d2def23c37087
HuaChong <chong_hua@ecquaria.com>
2020-07-07 10:11:14 +0800

    withdrawal application

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealWdAppBatchjobHandler.java
@@ -0,0 +1,44 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+
+import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.job.executor.biz.model.ReturnT;
+import com.ecquaria.cloud.job.executor.handler.IJobHandler;
+import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
+import com.ecquaria.cloud.job.executor.log.JobLogger;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
+import com.ecquaria.cloud.moh.iais.service.ApplicationService;
+import com.ecquaria.cloud.moh.iais.service.client.ApplicationClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import sop.webflow.rt.api.BaseProcessClass;
+
+import java.util.List;
+
+@JobHandler(value="approveWithdrawalJobHandler")
+@Component
+@Slf4j
+public class AppealWdAppBatchjobHandler extends IJobHandler {
+
+    @Autowired
+    private ApplicationClient applicationClient;
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Override
+    public ReturnT<String> execute(String s) throws Exception {
+        try {
+            List<ApplicationDto> applicationDtoList = applicationClient.saveWithdrawn().getEntity();
+            applicationDtoList.forEach(h -> {
+                applicationService.updateFEApplicaiton(h);
+            });
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+            JobLogger.log(e);
+            return ReturnT.FAIL;
+        }
+        return ReturnT.SUCCESS;
+    }
+}
