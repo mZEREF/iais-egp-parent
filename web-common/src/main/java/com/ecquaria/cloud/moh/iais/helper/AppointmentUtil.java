2995f546f5550a3bc6fb51e825cc8eede3741c4d
junyu <junyu@ecquaria.com>
2020-07-13 17:00:36 +0800

    rfi

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppointmentUtil.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppointmentUtil.java
@@ -11,6 +11,19 @@ import com.ecquaria.cloud.moh.iais.common.constant.ApplicationConsts;
 public class AppointmentUtil {
     public static String[] getNoReschdulingAppStatus(){
 
-        return new String[]{ApplicationConsts.APPLICATION_STATUS_REQUEST_INFORMATION,ApplicationConsts.APPLICATION_STATUS_REQUEST_INFORMATION_REPLY,ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_AO,ApplicationConsts.APPLICATION_STATUS_PSO_ROUTE_BACK,ApplicationConsts.APPLICATION_STATUS_INSPECTOR_ROUTE_BACK,ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_ASO,ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_PSO,ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_INSPECTOR,ApplicationConsts.APPLICATION_STATUS_RE_SCHEDULING_COMMON_POOL};
+        return new String[]{
+                ApplicationConsts.APPLICATION_STATUS_REQUEST_INFORMATION,
+                ApplicationConsts.APPLICATION_STATUS_REQUEST_INFORMATION_REPLY,
+                ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_AO,
+                ApplicationConsts.APPLICATION_STATUS_PSO_ROUTE_BACK,
+                ApplicationConsts.APPLICATION_STATUS_INSPECTOR_ROUTE_BACK,
+                ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_ASO,
+                ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_PSO,
+                ApplicationConsts.APPLICATION_STATUS_AO_ROUTE_BACK_INSPECTOR,
+                ApplicationConsts.APPLICATION_STATUS_RE_SCHEDULING_COMMON_POOL,
+                ApplicationConsts.APPLICATION_STATUS_RE_SCHEDULING_APPLICANT,
+                ApplicationConsts.APPLICATION_STATUS_OFFICER_RESCHEDULING_APPLICANT,
+                ApplicationConsts.APPLICATION_STATUS_OFFICER_RESCHEDULING_PENDING_FE
+        };
     }
 }
