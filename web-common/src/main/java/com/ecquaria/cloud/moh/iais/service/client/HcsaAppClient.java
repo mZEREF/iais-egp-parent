8f728a4403f075964cb4f5cc7e13f8e97c52b4f6
Jinhua <jinhua@ecquaria.com>
2020-07-14 14:10:37 +0800

    For Email helper

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/HcsaAppClient.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/HcsaAppClient.java
@@ -1,8 +1,10 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesRoutingHistoryDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationGroupDto;
 import com.ecquaria.cloudfeign.FeignConfiguration;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,9 @@ public interface HcsaAppClient {
     @GetMapping(value = "/hcsa-app-common/app-grp/{appGroupId}",produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<ApplicationGroupDto> getAppGrpById(@PathVariable("appGroupId") String appGroupId);
 
-    @GetMapping(value = "/hcsa-app-common/app-grp-appId/{appId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    FeignResponseEntity<ApplicationGroupDto> getAppGrpByAppId(@PathVariable("appId") String appId);
+    @GetMapping(value = "/hcsa-app-common/app-grp-appNo/{appNo}",produces = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<ApplicationGroupDto> getAppGrpByAppNo(@PathVariable("appNo") String appNo);
+
+    @GetMapping(value = "/iais-application-history/appPremisesRoutingHistorys/{appNo}",produces = MediaType.APPLICATION_JSON_VALUE)
+    FeignResponseEntity<List<AppPremisesRoutingHistoryDto>> getAppPremisesRoutingHistorysByAppNo(@PathVariable("appNo") String appNo);
 }
