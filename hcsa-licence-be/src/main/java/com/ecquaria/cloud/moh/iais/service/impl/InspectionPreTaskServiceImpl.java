7cff938a3915a860e72d41cc9c6d71c8dd50f595
ShiCheng_Xu <shicheng@ecquaria.com>
2020-07-20 14:26:00 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionPreTaskServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionPreTaskServiceImpl.java
@@ -530,6 +530,7 @@ public class InspectionPreTaskServiceImpl implements InspectionPreTaskService {
                     if(premisesStr != null && premisesStr2 != null) {
                         if (premisesStr.equals(premisesStr2)) {
                             hicCode = premisesDto.getHciCode();
+                            return hicCode;
                         }
                     }
                 }
