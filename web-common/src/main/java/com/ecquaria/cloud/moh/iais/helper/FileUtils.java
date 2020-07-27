3d8acf47f38d9282bd549cfa7510764f7e6baf42
yichen <yichen_guo@ecquaria.com>
2020-07-27 15:18:27 +0800

    fix(65886):

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/FileUtils.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/FileUtils.java
@@ -20,7 +20,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.regex.Matcher;
 
 /**
  * @author: yichen
@@ -141,6 +140,7 @@ public final class FileUtils {
         if (StringUtils.isEmpty(str)){
             return null;
         }
+
 
         return str.split(",");
     }
