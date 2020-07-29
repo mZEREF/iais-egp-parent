6eb9864de6494b888a85c6024b951eadb6e9574e
yichen <yichen_guo@ecquaria.com>
2020-07-29 15:23:39 +0800

    fix(66345):

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/validation/AuditTrailDtoValidate.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/validation/AuditTrailDtoValidate.java
@@ -1,6 +1,7 @@
 package com.ecquaria.cloud.moh.iais.validation;
 
 
+import com.ecquaria.cloud.moh.iais.common.config.SystemParamConfig;
 import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.audit.AuditTrailConstants;
 import com.ecquaria.cloud.moh.iais.common.constant.message.MessageCodeKey;
@@ -10,17 +11,23 @@ import com.ecquaria.cloud.moh.iais.common.validation.interfaces.CustomizeValidat
 import com.ecquaria.cloud.moh.iais.constant.IaisEGPConstant;
 import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
 import com.ecquaria.cloud.moh.iais.helper.MessageUtil;
-import com.ecquaria.cloud.moh.iais.helper.SystemParamCacheHelper;
-import java.util.Date;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Map;
 
 
 @Slf4j
+@Component
 public class AuditTrailDtoValidate implements CustomizeValidator {
-    private static final int MONTH_DAY = 30;
+    private static final int MONTH_DAY = 7;
+
+    @Autowired
+	private SystemParamConfig systemParamConfig;
 
 	@Override
     public Map<String, String> validate(HttpServletRequest request) {
@@ -37,13 +44,12 @@ public class AuditTrailDtoValidate implements CustomizeValidator {
 					int reduceDay = getReduceDay(IaisEGPHelper.parseToDate(startDate, AppConsts.DEFAULT_DATE_FORMAT),
 							IaisEGPHelper.parseToDate(endDate, AppConsts.DEFAULT_DATE_FORMAT));
 
-					String value = SystemParamCacheHelper.getParamValueById(SystemParamCacheHelper.AUDIT_TRAIL_TIME_LIMIT);
-					String postValue = value == null ? "3" : value;
-
+					int value = systemParamConfig.getAuditTrailSearchWeek();
+					log.info("value" + value);
 					String msg = MessageUtil.getMessageDesc("GENERAL_ERR0010");
 
-					if ((reduceDay > (Integer.parseInt(postValue) * MONTH_DAY))){
-						errMap.put("actionTime", MessageUtil.formatMessage(msg, postValue));
+					if ((reduceDay >= (value * MONTH_DAY))){
+						errMap.put("actionTime", MessageUtil.formatMessage(msg, String.valueOf(value)));
 					}
 				}
 				break;
