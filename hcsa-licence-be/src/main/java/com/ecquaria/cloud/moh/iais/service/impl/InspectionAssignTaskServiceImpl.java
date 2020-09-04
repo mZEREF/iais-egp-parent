c4a41169199c7c51ea0654df76bd20d17d5f4d9f
ShiCheng_Xu <shicheng@ecquaria.com>
2020-09-04 09:43:39 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionAssignTaskServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InspectionAssignTaskServiceImpl.java
@@ -510,6 +510,10 @@ public class InspectionAssignTaskServiceImpl implements InspectionAssignTaskServ
                     if (inspectorCheckList != null && inspectorCheckList.size() > 0) {
                         for (int i = 0; i < inspectorCheckList.size(); i++) {
                             if (ApplicationConsts.APPLICATION_STATUS_PENDING_TASK_ASSIGNMENT.equals(applicationDto.getStatus())) {
+                                String fastTrack = inspecTaskCreAndAssDto.getFastTrackCheck();
+                                if(!StringUtil.isEmpty(fastTrack)){
+                                    applicationDto.setFastTracking(true);
+                                }
                                 ApplicationDto applicationDto1 = updateApplication(applicationDto, ApplicationConsts.APPLICATION_STATUS_PENDING_APPOINTMENT_SCHEDULING);
                                 applicationService.updateFEApplicaiton(applicationDto1);
                                 inspecTaskCreAndAssDto.setApplicationStatus(applicationDto1.getStatus());
