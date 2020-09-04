f75ce099c87e1d4cdc27718ba8b553ada5650920
Jinhua <jinhua@ecquaria.com>
2020-09-04 15:26:13 +0800

    Change menu

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/listener/IaisSessionListener.java
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/listener/IaisSessionListener.java
@@ -1,9 +1,13 @@
 package com.ecquaria.cloud.moh.iais.listener;
 
-import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.session.events.SessionCreatedEvent;
+import org.springframework.session.events.SessionDeletedEvent;
+import org.springframework.session.events.SessionExpiredEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * IaisSessionListener
@@ -11,17 +15,25 @@ import lombok.extern.slf4j.Slf4j;
  * @author Jinhua
  * @date 2020/4/3 15:31
  */
-@WebListener
+@Component
 @Slf4j
-public class IaisSessionListener implements HttpSessionListener {
-
-    @Override
-    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
-        log.info("<======= Session created =======>");
+public class IaisSessionListener {
+    @EventListener(SessionCreatedEvent.class)
+    @Async
+    public void sessionCreatedEvent(SessionCreatedEvent sessionEvent) {
+        log.info(StringUtil.changeForLog("<==== Session created ====> "
+                + sessionEvent.getSession().getId()));
     }
 
-    @Override
-    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-        log.info("<======= Session destroyed =======>");
+    @EventListener(SessionExpiredEvent.class)
+    public void sessionExpiredEvent(SessionExpiredEvent sessionEvent) {
+        log.info(StringUtil.changeForLog("<==== Session timeout ====> "
+                + sessionEvent.getSession().getId()));
     }
+
+    @EventListener(SessionDeletedEvent.class)
+    public void sessionDeletedEvent(SessionDeletedEvent sessionEvent) {
+
+    }
+
 }
