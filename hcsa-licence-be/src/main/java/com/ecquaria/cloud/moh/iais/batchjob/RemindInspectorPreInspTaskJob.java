9d83e82f9c43b83bf33ae0bb106edf9368ee787d
ShiCheng_Xu <shicheng@ecquaria.com>
2020-09-09 17:24:30 +0800

    new code

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/RemindInspectorPreInspTaskJob.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/RemindInspectorPreInspTaskJob.java
@@ -3,29 +3,42 @@ package com.ecquaria.cloud.moh.iais.batchjob;
 import com.ecquaria.cloud.annotation.Delegator;
 import com.ecquaria.cloud.job.executor.log.JobLogger;
 import com.ecquaria.cloud.moh.iais.common.config.SystemParamConfig;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.ApplicationConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.inspection.InspectionConstants;
 import com.ecquaria.cloud.moh.iais.common.constant.systemadmin.MsgTemplateConstants;
+import com.ecquaria.cloud.moh.iais.common.dto.emailsms.EmailDto;
+import com.ecquaria.cloud.moh.iais.common.dto.emailsms.SmsDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppGrpPremisesDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesCorrelationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppPremisesRecommendationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.ApplicationGroupDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaServiceDto;
+import com.ecquaria.cloud.moh.iais.common.dto.organization.OrgUserDto;
+import com.ecquaria.cloud.moh.iais.common.dto.task.TaskDto;
+import com.ecquaria.cloud.moh.iais.common.dto.templates.MsgTemplateDto;
+import com.ecquaria.cloud.moh.iais.common.exception.IaisRuntimeException;
 import com.ecquaria.cloud.moh.iais.common.utils.Formatter;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
+import com.ecquaria.cloud.moh.iais.common.utils.MessageTemplateUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.MiscUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
-import com.ecquaria.cloud.moh.iais.dto.EmailParam;
-import com.ecquaria.cloud.moh.iais.helper.NotificationHelper;
 import com.ecquaria.cloud.moh.iais.service.InspectionAssignTaskService;
 import com.ecquaria.cloud.moh.iais.service.client.ApplicationClient;
+import com.ecquaria.cloud.moh.iais.service.client.EmailClient;
 import com.ecquaria.cloud.moh.iais.service.client.FillUpCheckListGetAppClient;
 import com.ecquaria.cloud.moh.iais.service.client.HcsaConfigClient;
+import com.ecquaria.cloud.moh.iais.service.client.MsgTemplateClient;
+import com.ecquaria.cloud.moh.iais.service.client.OrganizationClient;
+import com.ecquaria.sz.commons.util.MsgUtil;
+import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import sop.webflow.rt.api.BaseProcessClass;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -43,8 +56,11 @@ public class RemindInspectorPreInspTaskJob {
     @Autowired
     private InspectionAssignTaskService inspectionAssignTaskService;
 
+    @Value("${iais.email.sender}")
+    private String mailSender;
+
     @Autowired
-    private NotificationHelper notificationHelper;
+    private OrganizationClient organizationClient;
 
     @Autowired
     private ApplicationClient applicationClient;
@@ -53,10 +69,16 @@ public class RemindInspectorPreInspTaskJob {
     private HcsaConfigClient hcsaConfigClient;
 
     @Autowired
+    private EmailClient emailClient;
+
+    @Autowired
     private SystemParamConfig systemParamConfig;
 
     @Autowired
     private FillUpCheckListGetAppClient fillUpCheckListGetAppClient;
+
+    @Autowired
+    private MsgTemplateClient msgTemplateClient;
 
     /**
      * StartStep: remindDoPreInspTaskStart
@@ -96,6 +118,8 @@ public class RemindInspectorPreInspTaskJob {
         log.info(StringUtil.changeForLog("System Inspection days = " + days));
         JobLogger.log(StringUtil.changeForLog("System Inspection days = " + days));
         String appNo = applicationDto.getApplicationNo();
+        log.info(StringUtil.changeForLog("Remind ApplicationNo = " + appNo));
+        JobLogger.log(StringUtil.changeForLog("Remind ApplicationNo = " + appNo));
         AppPremisesCorrelationDto appPremisesCorrelationDto = applicationClient.getAppPremCorrByAppNo(appNo).getEntity();
         String appPremCorrId = appPremisesCorrelationDto.getId();
         AppPremisesRecommendationDto appPremisesRecommendationDto = fillUpCheckListGetAppClient.getAppPremRecordByIdAndType(appPremCorrId, InspectionConstants.RECOM_TYPE_INSEPCTION_DATE).getEntity();
@@ -109,22 +133,116 @@ public class RemindInspectorPreInspTaskJob {
             JobLogger.log(StringUtil.changeForLog("jobRemindMsgTrackingDto2 not null, nowDays = " + nowDays));
             if(days == nowDays){
                 Map<String, Object> templateMap = getEmailField(applicationDto, appPremisesCorrelationDto);
-                EmailParam emailParam = new EmailParam();
-                emailParam.setTemplateId(MsgTemplateConstants.MSG_TEMPLATE_REMIND_INSPECTOR_PRE_INSP_READY);
-                emailParam.setTemplateContent(templateMap);
-                emailParam.setQueryCode(appNo);
-                emailParam.setReqRefNum(appNo);
-                emailParam.setRefIdType(NotificationHelper.RECEIPT_TYPE_APP);
-                emailParam.setRefId(appNo);
-                notificationHelper.sendNotification(emailParam);
-                EmailParam smsParam = new EmailParam();
-                smsParam.setTemplateId(MsgTemplateConstants.MSG_TEMPLATE_REMIND_INSPECTOR_PRE_INSP_READY);
-                smsParam.setQueryCode(appNo);
-                smsParam.setReqRefNum(appNo);
-                smsParam.setRefIdType(NotificationHelper.RECEIPT_TYPE_SMS_APP);
-                smsParam.setRefId(appNo);
-                smsParam.setSubject("MOH HALP - [Internal] Inspection Task not ready for inspection");
-                notificationHelper.sendNotification(smsParam);
+                List<TaskDto> taskDtos = organizationClient.getCurrTaskByRefNo(appPremCorrId).getEntity();
+                if(!IaisCommonUtils.isEmpty(taskDtos)){//NOSONAR
+                    for(TaskDto taskDto : taskDtos){
+                        if(taskDto != null) {
+                            //send email and sms
+                            sendEmailAndSmsByTask(templateMap, taskDto);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void sendEmailAndSmsByTask(Map<String, Object> templateMap, TaskDto taskDto) {
+        MsgTemplateDto msgTemplateDto = msgTemplateClient.getMsgTemplate(MsgTemplateConstants.MSG_TEMPLATE_REMIND_INSPECTOR_PRE_INSP_READY).getEntity();
+        if(AppConsts.COMMON_STATUS_IACTIVE.equals(msgTemplateDto.getStatus())){
+            log.info("msgTemplateDto is INACTIVE.......");
+            JobLogger.log(StringUtil.changeForLog("msgTemplateDto is INACTIVE......."));
+        } else {
+            if (msgTemplateDto != null) {
+                String userId = taskDto.getUserId();
+                String workGrpId = taskDto.getWkGrpId();
+                List<String> leads = organizationClient.getInspectionLead(workGrpId).getEntity();
+                if (!StringUtil.isEmpty(userId)) {//NOSONAR
+                    OrgUserDto orgUserDto = organizationClient.retrieveOrgUserAccountById(userId).getEntity();
+                    if (orgUserDto != null) {
+                        sendEmail(orgUserDto, msgTemplateDto, taskDto, templateMap);
+                        sendSms(orgUserDto, taskDto);
+                    }
+                }
+                if (!IaisCommonUtils.isEmpty(leads)) {//NOSONAR
+                    for (String lead : leads) {
+                        if (!StringUtil.isEmpty(lead) && !lead.equals(userId)) {//NOSONAR
+                            OrgUserDto orgUserDto = organizationClient.retrieveOrgUserAccountById(lead).getEntity();
+                            if (orgUserDto != null) {
+                                sendEmail(orgUserDto, msgTemplateDto, taskDto, templateMap);
+                                sendSms(orgUserDto, taskDto);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void sendSms(OrgUserDto orgUserDto, TaskDto taskDto) {
+        int smsFlag = systemParamConfig.getEgpSmsNotifications();
+        if (0 == smsFlag) {
+            log.info("please turn on sms param.......");
+            JobLogger.log(StringUtil.changeForLog("please turn on sms param......."));
+        } else {
+            List<String> mobile = IaisCommonUtils.genNewArrayList();
+            String phoneNo = orgUserDto.getMobileNo();
+            if(!StringUtil.isEmpty(phoneNo)) {
+                mobile.add(phoneNo);
+            }
+            if (!IaisCommonUtils.isEmpty(mobile)) {
+                SmsDto smsDto = new SmsDto();
+                smsDto.setSender(mailSender);
+                smsDto.setContent("MOH HALP - [Internal] Inspection Task not ready for inspection");
+                smsDto.setOnlyOfficeHour(true);
+                smsDto.setReceipts(mobile);
+                smsDto.setReqRefNum(taskDto.getApplicationNo());
+                emailClient.sendSMS(mobile, smsDto, taskDto.getApplicationNo());
+            } else {
+                log.info("mobile is null.......");
+                JobLogger.log(StringUtil.changeForLog("mobile is null......."));
+            }
+        }
+    }
+
+    private void sendEmail(OrgUserDto orgUserDto, MsgTemplateDto msgTemplateDto, TaskDto taskDto, Map<String, Object> templateMap) {
+        int emailFlag = systemParamConfig.getEgpEmailNotifications();
+        if (0 == emailFlag) {
+            log.info("please turn on email param.......");
+            JobLogger.log(StringUtil.changeForLog("please turn on email param......."));
+        } else {
+            List<String> receiptEmail = IaisCommonUtils.genNewArrayList();
+            String emailAddress = orgUserDto.getEmail();
+            if(!StringUtil.isEmpty(emailAddress)) {
+                receiptEmail.add(emailAddress);
+            }
+            templateMap.put("officer_name", orgUserDto.getDisplayName());
+            String emailTemplate = msgTemplateDto.getMessageContent();
+            //replace num
+            emailTemplate = MessageTemplateUtil.replaceNum(emailTemplate);
+            //get mesContext
+            String mesContext;
+            if (templateMap != null && !templateMap.isEmpty()) {
+                try {
+                    mesContext = MsgUtil.getTemplateMessageByContent(emailTemplate, templateMap);
+                } catch (IOException | TemplateException e) {
+                    log.error(e.getMessage(), e);
+                    throw new IaisRuntimeException(e);
+                }
+            } else {
+                mesContext = emailTemplate;
+            }
+            if(!IaisCommonUtils.isEmpty(receiptEmail)) {
+                EmailDto emailDto = new EmailDto();
+                emailDto.setContent(mesContext);
+                emailDto.setSubject(msgTemplateDto.getTemplateName());
+                emailDto.setSender(mailSender);
+                emailDto.setReceipts(receiptEmail);
+                emailDto.setClientQueryCode(taskDto.getRefNo());
+                emailDto.setReqRefNum(taskDto.getRefNo());
+                emailClient.sendNotification(emailDto);
+            } else {
+                log.info("receiptEmail is null.......");
+                JobLogger.log(StringUtil.changeForLog("receiptEmail is null......."));
             }
         }
     }
