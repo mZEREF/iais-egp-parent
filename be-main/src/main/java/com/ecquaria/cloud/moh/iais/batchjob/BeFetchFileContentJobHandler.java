56b2897fe7d10ae42c134b119c380b33d2c7bc9f
Jinhua <jinhua@ecquaria.com>
2020-09-23 15:49:32 +0800

    Add eventbus compersite function

---
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/BeFetchFileContentJobHandler.java
@@ -0,0 +1,40 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.job.executor.biz.model.ReturnT;
+import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
+import com.ecquaria.cloud.job.executor.log.JobLogger;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
+import com.ecquaria.cloud.moh.iais.service.client.BeMainFileRepoClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * BeFetchFileContentJobHandler
+ *
+ * @author Jinhua
+ * @date 2020/9/23 14:43
+ */
+@Component
+@JobHandler(value="beFetchFileContentJobHandler")
+@Slf4j
+public class BeFetchFileContentJobHandler extends MohJobHandler {
+    @Autowired
+    private BeMainFileRepoClient beMainFileRepoClient;
+
+    @Override
+    public ReturnT<String> doExecute(String str) throws Exception {
+        try {
+            AuditTrailDto.setThreadDto(AuditTrailHelper.getBatchJobDto(AppConsts.USER_DOMAIN_INTRANET));
+            beMainFileRepoClient.fetchFileContent();
+        } catch (Throwable th) {
+            log.error(th.getMessage(), th);
+            JobLogger.log(th);
+            return ReturnT.FAIL;
+        }
+
+        return ReturnT.SUCCESS;
+    }
+}
---
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/FeFetchFileContentJobHandler.java
@@ -0,0 +1,40 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.job.executor.biz.model.ReturnT;
+import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
+import com.ecquaria.cloud.job.executor.log.JobLogger;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
+import com.ecquaria.cloud.moh.iais.service.client.FeMainFileRepoClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * FeMainFileRepoClient
+ *
+ * @author Jinhua
+ * @date 2020/9/23 15:38
+ */
+@Component
+@JobHandler(value="feFetchFileContentJobHandler")
+@Slf4j
+public class FeFetchFileContentJobHandler extends MohJobHandler {
+    @Autowired
+    private FeMainFileRepoClient feMainFileRepoClient;
+
+    @Override
+    public ReturnT<String> doExecute(String str) throws Exception {
+        try {
+            AuditTrailDto.setThreadDto(AuditTrailHelper.getBatchJobDto(AppConsts.USER_DOMAIN_INTERNET));
+            feMainFileRepoClient.fetchFileContent();
+        } catch (Throwable th) {
+            log.error(th.getMessage(), th);
+            JobLogger.log(th);
+            return ReturnT.FAIL;
+        }
+
+        return ReturnT.SUCCESS;
+    }
+}
