2ed17b845e53fbf5a1b017e61e2cccffe6ba5f9d
suocheng <suocheng@ecquaria.com>
2020-09-23 10:16:51 +0800

    batchjob Handler

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealApprovejobHandler.java
@@ -0,0 +1,35 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.job.executor.biz.model.ReturnT;
+import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
+import com.ecquaria.cloud.job.executor.log.JobLogger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * AppealApprovejobHandler
+ *
+ * @author suocheng
+ * @date 9/23/2020
+ */
+@JobHandler(value="appealApprovejobHandler")
+@Component
+@Slf4j
+public class AppealApprovejobHandler extends MohJobHandler{
+
+    @Autowired
+    private AppealApproveBatchjob appealApproveBatchjob;
+
+    @Override
+    public ReturnT<String> doExecute(String var1) throws Exception {
+        try {
+            appealApproveBatchjob.jobExecute();
+            return  ReturnT.SUCCESS;
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+            JobLogger.log(e);
+            return ReturnT.FAIL;
+        }
+    }
+}
