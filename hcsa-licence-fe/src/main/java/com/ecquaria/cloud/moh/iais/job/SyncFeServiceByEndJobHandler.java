67c47de7c5c3e4e4df68230d8206a362566e7129
ShiCheng_Xu <shicheng@ecquaria.com>
2020-09-25 15:53:38 +0800

    new code

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/job/SyncFeServiceByEndJobHandler.java
@@ -0,0 +1,71 @@
+package com.ecquaria.cloud.moh.iais.job;
+
+import com.ecquaria.cloud.job.executor.biz.model.ReturnT;
+import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
+import com.ecquaria.cloud.job.executor.log.JobLogger;
+import com.ecquaria.cloud.moh.iais.batchjob.MohJobHandler;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.serviceconfig.HcsaServiceDto;
+import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import com.ecquaria.cloud.moh.iais.helper.HcsaServiceCacheHelper;
+import com.ecquaria.cloud.moh.iais.service.client.HcsaConfigFeClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author Shicheng
+ * @date 2020/9/25 9:45
+ **/
+@JobHandler(value="syncServiceByEndJobHandler")
+@Component
+@Slf4j
+public class SyncFeServiceByEndJobHandler extends MohJobHandler {
+
+    @Autowired
+    private HcsaConfigFeClient hcsaConfigFeClient;
+
+    @Override
+    public ReturnT<String> doExecute(String var1) {
+        try {
+            logAbout("SyncServiceByEndJobHandler");
+            //get expire Service By End Date
+            List<HcsaServiceDto> hcsaServiceDtos = hcsaConfigFeClient.getNeedInActiveServices(AppConsts.COMMON_STATUS_ACTIVE).getEntity();
+            if(!IaisCommonUtils.isEmpty(hcsaServiceDtos)){//NOSONAR
+                List<HcsaServiceDto> updateServiceList = IaisCommonUtils.genNewArrayList();
+                for(HcsaServiceDto hcsaServiceDto : hcsaServiceDtos){
+                    log.info(StringUtil.changeForLog("hcsaServiceDto Id = " + hcsaServiceDto.getId()));
+                    JobLogger.log(StringUtil.changeForLog("hcsaServiceDto Id = " + hcsaServiceDto.getId()));
+                    if(hcsaServiceDto != null){
+                        try {
+                            hcsaServiceDto.setStatus(AppConsts.COMMON_STATUS_IACTIVE);
+                            updateServiceList.add(hcsaServiceDto);
+                        } catch (Exception e) {
+                            log.error(e.getMessage(), e);
+                            JobLogger.log(e);
+                            continue;
+                        }
+                    }
+                }
+                hcsaConfigFeClient.saveServiceList(updateServiceList);
+                HcsaServiceCacheHelper.receiveServiceMapping();
+            } else {
+                log.info(StringUtil.changeForLog("hcsaServiceDtos is Null"));
+                JobLogger.log(StringUtil.changeForLog("hcsaServiceDtos is Null"));
+            }
+            return ReturnT.SUCCESS;
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+            JobLogger.log(e);
+            return ReturnT.FAIL;
+        }
+    }
+
+    private void logAbout(String methodName){
+        log.info(StringUtil.changeForLog("****The****" + methodName + "*****Start****"));
+        JobLogger.log(StringUtil.changeForLog("****The****" + methodName + "*****Start****"));
+    }
+}
