d362abc36bd2bbb84df9f7817a51c336f7d0121c
ShiCheng_Xu <shicheng@ecquaria.com>
2020-10-19 13:18:08 +0800

    new code

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/GiroDeductionBeDelegator.java
@@ -0,0 +1,15 @@
+package com.ecquaria.cloud.moh.iais.action;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Process: MohBeGiroDeduction
+ *
+ * @author Shicheng
+ * @date 2020/10/19 10:31
+ **/
+@Delegator(value = "giroDeductionBeDelegator")
+@Slf4j
+public class GiroDeductionBeDelegator {
+}
