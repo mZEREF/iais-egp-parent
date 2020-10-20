4083242d51f91c86ae02d9753343bd56b4d1e050
yichen <yichen_guo@ecquaria.com>
2020-10-20 17:14:46 +0800

    error message

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/initializer/FeMainInitializer.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/initializer/FeMainInitializer.java
@@ -48,9 +48,6 @@ public class FeMainInitializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext())
-                .getAutowireCapableBeanFactory().autowireBean(this);
-
         copySpConfigProperties();
     }
 
