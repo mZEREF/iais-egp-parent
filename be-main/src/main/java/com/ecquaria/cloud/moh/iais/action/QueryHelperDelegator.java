dcd1a1afb9e811a1038d2f4ae51d4d1a4aacb0c7
zhilin <lzl19980518>
2020-10-23 09:25:31 +0800

    query helper

---
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/action/QueryHelperDelegator.java
@@ -0,0 +1,51 @@
+package com.ecquaria.cloud.moh.iais.action;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import sop.webflow.rt.api.BaseProcessClass;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * HcsaApplicationDelegator
+ *
+ * @author zhilin
+ * @date 10/22/2020
+ */
+@Delegator("queryHelperDelegator")
+@Slf4j
+public class QueryHelperDelegator {
+    private final String PasswordCode = "WOWAFKLKJHGF";
+    public void start(BaseProcessClass bpc){
+        log.debug(StringUtil.changeForLog("queryHelperDelegator do cleanSession start ...."));
+    }
+
+    public void prepareLogin(BaseProcessClass bpc){
+
+    }
+
+    public void doLogin(BaseProcessClass bpc){
+        String queryHelperPassword = ParamUtil.getString(bpc.request, "queryHelperPassword");
+        String flag = "N";
+        if(getCurrentPassword().equals(queryHelperPassword)){
+            flag = "Y";
+        }
+        ParamUtil.setRequestAttr(bpc.request, "crud_action_type", flag);
+    }
+
+    public void prepareQuery(BaseProcessClass bpc){
+
+    }
+
+    private String getCurrentPassword(){
+        Calendar calendar = Calendar.getInstance();//日历对象
+        calendar.setTime(new Date());
+        int day = calendar.get(Calendar.DATE);
+        String dayStr = day < 10 ? "0" + day : day + "";
+        String currentPassword = PasswordCode + dayStr;
+        return currentPassword;
+    }
+}
