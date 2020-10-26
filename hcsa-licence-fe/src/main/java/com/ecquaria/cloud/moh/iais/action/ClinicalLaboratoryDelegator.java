2ccba66f064185808261f7a5c5428f1c72140bd1
zixian <zixian@ecquaria.com>
2020-10-26 14:31:52 +0800

    fix sonar issue

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClinicalLaboratoryDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClinicalLaboratoryDelegator.java
@@ -1124,6 +1124,7 @@ public class ClinicalLaboratoryDelegator {
                 for(AppSvcRelatedInfoDto oldSvcRelDto:oldAppSvcRelatedInfoDtos){
                     if(currentSvcId.equals(oldSvcRelDto.getServiceId())){
                         oldDocs = oldSvcRelDto.getAppSvcDocDtoLit();
+                        break;
                     }
                 }
             }
