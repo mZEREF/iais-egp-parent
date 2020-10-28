9a3fc8355fb1400da3d87a138f257c3fcd346e5a
Jinhua <jinhua@ecquaria.com>
2020-10-28 14:27:47 +0800

    Change DSS

---
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/action/HalpAuthorisationHandler.java
@@ -0,0 +1,20 @@
+package com.ecquaria.cloud.moh.iais.action;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import sop.security.DefaultAuthorisationHandler;
+import sop.webflow.rt.engine5.SOPEngineRequest;
+
+/**
+ * HalpAuthorisationHandler
+ *
+ * @author Jinhua
+ * @date 2020/10/28 10:54
+ */
+@Slf4j
+public class HalpAuthorisationHandler extends DefaultAuthorisationHandler {
+    @Override
+    public void onAuthorisationFailure(HttpServletRequest request, SOPEngineRequest sopReq) {
+        log.info("<==== HALP auth ====>");
+    }
+}
