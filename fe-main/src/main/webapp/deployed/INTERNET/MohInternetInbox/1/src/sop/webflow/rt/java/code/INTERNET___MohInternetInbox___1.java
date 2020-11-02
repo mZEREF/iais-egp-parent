93966ac9812963b28bec29c4de596fb626622d60
HuaChong <chong_hua@ecquaria.com>
2020-11-02 13:36:16 +0800

    Inter Inbox

--- fe-main/src/main/webapp/deployed/INTERNET/MohInternetInbox/1/src/sop/webflow/rt/java/code/INTERNET___MohInternetInbox___1.java
+++ fe-main/src/main/webapp/deployed/INTERNET/MohInternetInbox/1/src/sop/webflow/rt/java/code/INTERNET___MohInternetInbox___1.java
@@ -176,7 +176,9 @@ public class INTERNET___MohInternetInbox___1 extends BaseProcessClass {
 		EngineHelper.delegate(DELEGATOR, "licDoPrint", this);
 	}
 
-	
-	
+	public void doSelfAssMt_OnStepProcess_0() throws Exception {
+		EngineHelper.delegate(DELEGATOR, "doSelfAssMt", this);
+	}
+
 
 }
