2fbc4f09cac781328ef59b9d11fbd71631c8bd72
junyu <junyu@ecquaria.com>
2020-11-06 15:59:06 +0800

    Nets

--- payment/src/main/java/com/ecquaria/egp/core/payment/runtime/PaymentNetsProxy.java
+++ payment/src/main/java/com/ecquaria/egp/core/payment/runtime/PaymentNetsProxy.java
@@ -95,7 +95,7 @@ public class PaymentNetsProxy extends PaymentProxy {
 		String keyId=GatewayConfig.eNetsKeyId;
 		String secretKey=GatewayConfig.eNetsSecretKey ;
 		String s2sUrl=AppConsts.REQUEST_TYPE_HTTPS + bpc.request.getServerName()+bpc.request.getContextPath()+"/s2sTxnEnd";
-		String b2sUrl=AppConsts.REQUEST_TYPE_HTTPS + bpc.request.getServerName()+"/paycallback";
+		String b2sUrl=AppConsts.REQUEST_TYPE_HTTPS + bpc.request.getServerName()+"/payment-web";
 //		String b2sUrl=AppConsts.REQUEST_TYPE_HTTPS + bpc.request.getServerName()+"/paycallback?refNo="+reqNo;
 //		String b2sUrlPram="\""+fields.get("vpc_ReturnURL")+"\"";
 		String b2sUrlPram="";
