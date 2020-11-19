6b3c6a2d372e3f5432ead67520a6331a7153d3fc
Jinhua <jinhua@ecquaria.com>
2020-11-19 18:33:35 +0800

    Fix audit trail

--- payment/src/main/java/com/ecquaria/cloud/moh/iais/config/PaymentRedisConfig.java
+++ payment/src/main/java/com/ecquaria/cloud/moh/iais/config/PaymentRedisConfig.java
@@ -1,18 +1,8 @@
 package com.ecquaria.cloud.moh.iais.config;
 
-import java.lang.reflect.Method;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
@@ -25,44 +15,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @date 2020/11/19 18:08
  */
 @Configuration
-@EnableCaching
-@AutoConfigureAfter({RedisAutoConfiguration.class})
-public class PaymentRedisConfig extends CachingConfigurerSupport {
+public class PaymentRedisConfig {
     @Autowired
     private RedisConnectionFactory connectionFactory;
-
-    public PaymentRedisConfig() {
-    }
-
-    @Override
-    @Bean
-    public KeyGenerator keyGenerator() {
-        return new KeyGenerator() {
-            @Override
-            public Object generate(Object target, Method method, Object... params) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(target.getClass().getName());
-                sb.append(method.getName());
-                Object[] var5 = params;
-                int var6 = params.length;
-
-                for(int var7 = 0; var7 < var6; ++var7) {
-                    Object obj = var5[var7];
-                    sb.append(obj.toString());
-                }
-
-                return sb.toString();
-            }
-        };
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheWriter writer = RedisCacheWriter.lockingRedisCacheWriter(connectionFactory);
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-        RedisCacheManager cacheManager = new RedisCacheManager(writer, config);
-        return cacheManager;
-    }
 
     @Bean(name = "paymentRedisTemplate")
     public RedisTemplate<String, Object> paymentRedisTemplate() {
