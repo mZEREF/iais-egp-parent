e2faef36f741a0b2c64dd04ba0437028af0cd01f
Jinhua <jinhua@ecquaria.com>
2020-11-19 10:39:40 +0800

    Fix audit trail

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/action/HalpAuthorisationHandler.java
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/action/HalpAuthorisationHandler.java
@@ -1,5 +1,14 @@
 package com.ecquaria.cloud.moh.iais.action;
 
+import com.ecquaria.cloud.helper.SpringContextHelper;
+import com.ecquaria.cloud.moh.iais.common.constant.AuditTrailConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
+import com.ecquaria.cloud.moh.iais.common.utils.MiscUtil;
+import com.ecquaria.cloud.moh.iais.helper.IaisEGPHelper;
+import com.ecquaria.cloud.moh.iais.web.logging.util.AuditLogUtil;
+import com.ecquaria.cloud.submission.client.wrapper.SubmissionClient;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import sop.security.DefaultAuthorisationHandler;
@@ -16,5 +25,14 @@ public class HalpAuthorisationHandler extends DefaultAuthorisationHandler {
     @Override
     public void onAuthorisationFailure(HttpServletRequest request, SOPEngineRequest sopReq) {
         log.info("<==== HALP auth ====>");
+        AuditTrailDto atd = IaisEGPHelper.getCurrentAuditTrailDto();
+        if (atd != null) {
+            List<AuditTrailDto> list = IaisCommonUtils.genNewArrayList(1);
+            AuditTrailDto ad = MiscUtil.transferEntityDto(atd, AuditTrailDto.class);
+            ad.setOperation(AuditTrailConsts.OPERATION_UNAUTHORISED_ACCESS_SOURCES);
+            ad.setFunctionName(sopReq.getProcessName());
+            list.add(ad);
+            AuditLogUtil.callWithEventDriven(list, SpringContextHelper.getContext().getBean(SubmissionClient.class));
+        }
     }
 }
