b1bc886efe340cdf52bc1faf9f4523df6f392e3f
GuYin <password$1>
2020-11-19 19:13:13 +0800

    msg template

---
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/client/MsgTemplateClientFallback.java
@@ -0,0 +1,39 @@
+package com.ecquaria.cloud.moh.iais.service.client;
+
+import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
+import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
+import com.ecquaria.cloud.moh.iais.common.dto.templates.MsgTemplateDto;
+import com.ecquaria.cloud.moh.iais.common.dto.templates.MsgTemplateQueryDto;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * @Author: Hc
+ * @Program: iais-egp
+ * @Create: 2019-12-24 10:53
+ **/
+public class MsgTemplateClientFallback implements MsgTemplateClient{
+    @Override
+    public FeignResponseEntity<SearchResult<MsgTemplateQueryDto>> getMsgTemplateResult(SearchParam param) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<MsgTemplateDto> getMsgTemplate(String id) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<MsgTemplateDto> updateMasterCode(MsgTemplateDto dto) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+}
