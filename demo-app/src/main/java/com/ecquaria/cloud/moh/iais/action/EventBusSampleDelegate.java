94242555a0e3d29cb8bb695d1401ab92e5b017bb
Jinhua <jinhua@ecquaria.com>
2020-11-25 10:02:46 +0800

    Fix demo

--- demo-app/src/main/java/com/ecquaria/cloud/moh/iais/action/EventBusSampleDelegate.java
+++ demo-app/src/main/java/com/ecquaria/cloud/moh/iais/action/EventBusSampleDelegate.java
@@ -2,7 +2,6 @@ package com.ecquaria.cloud.moh.iais.action;
 
 import com.ecquaria.cloud.annotation.Delegator;
 import com.ecquaria.cloud.moh.iais.client.SampleClient;
-import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.EventBusConsts;
 import com.ecquaria.cloud.moh.iais.common.dto.sample.OrgSampleDto;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
@@ -45,6 +44,7 @@ public class EventBusSampleDelegate {
         String submissionId = sampleClient.getSeqId().getEntity();
         SubmitResp submitResp = eventBusHelper.submitAsyncRequest(orgDto, submissionId, EventBusConsts.SERVICE_NAME_DEMO,
                 EventBusConsts.OPERATION_DEMO_CREATE_ORG, orgDto.getEventRefNo(), bpc.process);
+        log.info(StringUtil.changeForLog("<==== End to call Event bus submit =====>"));
     }
 
 
