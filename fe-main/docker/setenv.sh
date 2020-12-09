293f034a97f86072dc3b624530e8c3f5d51ec2fe
Jinhua <jinhua@ecquaria.com>
2020-12-09 11:13:43 +0800

    For SSO

--- fe-main/docker/setenv.sh
+++ fe-main/docker/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Dlog4j.configurationFile=/etc/log4j2.xml"
+JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
 #JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000"
