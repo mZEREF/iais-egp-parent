d54ad5b8265e64dfa9b42f91ffc748c062ad2159
HuaChong <chong_hua@ecquaria.com>
2020-12-14 19:06:16 +0800

    withdrawal application

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaApplicationDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaApplicationDelegator.java
@@ -2189,7 +2189,7 @@ public class HcsaApplicationDelegator {
                             String applicationNo = oldApplication.getApplicationNo();
                             String applicationType1 = oldApplication.getApplicationType();
                             String loginUrl = HmacConstants.HTTPS +"://" + systemParamConfig.getInterServerName() + MessageConstants.MESSAGE_INBOX_URL_INTER_LOGIN;
-                            ApplicationGroupDto applicationGroupDto = applicationClient.getAppGrpByNo(applicationNo).getEntity();
+                            ApplicationGroupDto applicationGroupDto = applicationClient.getAppById(oldApplication.getAppGrpId()).getEntity();
 //                            ApplicationGroupDto applicationGroupDto = applicationViewDto.getApplicationGroupDto();
                             OrgUserDto orgUserDto = organizationClient.retrieveOrgUserAccountById(applicationGroupDto.getSubmitBy()).getEntity();
                             if(orgUserDto != null){
