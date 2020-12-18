cf587955b81f6ef9ce419ef04809e78b3d023a6a
GuYin <password$1>
2020-12-18 17:02:43 +0800

    68183

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealApproveBatchjob.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealApproveBatchjob.java
@@ -654,9 +654,7 @@ public class AppealApproveBatchjob {
             templateContent.put("licenceEndDate", Formatter.formatDate(licenceDto.getExpiryDate()));
             templateContent.put("newEndDate", Formatter.formatDate(expiryDate));
         }else{
-            if(!StringUtil.isEmpty(appPremiseMiscDto.getOtherReason())){
-                templateContent.put("content", "<p>num_rep&nbsp;" + appPremiseMiscDto.getOtherReason() + "</p>");
-            }
+            templateContent.put("content", appPremiseMiscDto.getOtherReason());
         }
 
         log.info(StringUtil.changeForLog("templateContent :" +JsonUtil.parseToJson(templateContent)));
