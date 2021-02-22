b6cc6c95082aa85ef6e10acf167827841c4b72bd
wenkang <wenkang@ecquaria.com>
2021-02-22 10:36:59 +0800

    message

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/ConfigServiceDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/ConfigServiceDelegator.java
@@ -688,8 +688,8 @@ public class ConfigServiceDelegator {
                         hcsaSvcSpeRoutingSchemeDto2.setIsMandatory("true");
                         hcsaSvcSpeRoutingSchemeDto2.setStageId(id);
                         if(flag){
-                            hcsaSvcSpeRoutingSchemeDto1.setSchemeType(TaskConsts.TASK_SCHEME_TYPE_ROUND);
-                            hcsaSvcSpeRoutingSchemeDto2.setSchemeType(TaskConsts.TASK_SCHEME_TYPE_ROUND);
+                            hcsaSvcSpeRoutingSchemeDto1.setSchemeType(routingScheme);
+                            hcsaSvcSpeRoutingSchemeDto2.setSchemeType(routingScheme);
                         }
                         hcsaSvcSpeRoutingSchemeDto2.setStatus(AppConsts.COMMON_STATUS_ACTIVE);
                         hcsaSvcSpeRoutingSchemeDto2.setAppType(every);
