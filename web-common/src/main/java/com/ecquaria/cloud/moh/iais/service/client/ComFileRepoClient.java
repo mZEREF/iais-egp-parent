bda8785dd9679582b31eb9b6d6d34b63e61a2a43
Jinhua <jinhua@ecquaria.com>
2021-03-11 14:25:21 +0800

    Change file repo

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ComFileRepoClient.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ComFileRepoClient.java
@@ -1,11 +1,16 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
+import com.ecquaria.cloud.moh.iais.common.constant.AuditTrailConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
 import com.ecquaria.cloud.moh.iais.common.exception.IaisRuntimeException;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
+import com.ecquaria.cloud.moh.iais.common.utils.JsonUtil;
+import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,6 +22,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * ComFileRepoClient
@@ -34,6 +41,7 @@ public class ComFileRepoClient {
     public List<String> saveFileRepo(List<File> files) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
         MultiValueMap<String, Object> multipartRequest = new LinkedMultiValueMap<>();
         if (IaisCommonUtils.isEmpty(files)) {
             for (File file : files) {
@@ -53,6 +61,22 @@ public class ComFileRepoClient {
                 }
             }
             HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(multipartRequest, headers);
+
+            AuditTrailDto dto = null;
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                if (request != null) {
+                    dto = (AuditTrailDto) ParamUtil.getSessionAttr(request, AuditTrailConsts.SESSION_ATTR_PARAM_NAME);
+                }
+            }
+            if (dto == null) {
+                dto = AuditTrailDto.getThreadDto();
+            }
+
+            if (dto != null) {
+                headers.add("currentAuditTrail", JsonUtil.parseToJson(dto));
+            }
 
             return restTemplate.postForObject("http://file-repository/saveFiles", requestEntity, List.class);
         }
