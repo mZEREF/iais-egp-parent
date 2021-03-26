c37c5f730792be2555e42c81e87f9017e4cc125e
Jinhua <jinhua@ecquaria.com>
2021-03-26 23:45:30 +0800

    Merge from SZ_DEV

---
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/service/PatchService.java
@@ -0,0 +1,11 @@
+package com.ecquaria.cloud.moh.iais.service;
+
+/**
+ * PatchWorkingGrpService
+ *
+ * @author Jinhua
+ * @date 2021/3/26 15:39
+ */
+public interface PatchService {
+    void patchWorkingGrpWithRole();
+}
