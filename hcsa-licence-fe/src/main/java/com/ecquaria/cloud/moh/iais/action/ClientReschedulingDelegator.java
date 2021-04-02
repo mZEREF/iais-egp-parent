e0d2d670589559db9f321d19c4060b7ff6a2ee08
junyu <junyu@ecquaria.com>
2021-04-02 09:51:02 +0800

    69008

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClientReschedulingDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/ClientReschedulingDelegator.java
@@ -386,7 +386,7 @@ public class ClientReschedulingDelegator {
             String nowDate = Formatter.formatDateTime(new Date(), SystemAdminBaseConstants.DATE_FORMAT);
             if(!StringUtil.isEmpty(inspStartDate)&&!StringUtil.isEmpty(inspToDate)){
                 if( inspStartDate_3.compareTo(inspToDate)>0){
-                    errMap.put("newDate" + appId,"Start date must be 3 days before the end date");
+                    errMap.put("newDate" + appId,MessageUtil.getMessageDesc("OAPPT_ERR013"));
                 }
                 if(inspStartDate.compareTo(nowDate)<0){
                     errMap.put("newDate" + appId,"Inspection date must be future");
