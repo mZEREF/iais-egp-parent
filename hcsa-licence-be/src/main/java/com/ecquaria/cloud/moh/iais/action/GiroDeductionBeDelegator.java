888e99e52e2492c5fc98740a8b7642af83fca7c4
wenkang <wenkang@ecquaria.com>
2021-04-08 13:02:46 +0800

    message

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/GiroDeductionBeDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/GiroDeductionBeDelegator.java
@@ -313,7 +313,7 @@ public class GiroDeductionBeDelegator {
             giroDeductionDto.setPmtStatus(v);
             giroDeductionDtoList.add(giroDeductionDto);
         });
-        if (StringUtil.isEmpty(giroDeductionDtoList)){
+        if (!StringUtil.isEmpty(giroDeductionDtoList)){
             HmacHelper.Signature signature = HmacHelper.getSignature(keyId, secretKey);
             HmacHelper.Signature signature2 = HmacHelper.getSignature(secKeyId, secSecretKey);
             beEicGatewayClient.updateDeductionDtoSearchResultUseGroups(giroDeductionDtoList, signature.date(), signature.authorization(),
