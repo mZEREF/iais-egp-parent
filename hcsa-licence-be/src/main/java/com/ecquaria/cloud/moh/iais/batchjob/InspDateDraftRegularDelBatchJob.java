797ea378888c37486352e40547e18e292336a799
ShiCheng_Xu <shicheng@ecquaria.com>
2021-04-10 10:47:39 +0800

    CR070

---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspDateDraftRegularDelBatchJob.java
@@ -0,0 +1,13 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Shicheng
+ * @date 2021/4/10 10:39
+ **/
+@Delegator("inspDateDraftRegularDelDelegator")
+@Slf4j
+public class InspDateDraftRegularDelBatchJob {
+}
---
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspDateDraftRegularDelHandler.java
@@ -0,0 +1,8 @@
+package com.ecquaria.cloud.moh.iais.batchjob;
+
+/**
+ * @author Shicheng
+ * @date 2021/4/10 10:39
+ **/
+public class InspDateDraftRegularDelHandler {
+}
