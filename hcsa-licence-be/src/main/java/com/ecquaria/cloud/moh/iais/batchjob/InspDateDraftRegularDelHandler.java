82156956101fad1cdff4d0720fe827ae841d1918
ShiCheng_Xu <shicheng@ecquaria.com>
2021-04-12 09:37:31 +0800

    remove inspection date draft

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspDateDraftRegularDelHandler.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/InspDateDraftRegularDelHandler.java
@@ -5,10 +5,13 @@ import com.ecquaria.cloud.job.executor.handler.IJobHandler;
 import com.ecquaria.cloud.job.executor.handler.annotation.JobHandler;
 import com.ecquaria.cloud.job.executor.log.JobLogger;
 import com.ecquaria.cloud.moh.iais.common.config.SystemParamConfig;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.dto.appointment.AppPremInspApptDraftDto;
+import com.ecquaria.cloud.moh.iais.common.dto.appointment.ApptCalendarStatusDto;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
+import com.ecquaria.cloud.moh.iais.service.client.AppointmentClient;
 import com.ecquaria.cloud.moh.iais.service.client.InspectionTaskClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +34,9 @@ public class InspDateDraftRegularDelHandler extends IJobHandler {
 
     @Autowired
     private InspectionTaskClient inspectionTaskClient;
+
+    @Autowired
+    private AppointmentClient appointmentClient;
 
     @Override
     public ReturnT<String> execute(String s) throws Exception {
@@ -62,6 +68,12 @@ public class InspDateDraftRegularDelHandler extends IJobHandler {
                         long hoursLong = hours;
                         if(diffHours > hoursLong) {
                             inspectionTaskClient.deleteInspDateDraftById(appPremInspApptDraftDto.getId());
+                            List<String> cancelRefNo = IaisCommonUtils.genNewArrayList();
+                            cancelRefNo.add(appPremInspApptDraftDto.getApptRefNo());
+                            ApptCalendarStatusDto apptCalendarStatusDto = new ApptCalendarStatusDto();
+                            apptCalendarStatusDto.setCancelRefNums(cancelRefNo);
+                            apptCalendarStatusDto.setSysClientKey(AppConsts.MOH_IAIS_SYSTEM_APPT_CLIENT_KEY);
+                            appointmentClient.updateUserCalendarStatus(apptCalendarStatusDto);
                         }
                     } catch (Exception e) {
                         JobLogger.log(e);
