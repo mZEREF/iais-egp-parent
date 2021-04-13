8ff845c83efd02e20c7ea36ac28c647e01098640
wenkang <wenkang@ecquaria.com>
2021-04-13 09:57:56 +0800

    message

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/RequestForChangeMenuDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/RequestForChangeMenuDelegator.java
@@ -773,6 +773,9 @@ public class RequestForChangeMenuDelegator {
                 appSubmissionDtos1.add(appSubmissionDto1);
             }
         }
+        for(AppSubmissionDto v : appSubmissionDtos1){
+            requestForChangeService.premisesDocToSvcDoc(v);
+        }
         //save
         List<AppSubmissionDto> appSubmissionDtos2 = requestForChangeService.saveAppsBySubmissionDtos(appSubmissionDtos1);
         ParamUtil.setRequestAttr(bpc.request, "action_type", "bank");
@@ -1703,7 +1706,7 @@ public class RequestForChangeMenuDelegator {
 
                 appSubmissionDtoByLicenceId.setGetAppInfoFromDto(true);
                 oldPremiseToNewPremise(appSubmissionDtoByLicenceId);
-                requestForChangeService.svcDocToPresmise(appSubmissionDtoByLicenceId);
+                requestForChangeService.premisesDocToSvcDoc(appSubmissionDtoByLicenceId);
                 appSubmissionDtos.add(appSubmissionDtoByLicenceId);
             }
         }
