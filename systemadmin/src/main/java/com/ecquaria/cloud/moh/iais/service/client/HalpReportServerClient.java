61fccaa0a72aee05929f907a625b53775346cb17
yichen <yichen_guo@ecquaria.com>
2021-04-16 14:57:34 +0800

    elis dm job

---
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/HalpReportServerClient.java
@@ -0,0 +1,16 @@
+package com.ecquaria.cloud.moh.iais.service.client;
+
+import com.ecquaria.cloudfeign.FeignConfiguration;
+import com.ecquaria.cloudfeign.FeignResponseEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author yichen
+ * @Date:2021/4/16
+ */
+@FeignClient(name = "halp-report", configuration = FeignConfiguration.class)
+public interface HalpReportServerClient {
+    @PostMapping(value = "/halp-sftp-elis/view-data/")
+    FeignResponseEntity<Void> createElisData();
+}
