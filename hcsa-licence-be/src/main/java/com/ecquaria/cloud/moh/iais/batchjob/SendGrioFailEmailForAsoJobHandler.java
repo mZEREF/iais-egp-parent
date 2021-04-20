82a80818355c1fbb8e600f56b63169e986e64aa3
wangyu <wangyu@ecquaria.com>
2021-04-20 16:50:01 +0800

    bug fix grio fail send email

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/SendGrioFailEmailForAsoJobHandler.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/SendGrioFailEmailForAsoJobHandler.java
@@ -115,14 +115,15 @@ public class SendGrioFailEmailForAsoJobHandler extends IJobHandler {
             log.info(StringUtil.changeForLog("sendGrioFailEmailForAso"));
             notificationHelper.sendNotification(emailParam);
             //send sms
-            emailParam = new EmailParam();
-            emailParam.setTemplateContent(emailMap);
-            emailParam.setQueryCode(applicationGroupDto.getGroupNo());
-            emailParam.setReqRefNum(applicationGroupDto.getGroupNo());
-            emailParam.setTemplateId(MsgTemplateConstants.MSG_TEMPLATE_EN_FEP_002_SMS);
-            emailParam.setRefIdType(NotificationHelper.RECEIPT_TYPE_SMS_APP);
-            emailParam.setSubject(smsSubject);
-            notificationHelper.sendNotification(emailParam);
+            EmailParam  emailParamSms = new EmailParam();
+            emailParamSms.setTemplateContent(emailMap);
+            emailParamSms.setQueryCode(applicationGroupDto.getGroupNo());
+            emailParamSms.setReqRefNum(applicationGroupDto.getGroupNo());
+            emailParamSms.setTemplateId(MsgTemplateConstants.MSG_TEMPLATE_EN_FEP_002_SMS);
+            emailParamSms.setRefIdType(NotificationHelper.RECEIPT_TYPE_SMS_APP);
+            emailParamSms.setRefId(applicationGroupDto.getGroupNo()+"-01");
+            emailParamSms.setSubject(smsSubject);
+            notificationHelper.sendNotification(emailParamSms);
         }catch (Exception e){
             log.error(e.getMessage(),e);
             return false;
