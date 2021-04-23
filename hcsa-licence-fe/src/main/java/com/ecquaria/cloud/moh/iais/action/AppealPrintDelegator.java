c91b05661ab00a95f5f3696a3234e860b50bb1c1
wenkang <wenkang@ecquaria.com>
2021-04-23 17:39:35 +0800

    comment something

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/AppealPrintDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/AppealPrintDelegator.java
@@ -1,8 +1,18 @@
 package com.ecquaria.cloud.moh.iais.action;
 
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.appeal.AppPremisesSpecialDocDto;
+import com.ecquaria.cloud.moh.iais.dto.PageShowFileDto;
+import com.ecquaria.cloud.moh.iais.utils.SingeFileUtil;
 import lombok.extern.slf4j.Slf4j;
 import sop.webflow.rt.api.BaseProcessClass;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Wenkang
@@ -15,6 +25,47 @@ public class AppealPrintDelegator {
     public void prepareData(BaseProcessClass bpc){
         log.info("------>prepareData start<------");
         bpc.request.setAttribute("crud_action_type","appeal");
+        Map<String, File> map = (Map<String, File>) bpc.request.getSession().getAttribute("seesion_files_map_ajax_feselectedFile");
+        Map<String, PageShowFileDto> pageShowFileHashMap = (Map<String, PageShowFileDto>)bpc.request.getSession().getAttribute("pageShowFileHashMap");
+        List<PageShowFileDto> pageShowFileDtos =new ArrayList<>(5);
+        if(map!=null&&!map.isEmpty()){
+            map.forEach((k,v)->{
+                if(v!=null){
+                    long length = v.length();
+                    if(length>0){
+                        Long size=length/1024;
+                        AppPremisesSpecialDocDto premisesSpecialDocDto=new AppPremisesSpecialDocDto();
+                        premisesSpecialDocDto.setDocName(v.getName());
+                        SingeFileUtil singeFileUtil=SingeFileUtil.getInstance();
+                        String fileMd5 = singeFileUtil.getFileMd5(v);
+                        premisesSpecialDocDto.setMd5Code(fileMd5);
+                        premisesSpecialDocDto.setDocSize(Integer.valueOf(size.toString()));
+                        PageShowFileDto pageShowFileDto =new PageShowFileDto();
+                        pageShowFileDto.setFileName(v.getName());
+                        String e = k.substring(k.lastIndexOf('e') + 1);
+                        pageShowFileDto.setIndex(e);
+                        pageShowFileDto.setFileMapId("selectedFileDiv"+e);
+                        pageShowFileDto.setSize(Integer.valueOf(size.toString()));
+                        pageShowFileDto.setMd5Code(fileMd5);
+                        premisesSpecialDocDto.setIndex(k);
+                        pageShowFileDtos.add(pageShowFileDto);
+                    }
+                }else {
+                    if(pageShowFileHashMap!=null){
+                        PageShowFileDto pageShowFileDto = pageShowFileHashMap.get(k);
+                        AppPremisesSpecialDocDto premisesSpecialDocDto=new AppPremisesSpecialDocDto();
+                        premisesSpecialDocDto.setDocName(pageShowFileDto.getFileName());
+                        premisesSpecialDocDto.setFileRepoId(pageShowFileDto.getFileUploadUrl());
+                        premisesSpecialDocDto.setDocSize(pageShowFileDto.getSize());
+                        premisesSpecialDocDto.setMd5Code(pageShowFileDto.getMd5Code());
+                        premisesSpecialDocDto.setIndex(k.substring(k.lastIndexOf('e') + 1));
+                        pageShowFileDtos.add(pageShowFileDto);
+                    }
+                }
+            });
+        }
+        Collections.sort(pageShowFileDtos,(s1, s2)->s1.getFileMapId().compareTo(s2.getFileMapId()));
+        bpc.request.getSession().setAttribute("pageShowFiles", pageShowFileDtos);
     }
     public void start(BaseProcessClass bpc){
         log.info("------>mohAppealPrint start<------");
--- hcsa-licence-fe/src/main/webapp/WEB-INF/jsp/iais/appealPrint/appealPrint.jsp
+++ hcsa-licence-fe/src/main/webapp/WEB-INF/jsp/iais/appealPrint/appealPrint.jsp
@@ -116,9 +116,7 @@
             <c:forEach items="${pageShowFiles}" var="pageShowFileDto" varStatus="ind">
               <div id="${pageShowFileDto.fileMapId}">
                   <span  name="fileName" style="font-size: 14px;color: #2199E8;text-align: center">
-                  <a  href="${pageContext.request.contextPath}/file-repo?filerepo=fileRo0&fileRo0=<iais:mask name="fileRo0" value="${pageShowFileDto.fileUploadUrl}"/>&fileRepoName=${pageShowFileDto.fileName}" title="Download" class="downloadFile">${pageShowFileDto.fileName}</a></span>
-                  <span class="error-msg" name="iaisErrorMsg" id="error_file${ind.index}"></span>
-
+                  ${pageShowFileDto.fileName}
                 <button type="button" class="btn btn-secondary btn-sm" onclick="javascript:deleteFileFeAjax('selectedFile',${pageShowFileDto.index});">
                 Delete</button>  <button type="button" class="btn btn-secondary btn-sm" onclick="javascript:reUploadFileFeAjax('selectedFile',${pageShowFileDto.index},'mainForm');">
               ReUpload</button>
