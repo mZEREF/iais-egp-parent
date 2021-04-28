b9ccb37dcd5b0a5025a9e8cf3574b21f6804c636
wangyu <wangyu@ecquaria.com>
2021-04-28 18:15:59 +0800

    giro no account detection

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ServiceConfigServiceImpl.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ServiceConfigServiceImpl.java
@@ -444,6 +444,8 @@ public class ServiceConfigServiceImpl implements ServiceConfigService {
         List<GiroPaymentDto> giroPaymentDtos = IaisCommonUtils.genNewArrayList();
         if(!IaisCommonUtils.isEmpty(INPUT_DETAIL)) {
             for (InputDetailDto inputDetailDto : INPUT_DETAIL) {
+                log.info(StringUtil.changeForLog("----------saveGiroPaymentDtosByInputDetail APPGROUPNO :" + inputDetailDto.getAppGroupNo()));
+                log.info(StringUtil.changeForLog("----------saveGiroPaymentDtosByInputDetail Dto :" + inputDetailDto.toString()));
                 GiroPaymentDto giroPaymentDto = new GiroPaymentDto();
                 giroPaymentDto.setPmtStatus(ApplicationConsts.PAYMENT_STATUS_PENDING_GIRO);
                 giroPaymentDto.setPmtType(ApplicationConsts.GIRO_BANK_PAYMENT_TYPE_MONEYPAY);
