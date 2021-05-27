0356e0584255b9f023f0873cd002bd62a7ee5bfd
Jinhua <jinhua@ecquaria.com>
2021-05-27 10:28:54 +0800

    Change for myinfo

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/model/MyinfoUtil.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/model/MyinfoUtil.java
@@ -18,26 +18,35 @@ import com.ecquaria.cloud.helper.ConfigHelper;
 import com.ecquaria.cloud.helper.SpringContextHelper;
 import com.ecquaria.cloud.moh.iais.auth.MyInfoClient;
 import com.ecquaria.cloud.moh.iais.common.constant.acra.AcraConsts;
-import com.ecquaria.cloud.moh.iais.common.dto.myinfo.AccessTokenDto;
 import com.ecquaria.cloud.moh.iais.common.dto.myinfo.GetTokenDto;
 import com.ecquaria.cloud.moh.iais.common.dto.myinfo.MyInfoTakenDto;
 import com.ecquaria.cloud.moh.iais.common.jwt.SignatureUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.encoders.Base64;
 import org.jose4j.jwa.AlgorithmConstraints;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jws.JsonWebSignature;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
-
-import java.security.*;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 /**
@@ -162,7 +171,7 @@ public class MyinfoUtil {
 
 	public static String getAuthorization(String realm, String signature, String appId, long nonce, long timestamp) {
 		StringBuilder sb = new StringBuilder();
-			sb.append("Apex_l2_Eg realm=\"http://").append(realm );
+			sb.append("Apex_l2_Eg realm=\"").append(realm );
 			sb.append("\",apex_l2_eg_app_id=\"").append(appId);
 			sb.append("\",apex_l2_eg_nonce=\"").append(nonce);
 			sb.append("\",apex_l2_eg_signature_method=\"SHA256withRSA\"");
