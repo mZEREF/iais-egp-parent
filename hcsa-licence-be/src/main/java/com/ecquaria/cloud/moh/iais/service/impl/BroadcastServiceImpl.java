d85ea3dbfa2938347564b1c5f1bd94fdc8f51985
ShiCheng_Xu <shicheng@ecquaria.com>
2021-06-08 16:28:06 +0800

    EAS MTS

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/BroadcastServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/BroadcastServiceImpl.java
@@ -1,15 +1,21 @@
 package com.ecquaria.cloud.moh.iais.service.impl;
 
 import com.ecquaria.cloud.moh.iais.common.constant.EventBusConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.application.ApplicationViewDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.AppSvcVehicleDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.application.BroadcastApplicationDto;
 import com.ecquaria.cloud.moh.iais.common.dto.organization.BroadcastOrganizationDto;
+import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.helper.EventBusHelper;
 import com.ecquaria.cloud.moh.iais.service.BroadcastService;
+import com.ecquaria.cloud.moh.iais.service.client.AppSvcVehicleBeClient;
 import com.ecquaria.cloud.moh.iais.service.client.OrganizationClient;
 import com.ecquaria.cloud.submission.client.model.SubmitResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sop.webflow.rt.api.Process;
+
+import java.util.List;
 
 /**
  * BroadcastServiceImpl
@@ -23,6 +29,10 @@ public class BroadcastServiceImpl implements BroadcastService {
     private OrganizationClient organizationClient;
     @Autowired
     private EventBusHelper eventBusHelper;
+
+    @Autowired
+    private AppSvcVehicleBeClient appSvcVehicleBeClient;
+
     @Override
     public BroadcastOrganizationDto svaeBroadcastOrganization(BroadcastOrganizationDto broadcastOrganizationDto,Process process,String submissionId) {
         SubmitResp submitResp = eventBusHelper.submitAsyncRequest(broadcastOrganizationDto, submissionId,
@@ -46,5 +56,19 @@ public class BroadcastServiceImpl implements BroadcastService {
     @Override
     public BroadcastOrganizationDto getBroadcastOrganizationDto(String groupName, String groupDomain) {
         return organizationClient.getBroadcastOrganizationDto(groupName,groupDomain).getEntity();
+    }
+
+    @Override
+    public BroadcastApplicationDto setAppSvcVehicleDtoByAppView(BroadcastApplicationDto broadcastApplicationDto, ApplicationViewDto applicationViewDto) {
+        if(applicationViewDto != null) {
+            List<AppSvcVehicleDto> appSvcVehicleDtos = applicationViewDto.getAppSvcVehicleDtos();
+            if(!IaisCommonUtils.isEmpty(appSvcVehicleDtos)) {
+                broadcastApplicationDto.setAppSvcVehicleDtos(appSvcVehicleDtos);
+                //get db data
+                List<AppSvcVehicleDto> appSvcVehicleDtoList = appSvcVehicleBeClient.getAppSvcVehicleDtoListByCorrId(appSvcVehicleDtos.get(0).getAppPremCorreId()).getEntity();
+                broadcastApplicationDto.setRollBackAppSvcVehicleDtos(appSvcVehicleDtoList);
+            }
+        }
+        return broadcastApplicationDto;
     }
 }
