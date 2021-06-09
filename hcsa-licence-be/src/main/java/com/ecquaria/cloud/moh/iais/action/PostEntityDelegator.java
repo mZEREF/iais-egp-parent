afbc5f61ca0c2fd97cd2da20862c59ac3b044119
chenlei <chenlei@ecquaria.com>
2021-06-09 13:53:17 +0800

    EDH

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/PostEntityDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/PostEntityDelegator.java
@@ -3,11 +3,16 @@ package com.ecquaria.cloud.moh.iais.action;
 import com.ecquaria.cloud.annotation.Delegator;
 import com.ecquaria.cloud.moh.iais.common.dto.arcaUen.GenerateUENDto;
 import com.ecquaria.cloud.moh.iais.common.dto.arcaUen.IaisUENDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.licence.PremisesDto;
+import com.ecquaria.cloud.moh.iais.common.utils.JsonUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.service.client.AcraUenBeClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import sop.webflow.rt.api.BaseProcessClass;
+
+import java.security.SecureRandom;
 
 /**
  * @author yichen
@@ -16,10 +21,11 @@ import sop.webflow.rt.api.BaseProcessClass;
 @Delegator("postEntityDelegator")
 @Slf4j
 public class PostEntityDelegator {
+
     @Autowired
     private AcraUenBeClient acraUenBeClient;
 
-    public void postEntityToEDH(BaseProcessClass bpc){
+    public void postEntityToEDH(BaseProcessClass bpc) {
         IaisUENDto iaisUENDto = new IaisUENDto();
         String licenseeId = ParamUtil.getString(bpc.request, "licenseeId");
         iaisUENDto.setLicenseeId(licenseeId);
@@ -27,6 +33,14 @@ public class PostEntityDelegator {
         int sequenceNumber = 1;
         generateUENDto.setSequenceNumber(sequenceNumber);
         iaisUENDto.setGenerateUENDto(generateUENDto);
+
+        PremisesDto premise = new PremisesDto();
+        SecureRandom secureRandom = new SecureRandom();
+        premise.setHciCode("Hci" + secureRandom.nextInt(100));
+        premise.setHciName("Name" + secureRandom.nextInt(100));
+        iaisUENDto.setPremises(premise);
+        log.info(StringUtil.changeForLog("Iais UEN Dto: " + JsonUtil.parseToJson(iaisUENDto)));
         acraUenBeClient.generateUen(iaisUENDto);
     }
+
 }
