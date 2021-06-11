2cc34620c2dd5da6b150e08fc1a198581fa35615
wenkang <wenkang@ecquaria.com>
2021-06-11 10:56:05 +0800

    69290

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/GiroDeductionBeDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/GiroDeductionBeDelegator.java
@@ -342,8 +342,8 @@ public class GiroDeductionBeDelegator {
             HmacHelper.Signature signature2 = HmacHelper.getSignature(secKeyId, secSecretKey);
             List<GiroDeductionDto> entity = beEicGatewayClient.updateDeductionDtoSearchResultUseGroups(giroDeductionDtoList, signature.date(), signature.authorization(),
                     signature2.date(), signature2.authorization()).getEntity();
-            applicationClient.updateBeGroupStatus(applicationGroupDtos);
-            updateFeApplicationGroupStatus(applicationGroupDtos);
+            List<ApplicationGroupDto> applicationGroupDtoList = applicationClient.updateBeGroupStatus(applicationGroupDtos).getEntity();
+            updateFeApplicationGroupStatus(applicationGroupDtoList);
             String general_ack021 = MessageUtil.getMessageDesc("GENERAL_ACK021");
             if(entity!=null&&entity.isEmpty()){
                 general_ack021=general_ack021.replace("{num}","0");
