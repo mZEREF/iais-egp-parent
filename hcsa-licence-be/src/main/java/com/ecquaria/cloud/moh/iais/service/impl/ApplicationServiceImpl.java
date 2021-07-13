9c8f812f7ac792282bf4b49b95be2f3f52d85120
ShiCheng_Xu <shicheng@ecquaria.com>
2021-07-13 14:19:01 +0800

    EAS MTS

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicationServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/ApplicationServiceImpl.java
@@ -1078,15 +1078,19 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     public String getVehicleFlagToShowOrEdit(TaskDto taskDto, String vehicleOpenFlag, ApplicationViewDto applicationViewDto) {
         String vehicleFlag = AppConsts.FALSE;
+        //filter appType
         boolean vehicleAppTypeFlag = getVehicleAppTypeFlag(applicationViewDto);
+        //filter vehicleOpenFlag
         if(vehicleAppTypeFlag && taskDto != null && InspectionConstants.SWITCH_ACTION_YES.equals(vehicleOpenFlag)) {
             boolean actionVehicleFlag = false;
+            //filter stage
             if(!IaisCommonUtils.isEmpty(applicationViewDto.getAppSvcVehicleDtos())) {
                 ApplicationGroupDto applicationGroupDto = applicationViewDto.getApplicationGroupDto();
                 String stageId = taskDto.getTaskKey();
                 if(applicationGroupDto != null) {
                     String newLicenseeId = applicationGroupDto.getNewLicenseeId();
                     String licenseeId = applicationGroupDto.getLicenseeId();
+                    //filter DM data
                     if(!StringUtil.isEmpty(newLicenseeId) && newLicenseeId.equals(licenseeId)) {
                         vehicleFlag = InspectionConstants.SWITCH_ACTION_VIEW;
                         actionVehicleFlag = true;
