38dfa171b1533104153575b9df666b60418d8a7d
chenlei <chenlei@ecquaria.com>
2021-07-15 16:48:45 +0800

    add log

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmissionServiceImpl.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/AppSubmissionServiceImpl.java
@@ -2389,6 +2389,7 @@ public class AppSubmissionServiceImpl implements AppSubmissionService {
             sB.append(errIcon);
         }
         bpc.request.getSession().setAttribute("serviceConfig",sB.toString());
+        log.info(StringUtil.changeForLog("Error Message: " + previewAndSubmitMap));
         return previewAndSubmitMap;
     }
 
