301c23c620e15156d0da1e24ad4a265b0e578a98
ShiCheng_Xu <shicheng@ecquaria.com>
2021-07-20 17:24:52 +0800

    sonar

--- be-main/src/main/java/com/ecquaria/cloud/moh/iais/action/BackendInboxDelegator.java
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/action/BackendInboxDelegator.java
@@ -778,7 +778,7 @@ public class BackendInboxDelegator {
                     throw new IaisRuntimeException("This getAppPremisesCorrelationId can not get the broadcast -- >:"+applicationViewDto.getAppPremisesCorrelationId());
                 }
             }else if(ApplicationConsts.APPLICATION_STATUS_PENDING_APPROVAL03.equals(appStatus) || ApplicationConsts.APPLICATION_STATUS_PENDING_APPROVAL02.equals(appStatus)){
-                
+
                 if(HcsaConsts.ROUTING_STAGE_INS.equals(taskDto.getTaskKey())){
                     updateInspectionStatus(applicationViewDto.getAppPremisesCorrelationId(), InspectionConstants.INSPECTION_STATUS_PENDING_AO2_RESULT);
                 }
@@ -1129,7 +1129,7 @@ public class BackendInboxDelegator {
             if(ApplicationConsts.APPLICATION_STATUS_REJECTED.equals(applicationDto.getStatus())){
                 AppReturnFeeDto appReturnFeeDto = new AppReturnFeeDto();
                 Double returnFee = applicationDto.getReturnFee();
-                if(returnFee==null || returnFee == 0d){
+                if(returnFee==null || MiscUtil.doubleEquals(returnFee, 0d)){
                     continue;
                 }
                 appReturnFeeDto.setReturnAmount(returnFee);
