700b266704545f7c9af5019435e801b590eb207e
ShiCheng_Xu <shicheng@ecquaria.com>
2021-07-23 13:40:33 +0800

    sonar

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealWdAppBatchjobHandler.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealWdAppBatchjobHandler.java
@@ -168,7 +168,7 @@ public class AppealWdAppBatchjobHandler extends IJobHandler {
                     msgInfoMap.put("S_LName", serviceName);
                     msgInfoMap.put("MOH_AGENCY_NAME", AppConsts.MOH_AGENCY_NAME);
                     msgInfoMap.put("ApplicationDate", Formatter.formatDateTime(new Date()));
-                    if (StringUtil.isEmpty(paymentMethod) || StringUtil.isEmpty(fee) ||fee==0.0||
+                    if (StringUtil.isEmpty(paymentMethod) || StringUtil.isEmpty(fee) ||MiscUtil.doubleEquals(fee, 0.0)||
                             ApplicationConsts.APPLICATION_TYPE_REQUEST_FOR_CHANGE.equals(applicationType1) || isCharity) {
                         msgInfoMap.put("paymentType", "2");
                         msgInfoMap.put("paymentMode", "");
