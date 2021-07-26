7e834d5b9b6c77169826bdf3b6292440141b787a
junyu <junyu@ecquaria.com>
2021-07-26 15:41:48 +0800

    updateAppPremiseMisc

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealApproveBatchjob.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealApproveBatchjob.java
@@ -157,7 +157,7 @@ public class AppealApproveBatchjob {
                           log.info(StringUtil.changeForLog("The AppealApproveBatchjob appealType  is -->"+appealType));
                           switch(appealType){
                               case ApplicationConsts.APPEAL_TYPE_APPLICAITON :
-                                  appealApplicaiton(appealApplicaiton,rollBackApplication,appealPersonnel,rollBackPersonnel,
+                                  appealApplicaiton(appPremiseMiscDtoList,appealApplicaiton,rollBackApplication,appealPersonnel,rollBackPersonnel,
                                           appealAppGrpPremisesDto,rollBackAppGrpPremisesDto,
                                           appealAppPremisesRecommendationDtos,rollBackAppPremisesRecommendationDtos,appealApplicationGroupDtos,rollBackApplicationGroupDtos,
                                           appealApproveDto);
@@ -233,7 +233,7 @@ public class AppealApproveBatchjob {
         log.info(StringUtil.changeForLog("The AppealApproveBatchjob is end ..."));
     }
 
-    private void appealApplicaiton( List<ApplicationDto> appealApplicaiton,
+    private void appealApplicaiton(List<AppPremiseMiscDto> appPremiseMiscDtoList, List<ApplicationDto> appealApplicaiton,
                                    List<ApplicationDto> rollBackApplication,
                                    List<AppSvcKeyPersonnelDto> appealPersonnel,
                                    List<AppSvcKeyPersonnelDto> rollBackPersonnel,
@@ -248,6 +248,7 @@ public class AppealApproveBatchjob {
         ApplicationDto applicationDto = appealApproveDto.getApplicationDto();
         AppPremiseMiscDto appealDto = appealApproveDto.getAppPremiseMiscDto();
         if(applicationDto!= null && appealDto != null){
+            appPremiseMiscDtoList.add(appealDto);
             String appealReason = appealDto.getReason();
             try {
                 switch(appealReason){
