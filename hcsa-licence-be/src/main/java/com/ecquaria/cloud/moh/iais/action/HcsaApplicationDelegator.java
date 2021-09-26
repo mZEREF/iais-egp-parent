f1bbd84d1c25dcffe9ce1edac56acf110ce4dee0
jiawei <jiawei@ecquaria.com>
2021-09-26 18:14:46 +0800

    73074

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaApplicationDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaApplicationDelegator.java
@@ -110,6 +110,9 @@ public class HcsaApplicationDelegator {
     private BroadcastService broadcastService;
 
     @Autowired
+    private LicenceViewService licenceViewService;
+
+    @Autowired
     private HcsaConfigClient hcsaConfigClient;
 
     @Autowired
@@ -3226,6 +3229,8 @@ public class HcsaApplicationDelegator {
         appPremisesCorrelationDto.setOldCorrelationId(correlationId);
         String newCorrelationId = appPremisesCorrelationDto.getId();
         ApplicationViewDto applicationViewDto = applicationViewService.getApplicationViewDtoByCorrId(newCorrelationId,taskDto.getRoleId());
+        //filter primary doc
+        clearOtherPremiseDoc(appPremisesCorrelationDto.getApplicationId(), applicationViewDto);
         applicationViewDto.setNewAppPremisesCorrelationDto(appPremisesCorrelationDto);
         //set can tcu date
         setShowAndEditTcuDate(bpc.request,applicationViewDto,taskDto);
@@ -3319,6 +3324,27 @@ public class HcsaApplicationDelegator {
         setCessation(bpc.request, applicationViewDto, correlationId);
         //set choose inspection
         setChooseInspectionValue(bpc.request, applicationViewDto);
+    }
+
+    private void clearOtherPremiseDoc(String appid, ApplicationViewDto applicationViewDto) {
+        AppSubmissionDto appSubmissionDto = licenceViewService.getAppSubmissionByAppId(appid);
+        List<AppSupDocDto> newAppSupDocDtoList = IaisCommonUtils.genNewArrayList();
+        for (AppSupDocDto appSupDocDto : applicationViewDto.getAppSupDocDtoList()){
+            if (StringUtil.isEmpty(appSupDocDto.getFileRepoId())){
+                continue;
+            }
+            if (!appSupDocDto.getDocType().equals("primary_doc")){
+                newAppSupDocDtoList.add(appSupDocDto);
+                continue;
+            }
+            for (AppGrpPrimaryDocDto appGrpPrimaryDocDto : appSubmissionDto.getAppGrpPrimaryDocDtos()){
+                if (appSupDocDto.getFileRepoId().equals(appGrpPrimaryDocDto.getFileRepoId())){
+                    newAppSupDocDtoList.add(appSupDocDto);
+                    break;
+                }
+            }
+        }
+        applicationViewDto.setAppSupDocDtoList(newAppSupDocDtoList);
     }
 
     private void setShowAndEditTcuDate(HttpServletRequest request,ApplicationViewDto applicationViewDto,TaskDto taskDto){
