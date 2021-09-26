a36508977957c84ffaa1de0978f36d76c553c2a1
suocheng <taicheng$1>
2021-09-26 10:38:32 +0800

    0073067

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/RequestForChangeDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/RequestForChangeDelegator.java
@@ -775,6 +775,7 @@ public class RequestForChangeDelegator {
 
                         requestForChangeService.checkAffectedAppSubmissions(appSubmissionDto, null, amount, draftNo, grpNo,
                                 null, null,null);
+                        appSubmissionDto.setGetAppInfoFromDto(false);
                         AppSubmissionDto tranferSub = requestForChangeService.submitChange(appSubmissionDto);
                         ParamUtil.setSessionAttr(bpc.request, "app-rfc-tranfer", tranferSub);
                         if (amount == null || MiscUtil.doubleEquals(amount, 0.0)) {
