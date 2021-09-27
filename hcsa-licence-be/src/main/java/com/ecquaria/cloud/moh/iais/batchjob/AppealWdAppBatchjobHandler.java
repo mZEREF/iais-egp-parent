f6f699b6ee57c8333fa9b1d1fcd6e3b56c42768f
Jinhua <jinhua@ecquaria.com>
2021-09-27 17:43:41 +0800

    new release

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealWdAppBatchjobHandler.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/batchjob/AppealWdAppBatchjobHandler.java
@@ -240,7 +240,7 @@ public class AppealWdAppBatchjobHandler extends IJobHandler {
                 List<AppEditSelectDto> appEditSelectDtos = applicationService.getAppEditSelectDtos(oldApplicationDto.getId(), ApplicationConsts.APPLICATION_EDIT_TYPE_RFC);
                 boolean changePrem=false;
                 for (AppEditSelectDto edit:appEditSelectDtos
-                ) {
+                     ) {
                     if(edit.isPremisesEdit()||edit.isPremisesListEdit()){
                         changePrem=true;
                     }
@@ -255,7 +255,7 @@ public class AppealWdAppBatchjobHandler extends IJobHandler {
             List<ApplicationDto> applicationDtoList = applicationService.getApplicaitonsByAppGroupId(oldAppGrpId);
             List<AppPremisesCorrelationDto> appPremisesCorrelationDtos=applicationService.getAppPremisesCorrelationByAppGroupId(oldAppGrpId);
             for (AppPremisesCorrelationDto apc:appPremisesCorrelationDtos
-            ) {
+                 ) {
                 if(apc.getApplicationId().equals(oldApplicationDto.getId())){
                     AppInspectionStatusDto appInspectionStatusDto = appInspectionStatusClient.getAppInspectionStatusByPremId(apc.getId()).getEntity();
                     if(appInspectionStatusDto!=null){
