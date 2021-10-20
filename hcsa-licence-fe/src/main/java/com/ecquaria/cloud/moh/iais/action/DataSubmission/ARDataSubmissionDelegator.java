9b3a38243ed5955a69da98768b0c5abbdb206155
suocheng <taicheng$1>
2021-10-20 16:50:30 +0800

    p2

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/DataSubmission/ARDataSubmissionDelegator.java
@@ -0,0 +1,90 @@
+package com.ecquaria.cloud.moh.iais.action.DataSubmission;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+import sop.webflow.rt.api.BaseProcessClass;
+
+/**
+ * ARDataSubmissionDelegator
+ *
+ * @author suocheng
+ * @date 10/20/2021
+ */
+@Delegator("arDataSubmissionDelegator")
+@Slf4j
+public class ARDataSubmissionDelegator {
+    /**
+     * StartStep: Start
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doStart(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareARSubmission
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareARSubmission(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PreparePIM
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPreparePIM(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PreparePIF
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPreparePIF(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareCSM
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareCSM(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareCSF
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareCSF(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareDS
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareDS(BaseProcessClass bpc) {
+
+    }
+     /**
+     * StartStep: Back
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doBack(BaseProcessClass bpc) {
+
+    }
+
+
+}
--- hcsa-licence-fe/src/main/webapp/WEB-INF/jsp/iais/dataSubmission/assistedReproduction/assistedReproductionSubmission.jsp
+++ hcsa-licence-fe/src/main/webapp/WEB-INF/jsp/iais/dataSubmission/assistedReproduction/assistedReproductionSubmission.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: tai13
   Date: 10/20/2021
-  Time: 4:26 PM
+  Time: 4:37 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,6 @@
   <title>Title</title>
 </head>
 <body>
-dataSubmissionSelect.jsp
+assistedReproductionSubmission.jsp
 </body>
 </html>
