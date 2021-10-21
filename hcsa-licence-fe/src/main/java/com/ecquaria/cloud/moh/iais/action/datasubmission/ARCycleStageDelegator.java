825132859d43fcb96ecf45f551709b06fe949515
suocheng <taicheng$1>
2021-10-21 10:54:35 +0800

    p2

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ARCycleStageDelegator.java
@@ -0,0 +1,60 @@
+package com.ecquaria.cloud.moh.iais.action.datasubmission;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+import sop.webflow.rt.api.BaseProcessClass;
+
+/**
+ * ArCycleStageDelegator
+ *
+ * @author suocheng
+ * @date 10/20/2021
+ */
+@Delegator("arCycleStageDelegator")
+@Slf4j
+public class ARCycleStageDelegator extends CommonDelegator {
+    @Override
+    public void start(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void prepareSwitch(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void returnStep(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void preparePage(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void prepareConfim(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void draft(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void submission(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void pageAction(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void pageConfirmAction(BaseProcessClass bpc) {
+
+    }
+}
