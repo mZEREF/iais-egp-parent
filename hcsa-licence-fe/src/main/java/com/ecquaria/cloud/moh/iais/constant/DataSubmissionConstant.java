55f67b52d39fb3777654efc7a57097753946d8e5
suocheng <taicheng$1>
2021-10-21 16:43:46 +0800

    p2

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/constant/DataSubmissionConstant.java
@@ -0,0 +1,15 @@
+package com.ecquaria.cloud.moh.iais.constant;
+
+/**
+ * DataSubmissionConstant
+ *
+ * @author suocheng
+ * @date 10/21/2021
+ */
+
+public class DataSubmissionConstant {
+    private DataSubmissionConstant() {throw new IllegalStateException("DataSubmissionConstant class");}
+    public static String CRUD_TYPE  = "crud_type";
+    public static String CRUD_ACTION_TYPE_DS = "crud_action_type_ds";
+    public static String CRUD_ACTION_TYPE_AR = "crud_action_type_ar";
+}
