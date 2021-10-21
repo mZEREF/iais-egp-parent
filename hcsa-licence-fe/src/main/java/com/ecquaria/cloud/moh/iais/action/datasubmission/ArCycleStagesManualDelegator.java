c1a48ca39c11df0f3a11a57bc203247e62b5f6b2
suocheng <taicheng$1>
2021-10-21 14:22:11 +0800

    p2

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArCycleStagesManualDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArCycleStagesManualDelegator.java
@@ -21,4 +21,181 @@ public class ArCycleStagesManualDelegator {
      */
     public void doStart(BaseProcessClass bpc) {
     }
+    /**
+     * StartStep: PrepareCycleStageSelection
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareCycleStageSelection(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareStage
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareStage(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareARCycle
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareARCycle(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareIUICycle
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareIUICycle(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareEFOCycle
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareEFOCycle(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareOocyteRetrieval
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareOocyteRetrieval(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareFertilisation
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareFertilisation(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareEmbryoCreated
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareEmbryoCreated(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareThawing
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareThawing(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PreparePreimplantation
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPreparePreimplantation(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareEmbryoTransfer
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareEmbryoTransfer(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareARTreatmentSubsidies
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareARTreatmentSubsidies(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareIUITreatmentSubsidies
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareIUITreatmentSubsidies(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareOutcomeEmbryoTransferred
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareOutcomeEmbryoTransferred(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareOutcome
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareOutcome(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareOutcomePregnancy
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareOutcomePregnancy(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareFreezing
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareFreezing(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareDonation
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareDonation(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareDisposal
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareDisposal(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareEndCycle
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareEndCycle(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: PrepareTransferInOut
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareTransferInOut(BaseProcessClass bpc) {
+    }
+    /**
+     * StartStep: Back
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doBack(BaseProcessClass bpc) {
+    }
+
 }
