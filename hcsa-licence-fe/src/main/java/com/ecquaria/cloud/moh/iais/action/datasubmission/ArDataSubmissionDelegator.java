dcc7c8acbfdf1fe6a88673f9684f91d5d497f5b4
suocheng <taicheng$1>
2021-10-21 11:04:49 +0800

    p2

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArDataSubmissionDelegator.java
@@ -0,0 +1,90 @@
+package com.ecquaria.cloud.moh.iais.action.datasubmission;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+import sop.webflow.rt.api.BaseProcessClass;
+
+/**
+ * ARDataSubmissionDelegator
+ *
+ * @author suocheng
+ * @date 10/20/2021
+ */
+@Delegator("arDataSubmissionDelegator")
+@Slf4j
+public class ArDataSubmissionDelegator {
+    /**
+     * StartStep: Start
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doStart(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareARSubmission
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareARSubmission(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PreparePIM
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPreparePIM(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PreparePIF
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPreparePIF(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareCSM
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareCSM(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareCSF
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareCSF(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: PrepareDS
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doPrepareDS(BaseProcessClass bpc) {
+
+    }
+    /**
+     * StartStep: Back
+     *
+     * @param bpc
+     * @throws
+     */
+    public void doBack(BaseProcessClass bpc) {
+
+    }
+
+
+}
