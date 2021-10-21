a45da0e6cc079e56604c2057047b08491019e601
suocheng <taicheng$1>
2021-10-21 10:53:36 +0800

    p2

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ARCycleStagesManualDelegator.java
@@ -0,0 +1,11 @@
+package com.ecquaria.cloud.moh.iais.action.datasubmission;
+
+/**
+ * ARCycleStagesManualDelegator
+ *
+ * @author suocheng
+ * @date 10/21/2021
+ */
+
+public class ARCycleStagesManualDelegator {
+}
