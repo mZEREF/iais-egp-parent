4bbdaf40a810aa4d421dd38a398cb21e21f6520f
renfanghao <fanghao@ecquaria.com>
2021-10-26 15:11:54 +0800

    fertilisationStage submit

---
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/fertilisationDelegator.java
@@ -0,0 +1,54 @@
+package com.ecquaria.cloud.moh.iais.action.datasubmission;
+
+import com.ecquaria.cloud.annotation.Delegator;
+import lombok.extern.slf4j.Slf4j;
+import sop.webflow.rt.api.BaseProcessClass;
+
+@Delegator("fertilisationDelegator")
+@Slf4j
+public class fertilisationDelegator extends CommonDelegator{
+    @Override
+    public void start(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void prepareSwitch(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void returnStep(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void preparePage(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void prepareConfim(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void draft(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void submission(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void pageAction(BaseProcessClass bpc) {
+
+    }
+
+    @Override
+    public void pageConfirmAction(BaseProcessClass bpc) {
+
+    }
+}
