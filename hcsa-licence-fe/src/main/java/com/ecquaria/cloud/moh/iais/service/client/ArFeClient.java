dc5a3e1930e4665c7e7411b608f6fffb7d366ad9
wangyu <wangyu@ecquaria.com>
2021-10-26 11:01:22 +0800

    ar fe client

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ArFeClient.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ArFeClient.java
@@ -7,11 +7,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "hcsa-licence",configuration = FeignConfiguration.class,fallback = ArFeClientFallback.class)
 public interface ArFeClient {
 
     @GetMapping(value = "/ar-common/patient/idnumber-nationality", produces = MediaType.APPLICATION_JSON_VALUE)
-    FeignResponseEntity<PatientDto> getPatientDto(@PathVariable("idNumber") String idNumber,
-            @PathVariable("nationality") String nationality, @PathVariable("orgId") String orgId);
+    FeignResponseEntity<PatientDto> getPatientDto(@RequestParam(name = "idNumber") String idNumber,
+                                                  @RequestParam(name = "nationality") String nationality,  @RequestParam(name = "orgId") String orgId);
 }
