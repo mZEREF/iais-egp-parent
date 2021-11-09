96d7e1e4de3719e027cc946ebd81ad97ccbdc701
jiawei <jiawei@ecquaria.com>
2021-11-09 10:30:20 +0800

    add EmbryoTransferStageDtoValidator Cannot be earlier than cycle start date

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/validation/dataSubmission/EmbryoTransferStageDtoValidator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/validation/dataSubmission/EmbryoTransferStageDtoValidator.java
@@ -1,12 +1,18 @@
 package com.ecquaria.cloud.moh.iais.validation.dataSubmission;
 
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.ArCycleStageDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.ArSuperDataSubmissionDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.EmbryoTransferStageDto;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.common.validation.interfaces.CustomizeValidator;
+import com.ecquaria.cloud.moh.iais.helper.DataSubmissionHelper;
 import com.ecquaria.cloud.moh.iais.helper.MessageUtil;
+import lombok.SneakyThrows;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
 /**
@@ -17,29 +23,39 @@ import java.util.Map;
  */
 
 public class EmbryoTransferStageDtoValidator implements CustomizeValidator {
+    @SneakyThrows
     @Override
     public Map<String, String> validate(Object obj, String profile, HttpServletRequest request) {
         Map<String, String> errorMap = IaisCommonUtils.genNewHashMap();
         EmbryoTransferStageDto embryoTransferStageDto = (EmbryoTransferStageDto) obj;
-        //TODO 1st Date of Transfer and 2nd Date of Transfer (if applicable)•	Cannot be earlier than cycle start date
-        if (embryoTransferStageDto.getTransferNum() > 1){
-            if (StringUtil.isEmpty(embryoTransferStageDto.getSecondEmbryoAge())){
+        if (embryoTransferStageDto.getTransferNum() > 1) {
+            if (StringUtil.isEmpty(embryoTransferStageDto.getSecondEmbryoAge())) {
                 String errMsg = MessageUtil.replaceMessage("GENERAL_ERR0006", "Age of 2nd Embryo Transferred", "field");
                 errorMap.put("secondEmbryoAge", errMsg);
             }
-            if (StringUtil.isEmpty(embryoTransferStageDto.getSecondEmbryoType())){
+            if (StringUtil.isEmpty(embryoTransferStageDto.getSecondEmbryoType())) {
                 String errMsg = MessageUtil.replaceMessage("GENERAL_ERR0006", "Was the 2nd Embryo Transferred a fresh or thawed embryo?", "field");
                 errorMap.put("secondEmbryoType", errMsg);
             }
         }
-        if (embryoTransferStageDto.getTransferNum() > 2){
-            if (StringUtil.isEmpty(embryoTransferStageDto.getThirdEmbryoAge())){
+        if (embryoTransferStageDto.getTransferNum() > 2) {
+            if (StringUtil.isEmpty(embryoTransferStageDto.getThirdEmbryoAge())) {
                 String errMsg = MessageUtil.replaceMessage("GENERAL_ERR0006", "Age of 3rd Embryo Transferred", "field");
                 errorMap.put("thirdEmbryoAge", errMsg);
             }
-            if (StringUtil.isEmpty(embryoTransferStageDto.getThirdEmbryoType())){
+            if (StringUtil.isEmpty(embryoTransferStageDto.getThirdEmbryoType())) {
                 String errMsg = MessageUtil.replaceMessage("GENERAL_ERR0006", "Was the 3rd Embryo Transferred a fresh or thawed embryo?", "field");
                 errorMap.put("thirdEmbryoType", errMsg);
+            }
+        }
+        ArSuperDataSubmissionDto arSuperDataSubmissionDto = DataSubmissionHelper.getCurrentArDataSubmission(request);
+        ArCycleStageDto arCycleStageDto = arSuperDataSubmissionDto.getArCycleStageDto();
+        if (arCycleStageDto != null && StringUtil.isNotEmpty(arCycleStageDto.getStartDate())) {
+            if (embryoTransferStageDto.getFirstTransferDate().before(new SimpleDateFormat(AppConsts.DEFAULT_DATE_FORMAT).parse(arCycleStageDto.getStartDate()))) {
+                errorMap.put("firstTransferDate", "Cannot be earlier than cycle start date");
+            }
+            if (embryoTransferStageDto.getSecondTransferDate().before(new SimpleDateFormat(AppConsts.DEFAULT_DATE_FORMAT).parse(arCycleStageDto.getStartDate()))) {
+                errorMap.put("secondTransferDate", "Cannot be earlier than cycle start date");
             }
         }
         return errorMap;
