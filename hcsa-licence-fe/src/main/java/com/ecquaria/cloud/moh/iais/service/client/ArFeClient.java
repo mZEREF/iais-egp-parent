ffa66f7ed6573e6913a5d63367726ba77a0d147b
zhixing <zhixing@ecquaria.com>
2021-11-22 18:08:42 +0800

    iuiTreatmentSubsidies

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ArFeClient.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/client/ArFeClient.java
@@ -111,7 +111,7 @@ public interface ArFeClient {
                                                                                                        @RequestParam(name = "hciCode") String hciCode,
                                                                                                        @RequestParam(name = "cycleType") String cycleType);
 
-    @GetMapping(value = "/iuiTreatmentSubsidiesDtos/patientcode-hcicode-cycletype", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/ar-common/iuiTreatmentSubsidiesDtos/patientcode-hcicode-cycletype", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<List<IuiTreatmentSubsidiesDto>> getIuiTreatmentSubsidiesDtosByPhc(@RequestParam(name = "patientCode") String patientCode,
                                                                                                                           @RequestParam(name = "hciCode") String hciCode,
                                                                                                                           @RequestParam(name = "cycleType") String cycleType);
