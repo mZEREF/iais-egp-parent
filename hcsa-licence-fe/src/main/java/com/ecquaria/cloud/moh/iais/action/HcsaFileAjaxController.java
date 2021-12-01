9c5e12700d83300f5c068e106d1e193d25f920f2
Jinhua <jinhua@ecquaria.com>
2021-12-01 13:05:38 +0800

    Fix File upload issue

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaFileAjaxController.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/HcsaFileAjaxController.java
@@ -218,8 +218,10 @@ public class HcsaFileAjaxController {
                     byte[] fileData = FileUtils.readFileToByteArray(file);
                     if(fileData != null){
                         try {
+                            String fileName = URLEncoder.encode(file.getName(), StandardCharsets.UTF_8.toString());
+                            log.info("Down load file name ==> {}", fileName);
                             response.addHeader("Content-Disposition", "attachment;filename=\""
-                                    +  URLEncoder.encode(file.getName(), StandardCharsets.UTF_8.toString()) + "\"");
+                                    +  fileName + "\"");
                             response.addHeader("Content-Length", "" + fileData.length);
                             response.setContentType("application/x-octet-stream");
                         }catch (Exception e){
