d0a14f814dc48f59e63bb88bcf67eafdc606cfe1
junyu <junyu@ecquaria.com>
2021-12-02 10:06:26 +0800

    user

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/IntranetUserClient.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/service/client/IntranetUserClient.java
@@ -57,7 +57,7 @@ public interface IntranetUserClient {
     @DeleteMapping(value = "/iais-orguser-be/{id}")
     FeignResponseEntity<Void> delOrgUser (@PathVariable("id")String id);
 
-    @GetMapping(value = "/iais-orguser-be/users-account/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/iais-orguser-be/users-account/management/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     FeignResponseEntity<OrgUserDto> findIntranetUserById(@PathVariable("id")String id);
 
     @GetMapping(value = "/iais-orguser-be//organization/uen-list",produces = MediaType.APPLICATION_JSON_VALUE)
