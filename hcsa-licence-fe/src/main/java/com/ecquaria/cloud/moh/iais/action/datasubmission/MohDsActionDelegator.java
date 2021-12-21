b2da7eb62a6566bca301cf9c17d504d4d6f2ae44
wangyu <wangyu@ecquaria.com>
2021-12-21 11:09:24 +0800

    bug fix 75615

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/MohDsActionDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/MohDsActionDelegator.java
@@ -2,6 +2,7 @@ package com.ecquaria.cloud.moh.iais.action.datasubmission;
 
 import com.ecquaria.cloud.RedirectUtil;
 import com.ecquaria.cloud.annotation.Delegator;
+import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.dataSubmission.DataSubmissionConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.inbox.InboxConst;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.ArSuperDataSubmissionDto;
@@ -93,6 +94,7 @@ public class MohDsActionDelegator {
                 iuiCycleStageDelegator.init(request);
                 arDataSubmissionService.setIuiCycleStageDtoDefaultVal(arSuper);
             }
+            ParamUtil.setRequestAttr(request, "preViewAr", AppConsts.YES);
         }
     }
     /**
