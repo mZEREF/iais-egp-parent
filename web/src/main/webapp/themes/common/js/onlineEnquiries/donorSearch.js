79bc66f9440aa3a1c6646e5a2d9986334d693e03
junyu <junyu@ecquaria.com>
2021-12-22 09:55:57 +0800

    js

--- web/src/main/webapp/themes/common/js/onlineEnquiries/donorSearch.js
+++ web/src/main/webapp/themes/common/js/onlineEnquiries/donorSearch.js
@@ -1,43 +1,41 @@
-$(document).ready(function() {
-    function doClear() {
-        $('input[type="text"]').val("");
-        $('input[type="checkbox"]').prop("checked", false);
-        $("option:first").prop("selected", 'selected');
-        $(".current").text("Please Select");
-        $('.date_picker').val("");
-        $(".multi-select-button").html("-- Select --");
-        $('#cycleStageDisplay').attr("style","display: none");
-    }
+function doClear() {
+    $('input[type="text"]').val("");
+    $('input[type="checkbox"]').prop("checked", false);
+    $("option:first").prop("selected", 'selected');
+    $(".current").text("Please Select");
+    $('.date_picker').val("");
+    $(".multi-select-button").html("-- Select --");
+    $('#cycleStageDisplay').attr("style","display: none");
+}
 
 
-    function jumpToPagechangePage() {
-        search();
-    }
+function jumpToPagechangePage() {
+    search();
+}
 
-    function doSearch() {
-        $('input[name="pageJumpNoTextchangePage"]').val(1);
-        search();
-    }
+function doSearch() {
+    $('input[name="pageJumpNoTextchangePage"]').val(1);
+    search();
+}
 
-    function search() {
-        showWaiting();
-        $("[name='crud_action_type']").val('search');
-        $('#mainForm').submit();
-    }
+function search() {
+    showWaiting();
+    $("[name='crud_action_type']").val('search');
+    $('#mainForm').submit();
+}
 
-    function sortRecords(sortFieldName, sortType) {
-        $("[name='crud_action_value']").val(sortFieldName);
-        $("[name='crud_action_additional']").val(sortType);
-        $("[name='crud_action_type']").val('search');
-        $('#mainForm').submit();
-    }
+function sortRecords(sortFieldName, sortType) {
+    $("[name='crud_action_value']").val(sortFieldName);
+    $("[name='crud_action_additional']").val(sortType);
+    $("[name='crud_action_type']").val('search');
+    $('#mainForm').submit();
+}
 
 
-    var fullDetailsView = function (submissionNo) {
+var fullDetailsView = function (submissionNo) {
 
-        showWaiting();
-        $("[name='crud_action_value']").val(submissionNo);
-        $("[name='crud_action_type']").val('viewInfo');
-        $('#mainForm').submit();
-    }
-});
+    showWaiting();
+    $("[name='crud_action_value']").val(submissionNo);
+    $("[name='crud_action_type']").val('viewInfo');
+    $('#mainForm').submit();
+}
