208df92aa38f1ae0709527d7991b004f4fdb75d8
zhixing <zhixing@ecquaria.com>
2021-12-23 10:48:34 +0800

    76892

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/EndCycleDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/EndCycleDelegator.java
@@ -18,6 +18,7 @@ import com.ecquaria.cloud.moh.iais.helper.WebValidationHelper;
 import lombok.extern.slf4j.Slf4j;
 import sop.webflow.rt.api.BaseProcessClass;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -32,11 +33,6 @@ import java.util.Map;
 @Slf4j
 public class EndCycleDelegator extends CommonDelegator{
     @Override
-    public void start(BaseProcessClass bpc) {
-
-    }
-
-    @Override
     public void prepareSwitch(BaseProcessClass bpc) {
 
     }
@@ -44,8 +40,9 @@ public class EndCycleDelegator extends CommonDelegator{
     public void preparePage(BaseProcessClass bpc) {
         log.info(StringUtil.changeForLog("crud_action_type is ======>"+ParamUtil.getRequestString(bpc.request,IaisEGPConstant.CRUD_ACTION_TYPE)));
         ParamUtil.setRequestAttr(bpc.request, "smallTitle", "You are submitting for <strong>Cycle Stages</strong>");
-        ArSuperDataSubmissionDto arSuperDataSubmissionDto = DataSubmissionHelper.getCurrentArDataSubmission(bpc.request);
-        ParamUtil.setSessionAttr(bpc.request, DataSubmissionConstant.AR_DATA_SUBMISSION, arSuperDataSubmissionDto);
+        HttpServletRequest request = bpc.request;
+        ArSuperDataSubmissionDto arSuperDataSubmissionDto = DataSubmissionHelper.getCurrentArDataSubmission(request);
+        ParamUtil.setSessionAttr(request, DataSubmissionConstant.AR_DATA_SUBMISSION, arSuperDataSubmissionDto);
     }
 
     @Override
@@ -62,9 +59,6 @@ public class EndCycleDelegator extends CommonDelegator{
         arSuperDataSubmissionDto = arSuperDataSubmissionDto == null ? new ArSuperDataSubmissionDto() : arSuperDataSubmissionDto;
         EndCycleStageDto endCycleStageDto =
                 arSuperDataSubmissionDto.getEndCycleStageDto() == null ? new EndCycleStageDto() : arSuperDataSubmissionDto.getEndCycleStageDto();
-
-        String actionType = ParamUtil.getRequestString(bpc.request, IaisEGPConstant.CRUD_ACTION_TYPE);
-        if (CommonDelegator.ACTION_TYPE_CONFIRM.equals(actionType)) {
             String cycleAbandoned = ParamUtil.getString(bpc.request, "cycleAbandoned");
             String abandonReasonSelect = ParamUtil.getRequestString(bpc.request, "abandonReasonSelect");
             String otherAbandonReason = ParamUtil.getRequestString(bpc.request, "otherAbandonReason");
@@ -87,8 +81,6 @@ public class EndCycleDelegator extends CommonDelegator{
                 ParamUtil.setRequestAttr(bpc.request, IaisEGPConstant.CRUD_ACTION_TYPE, "page");
                 return;
             }
-            ParamUtil.setRequestAttr(bpc.request, IaisEGPConstant.CRUD_ACTION_TYPE, "confirm");
-        }
     }
 
     @Override
