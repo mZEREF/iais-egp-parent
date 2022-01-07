be3d17cbf4a521a72cf1847c13409ba768c306f3
chen <chenlei@ecquaria.com>
2022-01-07 08:50:47 +0800

    P2

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/PatientService.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/PatientService.java
@@ -1,12 +1,20 @@
 package com.ecquaria.cloud.moh.iais.service.datasubmission;
 
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.DataSubmissionDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.PatientDto;
+import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.PatientInfoDto;
 
 public interface PatientService {
+
+    DataSubmissionDto getPatientDataSubmissionByConds(String idType, String idNumber, String nationality, String orgId, String patientType);
+
+    PatientDto getActivePatientByConds(String idType, String idNumber, String nationality, String orgId, String patientType);
 
     PatientDto getArPatientDto(String idType, String idNumber, String nationality, String orgId);
 
     PatientDto getDpPatientDto(String idType, String idNumber, String nationality, String orgId);
 
     String getPatientCode(String patientCode);
+
+    PatientInfoDto getPatientInfoDto(String patientCode);
 }
