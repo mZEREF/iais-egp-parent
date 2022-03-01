2e66e0b07cd87d62b1e9896171bf5a122862f6e7
zhixing <zhixing@ecquaria.com>
2022-03-01 17:46:37 +0800

    a

--- hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohDataSubmission/1/src/sop/webflow/rt/java/code/INTERNET___MohDataSubmission___1.java
+++ hcsa-licence-fe/src/main/webapp/deployed/INTERNET/MohDataSubmission/1/src/sop/webflow/rt/java/code/INTERNET___MohDataSubmission___1.java
@@ -14,7 +14,7 @@ import com.ecquaria.cloud.helper.EngineHelper;
 
 import sop.webflow.rt.api.BaseProcessClass;
 
-public class INTERNET___MohDataSubmission___1 extends BaseProcessClass {
+public class INTERNET___MohDataSubmission___8 extends BaseProcessClass {
 	private static final String DELEGATOR ="dataSubmissionDelegator";
 
 	public void start_OnStepProcess_0() throws Exception {
