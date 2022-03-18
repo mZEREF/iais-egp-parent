c93f5100d07edcb828980a94da6b44ee736963a6
chen <chenlei@ecquaria.com>
2022-03-18 11:01:51 +0800

    80078

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/helper/NewApplicationHelper.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/helper/NewApplicationHelper.java
@@ -640,7 +640,6 @@ public class NewApplicationHelper {
                     if (isValid) {
                         if (stringList.contains(personKey)) {
                             errMap.put(keyIdNo, "NEW_ERR0012");
-                            isValid = false;
                         } else {
                             stringList.add(personKey);
                         }
@@ -854,7 +853,6 @@ public class NewApplicationHelper {
                         if (isValid) {
                             if (stringList.contains(personKey)) {
                                 oneErrorMap.put(keyIdNo, "NEW_ERR0012");
-                                isValid = false;
                             } else {
                                 stringList.add(personKey);
                             }
@@ -954,7 +952,6 @@ public class NewApplicationHelper {
                         if (isValid) {
                             if (stringList.contains(personKey)) {
                                 oneErrorMap.put(keyIdNo, "NEW_ERR0012");
-                                isValid = false;
                             } else {
                                 stringList.add(personKey);
                             }
@@ -969,7 +966,6 @@ public class NewApplicationHelper {
                                         subLicenseeDto.getIdType(), subLicenseeDto.getIdNumber());
                                 if (Objects.equals(subLicenseeKey, personKey)) {
                                     oneErrorMap.put("conflictError" + dpoIndex, MessageUtil.getMessageDesc("NEW_ERR0034"));
-                                    isValid = false;
                                 }
                             }
                         }
@@ -1421,7 +1417,6 @@ public class NewApplicationHelper {
                     if (isValid) {
                         if (stringList.contains(personKey)) {
                             errMap.put(keyIdNo, "NEW_ERR0012");
-                            isValid = false;
                         } else {
                             stringList.add(personKey);
                         }
@@ -1508,7 +1503,6 @@ public class NewApplicationHelper {
                     if (isValid) {
                         if (stringList.contains(personKey)) {
                             errMap.put(keyIdNo, "NEW_ERR0012");
-                            isValid = false;
                         } else {
                             stringList.add(personKey);
                         }
@@ -4043,6 +4037,7 @@ public class NewApplicationHelper {
             // check it only for Passport
             if ("-1".equals(nationality) || StringUtil.isEmpty(nationality)) {
                 errMap.put(keyNationality, MessageUtil.replaceMessage("GENERAL_ERR0006", "Country of issuance", "field"));
+                isValid = false;
             }
         }
         return isValid;
