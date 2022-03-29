2b4e8d65b08251e3bc0dec188078e610655018c5
jiawei <37257891+HeYaoDaDa@users.noreply.github.com>
2022-03-29 17:54:13 +0800

    Checklist retrieve common logic modificaiton and related APIs

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/constant/ChecklistSvcCode.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/constant/ChecklistSvcCode.java
@@ -1,21 +1,11 @@
 package sg.gov.moh.iais.egp.bsb.constant;
 
 public enum ChecklistSvcCode {
-    BCD("(BSL-3) First and/or Second Schedule Biological Agent"),
-    BCL("(BSL-3) Large-Scale Production of First Schedule Biological Agent"),
-    BCF("(BSL-3) Handling of Fifth Schedule Toxin"),
-    BCP("(BSL-3) Handling of Poliovirus Infectious Materials"),
-    BDD("(BSL-4) First and/or Second Schedule Biological Agent"),
-    BDL("(BSL-4) Large-Scale Production of First Schedule Biological Agent"),
-    BDF("(BSL-4) Fifth Schedule Toxin"),
-    BDP("(BSL-4) Poliovirus Infectious Materials"),
-    UFD("(UF) First Schedule Biological Agent"),
-    UFL("(UF) Large-Scale Production of Third Schedule Biological Agent"),
-    UFF("(UF) Fifth Schedule Toxin"),
-    LSF("(LSP) Large-Scale Production of First Schedule Biological Agent"),
-    LST("(LSP) Large-Scale Production of Third Schedule Biological Agent"),
-    RFF("(RF) Handling of Fifth Schedule Toxin for Exempted Purposes"),
-    RFP("(RF) Handling of Poliovirus Potentially Infectious Materials")
+    CHC("Certified High Containment (BSL-3) Facility"),
+    CMC("Certified Maximum Containment (BSL-4) Facility"),
+    UFT("Uncertified Facility"),
+    LSP("Biomanufacturing (Large-Scale Production) Facility"),
+    RFT("Registered Facility")
     ;
 
     private final String desc;
