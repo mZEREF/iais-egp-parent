58d495fd750d3fbb30e85543288e8c0a76fa57f4
Jinhua <jinhua@ecquaria.com>
2022-04-06 17:43:40 +0800

    Create event bus callback tracking

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EventBusClientClientFallBack.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/service/client/EventBusClientClientFallBack.java
@@ -1,6 +1,7 @@
 package com.ecquaria.cloud.moh.iais.service.client;
 
 import com.ecquaria.cloud.moh.iais.common.dto.system.EventCallbackTrackDto;
+import com.ecquaria.cloud.submission.client.model.GetSubmissionStatusResp;
 import com.ecquaria.cloud.submission.client.model.SubmitReq;
 import com.ecquaria.cloudfeign.FeignResponseEntity;
 import java.util.List;
@@ -39,6 +40,14 @@ public class EventBusClientClientFallBack implements EventBusClient{
 
     @Override
     public FeignResponseEntity<Void> updateCallbackTracking(EventCallbackTrackDto dto) {
+        FeignResponseEntity entity = new FeignResponseEntity<>();
+        HttpHeaders headers = new HttpHeaders();
+        entity.setHeaders(headers);
+        return entity;
+    }
+
+    @Override
+    public FeignResponseEntity<GetSubmissionStatusResp> getSubmissionStatus(String submissionId, String operation) {
         FeignResponseEntity entity = new FeignResponseEntity<>();
         HttpHeaders headers = new HttpHeaders();
         entity.setHeaders(headers);
