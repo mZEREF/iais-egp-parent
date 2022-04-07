f2f40d8879d416de3c6b50841857cda4580e6ed8
Jinhua <jinhua@ecquaria.com>
2022-04-07 09:41:31 +0800

    For merge

--- systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/action/MasterCodeDelegator.java
+++ systemadmin/src/main/java/com/ecquaria/cloud/moh/iais/action/MasterCodeDelegator.java
@@ -960,30 +960,30 @@ public class MasterCodeDelegator {
                 errorMap.put("codeValue", errMsg);
                 WebValidationHelper.saveAuditTrailForNoUseResult(errorMap);
             }
-        if (IaisCommonUtils.isNotEmpty(errorMap)) {
-            ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ERROR_MSG, WebValidationHelper.generateJsonStr(errorMap));
-            ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ISVALID, SystemAdminBaseConstants.NO);
-            ParamUtil.setRequestAttr(request, "codeCategory", ParamUtil.getString(request, MasterCodeConstants.MASTER_CODE_CATEGORY));
-            return;
+            if (IaisCommonUtils.isNotEmpty(errorMap)) {
+                ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ERROR_MSG, WebValidationHelper.generateJsonStr(errorMap));
+                ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ISVALID, SystemAdminBaseConstants.NO);
+                ParamUtil.setRequestAttr(request, "codeCategory", ParamUtil.getString(request, MasterCodeConstants.MASTER_CODE_CATEGORY));
+                return;
+            }
+            log.info(StringUtil.changeForLog("isEffect:"+isEffect));
+            if(!isEffect){
+                masterCodeDto.setStatus(AppConsts.COMMON_STATUS_IACTIVE);
+            }
+            MasterCodeDto msDto = masterCodeService.saveMasterCode(masterCodeDto);
+            //eic
+            List<MasterCodeDto> syncMasterCodeList = IaisCommonUtils.genNewArrayList();
+            msDto.setUpdateAt(new Date());
+            syncMasterCodeList.add(msDto);
+            masterCodeService.syncMasterCodeFe(syncMasterCodeList);
+            MasterCodeUtil.refreshCache();
+            ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ISVALID, SystemAdminBaseConstants.YES);
+            Date date = new Date();
+            String dateStr = Formatter.formatDateTime(date);
+            String dateReplace = dateStr.replace(" "," at ");
+            String ackMsg = MessageUtil.replaceMessage("ACKMCM001",dateReplace,"Date");
+            ParamUtil.setRequestAttr(request,"CREATE_ACKMSG",ackMsg);
         }
-        log.info(StringUtil.changeForLog("isEffect:"+isEffect));
-        if(!isEffect){
-            masterCodeDto.setStatus(AppConsts.COMMON_STATUS_IACTIVE);
-        }
-        MasterCodeDto msDto = masterCodeService.saveMasterCode(masterCodeDto);
-        //eic
-        List<MasterCodeDto> syncMasterCodeList = IaisCommonUtils.genNewArrayList();
-        msDto.setUpdateAt(new Date());
-        syncMasterCodeList.add(msDto);
-        masterCodeService.syncMasterCodeFe(syncMasterCodeList);
-        MasterCodeUtil.refreshCache();
-        ParamUtil.setRequestAttr(request, SystemAdminBaseConstants.ISVALID, SystemAdminBaseConstants.YES);
-        Date date = new Date();
-        String dateStr = Formatter.formatDateTime(date);
-        String dateReplace = dateStr.replace(" "," at ");
-        String ackMsg = MessageUtil.replaceMessage("ACKMCM001",dateReplace,"Date");
-        ParamUtil.setRequestAttr(request,"CREATE_ACKMSG",ackMsg);
-
     }
 
     /**
