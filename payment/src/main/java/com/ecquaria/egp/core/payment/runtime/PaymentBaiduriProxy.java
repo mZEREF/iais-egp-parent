c334a479c6aa0d5ef179c28d97982ec340d9fede
chen <chenlei@ecquaria.com>
2022-04-11 17:23:14 +0800

    Sonar Issue

--- payment/src/main/java/com/ecquaria/egp/core/payment/runtime/PaymentBaiduriProxy.java
+++ payment/src/main/java/com/ecquaria/egp/core/payment/runtime/PaymentBaiduriProxy.java
@@ -295,7 +295,8 @@ public class PaymentBaiduriProxy extends PaymentProxy {
 		return fields;
 	}
 
-	private String hashAllFields(Map<String, String> fields, String secureHashType) throws NoSuchAlgorithmException {
+	private String hashAllFields(Map<String, String> fields, String secureHashType)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		// create a list and sort it
 		List<String> fieldNames = new ArrayList<String>(fields.keySet());
 		Collections.sort(fieldNames);
@@ -320,7 +321,8 @@ public class PaymentBaiduriProxy extends PaymentProxy {
 		}
 
 		// create the SHA-256(default) hash and UTF-8 encode it
-		MessageDigest md = MessageDigest.getInstance(MessageDigest.isEqual(DEFAULT_SECURE_HASH_TYPE.getBytes(), secureHashType.getBytes()) ? "SHA-256" : secureHashType);
+		MessageDigest md = MessageDigest.getInstance(MessageDigest.isEqual(DEFAULT_SECURE_HASH_TYPE.getBytes(AppConsts.DFT_CHARSET),
+				secureHashType.getBytes(AppConsts.DFT_CHARSET)) ? "SHA-256" : secureHashType);
 		byte[] ba = md.digest(SMCStringHelperUtil.getStringBytes(buf.toString()));
 
 
