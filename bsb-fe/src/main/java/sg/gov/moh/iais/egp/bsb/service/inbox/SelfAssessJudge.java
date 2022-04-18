c53cec19ce01a3a2dd1923a08489e6d40ea19045
jiawei <37257891+HeYaoDaDa@users.noreply.github.com>
2022-04-18 17:18:55 +0800

    pre inspection rfi fe

--- bsb-fe/src/main/java/sg/gov/moh/iais/egp/bsb/service/inbox/SelfAssessJudge.java
+++ bsb-fe/src/main/java/sg/gov/moh/iais/egp/bsb/service/inbox/SelfAssessJudge.java
@@ -2,6 +2,8 @@ package sg.gov.moh.iais.egp.bsb.service.inbox;
 
 import sg.gov.moh.iais.egp.bsb.constant.MasterCodeConstants;
 
+import java.util.Arrays;
+
 
 public class SelfAssessJudge implements AppActionJudge {
     private final String appStatus;
@@ -12,6 +14,9 @@ public class SelfAssessJudge implements AppActionJudge {
 
     @Override
     public boolean judge() {
-        return MasterCodeConstants.APP_STATUS_PEND_SUBMIT_SELF_ASSESSMENT.equals(appStatus);
+        return Arrays.asList(
+                MasterCodeConstants.APP_STATUS_PEND_SUBMIT_SELF_ASSESSMENT,
+                MasterCodeConstants.APP_STATUS_PEND_INSPECTION_READINESS,
+                MasterCodeConstants.APP_STATUS_PEND_CLARIFICATION).contains(appStatus);
     }
 }
