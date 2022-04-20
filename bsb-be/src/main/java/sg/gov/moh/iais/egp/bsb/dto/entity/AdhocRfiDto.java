8e5591f31edf7e818e0a58bef509410a29cfff17
renfanghao <fanghao@ecquaria.com>
2022-04-20 17:57:33 +0800

    bsb adhoc Rfi

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/entity/AdhocRfiDto.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/entity/AdhocRfiDto.java
@@ -21,6 +21,8 @@ public class AdhocRfiDto extends BaseEntityDto{
 
     private String facilityNo;
 
+    private String approvalNo;
+
     private String submissionType;
 
     private String email;
