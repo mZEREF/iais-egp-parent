006f43214526124ed09fcd6b45e0a583c5d2a94a
zhixing <zhixing@ecquaria.com>
2022-04-22 10:00:22 +0800

    p2

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/MohDsDraftDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/MohDsDraftDelegator.java
@@ -134,11 +134,6 @@ public class MohDsDraftDelegator {
         if (topSuper == null) {
             uri = DEFAULT_URI;
         } else {
-            /*if (DataSubmissionConsts.TOP_TYPE_SBT_PATIENT_INFO.equals(topSuper.getSubmissionType())) {
-                uri = InboxConst.URL_LICENCE_WEB_MODULE + "MohNewTOPDataSubmission/PatientInformation";
-            } else if (DataSubmissionConsts.TOP_TYPE_SBT_TERMINATION_OF_PRE.equals(topSuper.getSubmissionType())) {
-                uri = InboxConst.URL_LICENCE_WEB_MODULE + "MohNewTOPDataSubmission/TerminationOfPregnancy";
-            }*/
             uri = InboxConst.URL_LICENCE_WEB_MODULE + "MohTOPDataSubmission/PrepareSwitch";
         }
         DataSubmissionHelper.setCurrentTopDataSubmission(topSuper, request);
