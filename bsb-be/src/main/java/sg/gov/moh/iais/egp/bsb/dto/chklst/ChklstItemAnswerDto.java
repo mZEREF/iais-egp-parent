8a08eb440cb7dc75a1e1ae56123b9a012e8c123f
Jinhua <jinhua@ecquaria.com>
2022-04-25 14:13:46 +0800

    Combine check list result

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/chklst/ChklstItemAnswerDto.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/chklst/ChklstItemAnswerDto.java
@@ -1,6 +1,5 @@
 package sg.gov.moh.iais.egp.bsb.dto.chklst;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,12 +7,21 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ChklstItemAnswerDto implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    private String userId;
+    private String snNo;
     private String configId;
     private String sectionId;
     private String itemId;
     private String answer;
     private String remarks;
+    private String findings;
+    private String actionRequired;
+    private boolean rectified;
+    private String followupItem;
+    private String observeFollowup;
+    private String followupAction;
+    private String dueDate;
 }
