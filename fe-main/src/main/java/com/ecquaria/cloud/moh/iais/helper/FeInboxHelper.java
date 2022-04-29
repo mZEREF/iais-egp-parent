0c9d817d1e94e96f41ca7de102e98b3558f05459
wangyu <wangyu@ecquaria.com>
2022-04-29 18:18:52 +0800

    remove ar sub type

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/helper/FeInboxHelper.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/helper/FeInboxHelper.java
@@ -1,5 +1,6 @@
 package com.ecquaria.cloud.moh.iais.helper;
 import com.ecquaria.cloud.moh.iais.common.constant.dataSubmission.DataSubmissionConsts;
+import com.ecquaria.cloud.moh.iais.common.constant.privilege.PrivilegeConsts;
 import com.ecquaria.cloud.moh.iais.common.dto.SelectOption;
 import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 public final class FeInboxHelper {
@@ -46,6 +48,33 @@ public final class FeInboxHelper {
         }
         return selectOptions;
     }
+
+    public static List<SelectOption> getSubmissionTypes(List<String> privilegeIds){
+        List<String> subTypes = IaisCommonUtils.genNewArrayList(13);
+        privilegeIds.stream().forEach(privilegeId ->{
+            switch(privilegeId){
+                case PrivilegeConsts.USER_PRIVILEGE_DS_AR :
+                    subTypes.addAll(DataSubmissionConsts.DS_CYCLE_AR_TYPE_LIST);
+                    break;
+                case PrivilegeConsts.USER_PRIVILEGE_DS_DP :
+                    subTypes.addAll(DataSubmissionConsts.DS_CYCLE_DP_TYPE_LIST);
+                    break;
+                case PrivilegeConsts.USER_PRIVILEGE_DS_TOP:
+                    subTypes.addAll(DataSubmissionConsts.DS_CYCLE_TOP_TYPE_LIST);
+                    break;
+                case PrivilegeConsts.USER_PRIVILEGE_DS_VSS:
+                    subTypes.addAll(DataSubmissionConsts.DS_CYCLE_VSS_TYPE_LIST);
+                    break;
+                case PrivilegeConsts.USER_PRIVILEGE_DS_LDT:
+                    subTypes.addAll(DataSubmissionConsts.DS_CYCLE_LDT_TYPE_LIST);
+                    break;
+                default: break;
+            }
+        });
+        return IaisCommonUtils.isNotEmpty(subTypes) ?
+                MasterCodeUtil.retrieveOptionsByCate(MasterCodeUtil.DATA_SUBMISSION_TYPE).stream().filter( selectOption -> subTypes.contains(selectOption.getValue())).collect(Collectors.toList()) : null;
+    }
+
 
     private FeInboxHelper(){
         throw new IllegalStateException("Utility class");
