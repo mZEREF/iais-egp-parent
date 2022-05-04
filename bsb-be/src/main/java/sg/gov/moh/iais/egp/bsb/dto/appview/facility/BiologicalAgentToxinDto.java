4ee99fd8726e7bc38473fb8a91527fd59dd8c06f
liran <1780654737@qq.com>
2022-05-04 09:38:52 +0800

    update be view facility registration bat application

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/appview/facility/BiologicalAgentToxinDto.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/dto/appview/facility/BiologicalAgentToxinDto.java
@@ -12,31 +12,39 @@ public class BiologicalAgentToxinDto {
     @NoArgsConstructor
     public static class BATInfo implements Serializable {
         private String schedule;
-
         private String batName;
-
         private List<String> sampleType;
-
         private List<String> workType;
-
         private String sampleWorkDetail;
-
         private String procurementMode;
-
-        private String postalCode;
-        private String addressType;
-        private String blockNo;
-        private String floorNo;
-        private String unitNo;
-        private String streetName;
-        private String buildingName;
-
-        private String contactPersonName;
-        private String emailAddress;
-        private String contactNo;
-        private String expectedDate;
-        private String courierServiceProviderName;
-        private String remarks;
+        private String postalCodeT;
+        private String addressTypeT;
+        private String blockNoT;
+        private String floorNoT;
+        private String unitNoT;
+        private String streetNameT;
+        private String buildingNameT;
+        private String facNameE;
+        private String postalCodeE;
+        private String blockNoE;
+        private String floorNoE;
+        private String unitNoE;
+        private String streetNameE;
+        private String countryE;
+        private String stateE;
+        private String contactPersonNameT;
+        private String emailAddressT;
+        private String contactNoT;
+        private String expectedDateT;
+        private String courierServiceProviderNameT;
+        private String remarksT;
+        private String contactPersonNameE;
+        private String emailAddressE;
+        private String contactNoE;
+        private String expectedDateE;
+        private String courierServiceProviderNameE;
+        private String remarksE;
+        private String toxinRegulationDeclare;
     }
 
     private String activityEntityId;
