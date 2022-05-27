8127613bf8071658a81b3ab8250cab9182698de0
yiming <1614697209@qq.com>
2022-05-27 13:44:56 +0800

    update js add commit remove disabled

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/client/ProcessClient.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/client/ProcessClient.java
@@ -3,12 +3,15 @@ package sg.gov.moh.iais.egp.bsb.client;
 import com.ecquaria.cloudfeign.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import sg.gov.moh.iais.egp.bsb.dto.ResponseDto;
 import sg.gov.moh.iais.egp.bsb.dto.inspection.ReportDto;
 import sg.gov.moh.iais.egp.bsb.dto.inspection.afc.ReviewAFCReportDto;
+import sg.gov.moh.iais.egp.bsb.dto.process.MohProcessDto;
 import sg.gov.moh.iais.egp.bsb.dto.validation.ValidationResultDto;
-import sg.gov.moh.iais.egp.bsb.dto.process.*;
 
 
 /**
