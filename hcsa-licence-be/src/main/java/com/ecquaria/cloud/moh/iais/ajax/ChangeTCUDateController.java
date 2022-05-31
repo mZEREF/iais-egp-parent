3f738cd5c1f4f2287fa8274c732b6836fd1b9aa6
jiawei <37257891+HeYaoDaDa@users.noreply.github.com>
2022-05-31 09:08:00 +0800

    82164

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/ajax/ChangeTCUDateController.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/ajax/ChangeTCUDateController.java
@@ -5,9 +5,11 @@ import com.ecquaria.cloud.moh.iais.common.dto.SearchParam;
 import com.ecquaria.cloud.moh.iais.common.dto.SearchResult;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.licence.LicPremisesQueryDto;
 import com.ecquaria.cloud.moh.iais.common.utils.Formatter;
+import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
 import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.constant.HcsaLicenceBeConstant;
+import com.ecquaria.cloud.moh.iais.dto.ExcelSheetDto;
 import com.ecquaria.cloud.moh.iais.helper.FileUtils;
 import com.ecquaria.cloud.moh.iais.helper.QueryHelp;
 import com.ecquaria.cloud.moh.iais.helper.excel.ExcelWriter;
@@ -23,7 +25,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -51,10 +55,29 @@ public class ChangeTCUDateController {
 
         if (!Objects.isNull(results)) {
             List<LicPremisesQueryDto> queryList = results.getRows();
-            queryList.forEach(i -> i.setTcuDateStr(Formatter.formatDateTime(i.getTcuDate(), AppConsts.DEFAULT_DATE_FORMAT)));
+            int count = 1;
+            for (LicPremisesQueryDto queryDto : queryList) {
+                queryDto.setTcuDateStr(Formatter.formatDateTime(queryDto.getTcuDate(), AppConsts.DEFAULT_DATE_FORMAT));
+                queryDto.setSn(count++);
+            }
+
+            ExcelSheetDto excelSheetDto = new ExcelSheetDto();
+            excelSheetDto.setSheetAt(0);
+            excelSheetDto.setSheetName("sheet");
+            excelSheetDto.setBlock(false);
+            excelSheetDto.setPwd(Formatter.formatDateTime(new Date(), "yyyyMMdd"));
+            excelSheetDto.setStartRowIndex(1);
+            excelSheetDto.setSource(queryList);
+            excelSheetDto.setSourceClass(LicPremisesQueryDto.class);
+            excelSheetDto.setDefaultRowHeight((short) 600);
+            excelSheetDto.setChangeHeight(true);
+            excelSheetDto.setNeedFiled(true);
+            Map<Integer, Integer> widthMap = IaisCommonUtils.genNewHashMap(1);
+            widthMap.put(7, 50);
+            excelSheetDto.setWidthMap(widthMap);
 
             try {
-                file = ExcelWriter.writerToExcel(queryList, LicPremisesQueryDto.class, "ChangeTCUDate_SearchResult_Download");
+                file = ExcelWriter.writerToExcel(excelSheetDto, null, "ChangeTCUDate_SearchResult_Download");
             } catch (Exception e) {
                 log.error("=======>fileHandler error >>>>>", e);
             }
