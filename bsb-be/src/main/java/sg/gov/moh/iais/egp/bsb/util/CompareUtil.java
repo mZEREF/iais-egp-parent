e04969126a17fcc5e58d0773e76dff6813d5b42d
liran <1780654737@qq.com>
2022-06-23 14:04:35 +0800

    update CompareUtil not null judge logic

--- bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/util/CompareUtil.java
+++ bsb-be/src/main/java/sg/gov/moh/iais/egp/bsb/util/CompareUtil.java
@@ -1,10 +1,11 @@
 package sg.gov.moh.iais.egp.bsb.util;
 
+import com.ecquaria.cloud.moh.iais.common.exception.IaisRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 import sg.gov.moh.iais.egp.bsb.dto.compare.CompareWrap;
 
-import java.util.ArrayList;                                                                                                                                                                       
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -35,35 +36,33 @@ public class CompareUtil {
         remainList.addAll(intersectionList);
     }
 
-    /**
-     *
-     * @param oldList
-     * @param newList
-     * @param function
-     * @param clazz
-     * @param <T>
-     * @return
-     */
     public static <T> List<CompareWrap<T>> compareAndAssembleList (List<T> oldList, List<T> newList, Function<T, String> function, Class<T> clazz) {
-        List<String> oldMarkList = oldList.stream().map(function).collect(Collectors.toList());
-        List<String> newMarkList = oldList.stream().map(function).collect(Collectors.toList());
-        List<String> addList = new ArrayList<>();
-        List<String> deleteList = new ArrayList<>();
-        List<String> remainList = new ArrayList<>();
-        filterStringList(oldMarkList, newMarkList, addList, deleteList, remainList);
-
-        List<CompareWrap<T>> compareWrapList = new ArrayList<>(oldList.size() + newList.size());
-        Map<String, T> oldMap = sg.gov.moh.iais.egp.bsb.util.CollectionUtils.uniqueIndexMap(oldList, function);
-        Map<String, T> newMap = sg.gov.moh.iais.egp.bsb.util.CollectionUtils.uniqueIndexMap(newList, function);
-
+        List<CompareWrap<T>> compareWrapList = new ArrayList<>();
+        T t;
         try {
-            T t = clazz.getConstructor().newInstance();
+            t = clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new IaisRuntimeException("No arguments constructor method", e);
+        }
+
+        if (CollectionUtils.isEmpty(oldList) && !CollectionUtils.isEmpty(newList)) {
+            newList.forEach(s -> compareWrapList.add(new CompareWrap<>(t, s)));
+        } else if (!CollectionUtils.isEmpty(oldList) && CollectionUtils.isEmpty(newList)) {
+            oldList.forEach(s -> compareWrapList.add(new CompareWrap<>(s, t)));
+        } else if (!CollectionUtils.isEmpty(oldList) && !CollectionUtils.isEmpty(newList)) {
+            List<String> oldMarkList = oldList.stream().map(function).collect(Collectors.toList());
+            List<String> newMarkList = newList.stream().map(function).collect(Collectors.toList());
+            List<String> addList = new ArrayList<>();
+            List<String> deleteList = new ArrayList<>();
+            List<String> remainList = new ArrayList<>();
+            filterStringList(oldMarkList, newMarkList, addList, deleteList, remainList);
+
+            Map<String, T> oldMap = sg.gov.moh.iais.egp.bsb.util.CollectionUtils.uniqueIndexMap(oldList, function);
+            Map<String, T> newMap = sg.gov.moh.iais.egp.bsb.util.CollectionUtils.uniqueIndexMap(newList, function);
+
             remainList.forEach(s -> compareWrapList.add(new CompareWrap<>(oldMap.get(s), newMap.get(s))));
             deleteList.forEach(s -> compareWrapList.add(new CompareWrap<>(oldMap.get(s), t)));
             addList.forEach(s -> compareWrapList.add(new CompareWrap<>(t, newMap.get(s))));
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("Object gets instance errors through reflection!");
         }
         return compareWrapList;
     }
