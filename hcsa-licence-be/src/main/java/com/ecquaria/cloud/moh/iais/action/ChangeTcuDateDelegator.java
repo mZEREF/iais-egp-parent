8d1795cc5fe12250718b2e3d0f95b2d3403799b6
jiawei <37257891+HeYaoDaDa@users.noreply.github.com>
2022-06-27 15:16:46 +0800

    fix duplicate message

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/ChangeTcuDateDelegator.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/action/ChangeTcuDateDelegator.java
@@ -129,7 +129,7 @@ public class ChangeTcuDateDelegator {
         if (ACTION_CHANGE_TCU_DATE.equals(action)) {
             String[] checkBoxItemIds = ParamUtil.getStrings(request, PARAM_CHKL_ITEM_CHECKBOX);
             if (checkBoxItemIds == null || checkBoxItemIds.length == 0) {
-                ParamUtil.setRequestAttr(request, IaisEGPConstant.ERRORMSG, WebValidationHelper.generateJsonStr("premiseItems", MessageUtil.getMessageDesc("GENERAL_ERR0057")));
+                ParamUtil.setRequestAttr(request, IaisEGPConstant.ERRORMSG, WebValidationHelper.generateJsonStr("premiseItems", MessageUtil.getMessageDesc("GENERAL_ERR0067")));
                 action = ACTION_PREMISE_LIST;
             } else {
                 SearchResult<LicPremisesQueryDto> searchResult = (SearchResult<LicPremisesQueryDto>) ParamUtil.getSessionAttr(request, HcsaLicenceBeConstant.SEARCH_RESULT_CHANGE_TUC_DATE);
