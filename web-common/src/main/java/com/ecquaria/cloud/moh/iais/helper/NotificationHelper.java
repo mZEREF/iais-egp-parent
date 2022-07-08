c0482173ba24ac17eeb4d88ea970924b0de44545
Jinhua <jinhua@ecquaria.com>
2022-07-08 14:39:31 +0800

    Add roles

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/NotificationHelper.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/helper/NotificationHelper.java
@@ -88,6 +88,16 @@ public class NotificationHelper {
 
 	public static final String RECEIPT_ROLE_LICENSEE                			= "EM-LIC";
 	public static final String RECEIPT_ROLE_LICENSEE_ALL                		= "EM-LIC-ALL";
+	public static final String RECEIPT_ROLE_LICENSEE_AR_SUBMITTER               = "EM-DS-AR";
+	public static final String RECEIPT_ROLE_LICENSEE_DP_SUBMITTER               = "EM-DS-DP";
+	public static final String RECEIPT_ROLE_LICENSEE_VS_SUBMITTER               = "EM-DS-VS";
+	public static final String RECEIPT_ROLE_LICENSEE_LDT_SUBMITTER              = "EM-DS-LDT";
+	public static final String RECEIPT_ROLE_LICENSEE_TOP_SUBMITTER              = "EM-DS-TOP";
+	public static final String RECEIPT_ROLE_LICENSEE_AR_SUPERVISOR              = "EM-DR-AR";
+	public static final String RECEIPT_ROLE_LICENSEE_DP_SUPERVISOR              = "EM-DR-DP";
+	public static final String RECEIPT_ROLE_LICENSEE_VS_SUPERVISOR              = "EM-DR-VS";
+	public static final String RECEIPT_ROLE_LICENSEE_LDT_SUPERVISOR             = "EM-DR-LDT";
+	public static final String RECEIPT_ROLE_LICENSEE_TOP_SUPERVISOR             = "EM-DR-TOP";
 	public static final String RECEIPT_ROLE_TRANSFEREE_LICENSEE                	= "TRANSFEREE";
 	public static final String RECEIPT_ROLE_AUTHORISED_PERSON       			= "EM-AP";
 	public static final String RECEIPT_ROLE_ASSIGNED_ASO            			= "EM-A-ASO";
