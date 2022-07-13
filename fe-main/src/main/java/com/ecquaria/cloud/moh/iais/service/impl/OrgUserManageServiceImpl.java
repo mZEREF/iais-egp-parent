0cb4e71e9700a71e649c379fa0d00d9e10616f1c
junyu <junyu@ecquaria.com>
2022-07-13 10:25:51 +0800

    addlist

--- fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/OrgUserManageServiceImpl.java
+++ fe-main/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/OrgUserManageServiceImpl.java
@@ -46,9 +46,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sop.rbac.user.UserIdentifier;
 
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -585,11 +583,17 @@ public class OrgUserManageServiceImpl implements OrgUserManageService {
             log.info(StringUtil.changeForLog("The Licensee Id: " + licenseeId));
             List<DsCenterDto> dsCenterDtos = licenceClient.getDsCenterDtosByLicenseeId(licenseeId).getEntity();
             if (IaisCommonUtils.isNotEmpty(dsCenterDtos)) {
-                dsCenterDtos.stream().forEach(dsCenterDto -> IaisCommonUtils.addToList(dsCenterDto.getCenterType(), data));
+                for (DsCenterDto dsCenterDto:dsCenterDtos
+                     ) {
+                    data.add(dsCenterDto.getCenterType());
+                }
             }
             List<LicenceDto> licenceDtos = licenceClient.getActiveLicencesByLicenseeId(licenseeId).getEntity();
             if (IaisCommonUtils.isNotEmpty(licenceDtos)) {
-                licenceDtos.stream().forEach(licenceDto -> IaisCommonUtils.addToList(licenceDto.getSvcName(), data));
+                for (LicenceDto licenceDto:licenceDtos
+                ) {
+                    data.add(licenceDto.getSvcName());
+                }
             }
         }
         List<String> roles = IaisEGPHelper.getFeRoles(data);
