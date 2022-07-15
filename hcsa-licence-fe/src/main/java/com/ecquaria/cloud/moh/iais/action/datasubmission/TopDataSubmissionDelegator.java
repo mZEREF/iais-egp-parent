6940d57a909f1c657fa355795e3137f327824f2b
yiming <1614697209@qq.com>
2022-07-15 13:28:17 +0800

    update data-submission  content-params of email

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/TopDataSubmissionDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/TopDataSubmissionDelegator.java
@@ -1775,7 +1775,7 @@ public class TopDataSubmissionDelegator {
     private void sendRfcMsgAndEmail(String licenseeId, String submitterName, String submissionNo) throws IOException, TemplateException {
         MsgTemplateDto msgTemplateDto = licenceFeMsgTemplateClient.getMsgTemplate(MsgTemplateConstants.MSG_TEMPLATE_DS_SUBMITTED_RFC_ACK_MSG).getEntity();
         Map<String, Object> msgContentMap = IaisCommonUtils.genNewHashMap();
-        msgContentMap.put("dataSupervisor", submitterName);
+        msgContentMap.put("ApplicantName", submitterName);
         msgContentMap.put("submissionId", submissionNo);
         msgContentMap.put("date", Formatter.formatDateTime(new Date(),"dd/MM/yyyy HH:mm:ss"));
         msgContentMap.put("MOH_AGENCY_NAME", AppConsts.MOH_AGENCY_NAME);
