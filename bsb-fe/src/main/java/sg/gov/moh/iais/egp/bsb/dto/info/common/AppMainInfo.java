6b6be69bfff22450b8325f19c58acc7859ebb3e6
chenwei <chenwei@toppanecquaria.com>
2022-07-28 17:59:49 +0800

    Bsb remove stageId and some useless columns

--- bsb-fe/src/main/java/sg/gov/moh/iais/egp/bsb/dto/info/common/AppMainInfo.java
+++ bsb-fe/src/main/java/sg/gov/moh/iais/egp/bsb/dto/info/common/AppMainInfo.java
@@ -1,7 +1,6 @@
 package sg.gov.moh.iais.egp.bsb.dto.info.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.googlecode.jmapper.annotations.JMap;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,10 +19,6 @@ public class AppMainInfo extends AppBasicInfo {
 
     @JMap
     private String assigned;
-
-    @JMap
-    @JsonProperty("stage_id")
-    private String stageId;
 
     @JMap
     private String facilityNo;
