f49270e78649df5507124a780a5a48b1b0c6deb9
Jinhua <jinhua@ecquaria.com>
2022-08-26 16:08:22 +0800

    Fix security issue

--- web-common/src/main/java/com/ecquaria/cloud/moh/iais/tags/InputTag.java
+++ web-common/src/main/java/com/ecquaria/cloud/moh/iais/tags/InputTag.java
@@ -79,7 +79,7 @@ public class InputTag extends DivTagSupport{
         }
 
         if (!StringUtil.isEmpty(value)) {
-            html.append(" value=\"").append(StringUtil.viewHtml(value)).append('\"');
+            html.append(" value=\"").append(StringUtil.viewNonNullHtml(value)).append('\"');
         }
         if(!StringUtil.isEmpty(autocomplete)){
             html.append(" maxlength=\"").append(maxLength).append('\"');
