786ad72c8c6e76f2cd1588a0e3867fbc175b6eb8
shu <shufei>
2022-09-02 13:44:26 +0800

    shufei

--- hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppDataHelper.java
+++ hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppDataHelper.java
@@ -70,7 +70,6 @@ import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
