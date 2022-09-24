5cea3aab1fb947449f66800976b9c1e4f84df766
shu <shufei>
2022-09-24 13:49:25 +0800

    outsource

--- hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/action/ServiceInfoDelegator.java
+++ hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/action/ServiceInfoDelegator.java
@@ -2005,7 +2005,7 @@ public class ServiceInfoDelegator {
     private boolean skipStep(String stepCode, AppSubmissionDto appSubmissionDto) {
         String[] skipList = new String[]{HcsaConsts.STEP_LABORATORY_DISCIPLINES,
                 HcsaConsts.STEP_DISCIPLINE_ALLOCATION,
-//                HcsaConsts.STEP_OUTSOURCED_PROVIDERS
+                HcsaConsts.STEP_OUTSOURCED_PROVIDERS
 //                HcsaConsts.STEP_PRINCIPAL_OFFICERS,
 //                HcsaConsts.STEP_SERVICE_PERSONNEL,
 //                HcsaConsts.STEP_KEY_APPOINTMENT_HOLDER,
