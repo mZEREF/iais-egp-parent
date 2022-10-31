541fbe828f8ecaa3f4f7b09a4902e8adabcad2df
huangkun <huangkun@ecquaria.com>
2022-10-31 16:45:54 +0800

    SSI

--- hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppDataHelper.java
+++ hcsa-licence-comm/src/main/java/com/ecquaria/cloud/moh/iais/helper/AppDataHelper.java
@@ -2586,7 +2586,6 @@ public final class AppDataHelper {
 
     public static List<AppSvcSpecialServiceInfoDto> getAppSvcSpecialServiceInfoList(HttpServletRequest request,
             List<AppSvcSpecialServiceInfoDto> appSvcSpecialServiceInfoDtoList, String appType) {
-
         if (!IaisCommonUtils.isEmpty(appSvcSpecialServiceInfoDtoList)) {
             String prefix = "";
             int i = 0;
@@ -2684,7 +2683,7 @@ public final class AppDataHelper {
                     }
                     if (!IaisCommonUtils.isEmpty(specialServiceSectionDto.getAppSvcSuplmFormDto().getAppSvcSuplmGroupDtoList())) {
                         String isPartEdit = ParamUtil.getString(request, prefix + i + j + "isPartEdit" + "Sup");
-                        if (AppConsts.YES.equals(isPartEdit)) {
+                        if (AppConsts.YES.equals(isPartEdit)||ApplicationConsts.APPLICATION_TYPE_NEW_APPLICATION.equals(appType)) {
                             setAppSvcSuplmFormDto(specialServiceSectionDto.getAppSvcSuplmFormDto(), prefix + i + j, request);
                         }
                     }
