e1d0b357805b1e841a1ead3f1d51756db912d97a
shu <shufei>
2023-02-14 17:35:03 +0800

    batch upload sfo and transferInOut service
    # Conflicts:
    #	hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArIUIDataSubmissionDelegator.java

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArIUIDataSubmissionDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/ArIUIDataSubmissionDelegator.java
@@ -50,6 +50,13 @@ import com.ecquaria.cloud.moh.iais.service.datasubmission.PatientService;
 import com.ecquaria.cloud.moh.iais.service.datasubmission.SfoCycleUploadService;
 import com.ecquaria.cloud.moh.iais.service.datasubmission.TransferInOutCycleUploadService;
 import com.ecquaria.cloud.moh.iais.service.datasubmission.impl.NonPatientDonorSampleUploadServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import sop.webflow.rt.api.BaseProcessClass;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
