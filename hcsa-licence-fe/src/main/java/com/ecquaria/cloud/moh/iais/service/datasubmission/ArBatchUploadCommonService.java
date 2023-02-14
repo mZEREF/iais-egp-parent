77d03b210e3496273d7577fd89a03246f2f16fb1
shu <shufei>
2023-02-14 16:19:08 +0800

    batch upload common service

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/ArBatchUploadCommonService.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/ArBatchUploadCommonService.java
@@ -1,8 +1,12 @@
 package com.ecquaria.cloud.moh.iais.service.datasubmission;
 
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.PatientInfoDto;
+import com.ecquaria.cloud.moh.iais.dto.PageShowFileDto;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ArBatchUploadCommonService
@@ -12,4 +16,10 @@ import javax.servlet.http.HttpServletRequest;
  **/
 public interface ArBatchUploadCommonService {
     PatientInfoDto setPatientInfo(String idType, String idNo, HttpServletRequest request);
+
+    Map.Entry<String, File> getFileEntry(HttpServletRequest request);
+
+    PageShowFileDto getPageShowFileDto(Map.Entry<String, File> fileEntry);
+
+    <T> List<T> getExcelDtoList(Map.Entry<String, File> fileEntry, Class<T> tClass);
 }
--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/impl/ArBatchUploadCommonServiceImpl.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/impl/ArBatchUploadCommonServiceImpl.java
@@ -1,16 +1,28 @@
 package com.ecquaria.cloud.moh.iais.service.datasubmission.impl;
 
+import com.ecquaria.cloud.moh.iais.action.HcsaFileAjaxController;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.PatientInfoDto;
+import com.ecquaria.cloud.moh.iais.common.utils.IaisCommonUtils;
+import com.ecquaria.cloud.moh.iais.common.utils.ParamUtil;
+import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.common.validation.SgNoValidator;
 import com.ecquaria.cloud.moh.iais.dto.LoginContext;
+import com.ecquaria.cloud.moh.iais.dto.PageShowFileDto;
 import com.ecquaria.cloud.moh.iais.helper.DataSubmissionHelper;
+import com.ecquaria.cloud.moh.iais.helper.FileUtils;
 import com.ecquaria.cloud.moh.iais.service.datasubmission.ArBatchUploadCommonService;
+import com.ecquaria.cloud.moh.iais.service.datasubmission.ArDataSubmissionService;
 import com.ecquaria.cloud.moh.iais.service.datasubmission.PatientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ArBatchUploadCommonServiceImpl
@@ -21,8 +33,15 @@ import javax.servlet.http.HttpServletRequest;
 @Service
 @Slf4j
 public class ArBatchUploadCommonServiceImpl implements ArBatchUploadCommonService {
+
+    private static final String FILE_APPEND = "uploadFile";
+    private static final String SEESION_FILES_MAP_AJAX = HcsaFileAjaxController.SEESION_FILES_MAP_AJAX + FILE_APPEND;
+
     @Autowired
     private PatientService patientService;
+
+    @Autowired
+    private ArDataSubmissionService arDataSubmissionService;
 
     @Override
     public PatientInfoDto setPatientInfo(String idType, String idNo, HttpServletRequest request) {
@@ -51,5 +70,64 @@ public class ArBatchUploadCommonServiceImpl implements ArBatchUploadCommonServic
             patientInfoDto = patientService.getPatientInfoDtoByIdTypeAndIdNumber(idType,idNo, orgId);
         }
         return patientInfoDto;
+    }
+
+    @Override
+    public Map.Entry<String, File> getFileEntry(HttpServletRequest request) {
+        Map<String, File> fileMap = (Map<String, File>) ParamUtil.getSessionAttr(request, SEESION_FILES_MAP_AJAX);
+        if (fileMap == null || fileMap.isEmpty()) {
+            return null;
+        }
+        // only one
+        Iterator<Map.Entry<String, File>> iterator = fileMap.entrySet().iterator();
+        if (!iterator.hasNext()) {
+            return null;
+        }
+        Map.Entry<String, File> next = iterator.next();
+        File file = next.getValue();
+        long length = file.length();
+        if (length == 0) {
+            return null;
+        }
+        return next;
+    }
+
+    @Override
+    public PageShowFileDto getPageShowFileDto(Map.Entry<String, File> fileEntry) {
+        if (fileEntry == null) {
+            return null;
+        }
+        File file = fileEntry.getValue();
+        PageShowFileDto pageShowFileDto = new PageShowFileDto();
+        String index = fileEntry.getKey().substring(FILE_APPEND.length());
+        String fileMd5 = FileUtils.getFileMd5(file);
+        pageShowFileDto.setIndex(index);
+        pageShowFileDto.setFileName(file.getName());
+        pageShowFileDto.setFileMapId(FILE_APPEND + "Div" + index);
+        pageShowFileDto.setSize((int) (file.length() / 1024));
+        pageShowFileDto.setMd5Code(fileMd5);
+        List<String> list = arDataSubmissionService.saveFileRepo(Collections.singletonList(file));
+        if (!list.isEmpty()) {
+            pageShowFileDto.setFileUploadUrl(list.get(0));
+        }
+        return pageShowFileDto;
+    }
+
+    @Override
+    public <T> List<T> getExcelDtoList(Map.Entry<String, File> fileEntry, Class<T> tClass) {
+        if (fileEntry == null) {
+            return IaisCommonUtils.genNewArrayList(0);
+        }
+        try {
+            File file = fileEntry.getValue();
+            if (FileUtils.isExcel(file.getName())) {
+                return FileUtils.transformToJavaBean(fileEntry.getValue(), tClass, true);
+            } else if (FileUtils.isCsv(file.getName())) {
+                return FileUtils.transformCsvToJavaBean(fileEntry.getValue(), tClass, true);
+            }
+        } catch (Exception e) {
+            log.error(StringUtil.changeForLog(e.getMessage()), e);
+        }
+        return IaisCommonUtils.genNewArrayList(0);
     }
 }
