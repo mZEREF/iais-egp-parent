da3f79bd0f79c76d9f6056f57c3fa28538d9763f
Jinhua <jinhua@ecquaria.com>
2023-02-22 08:51:22 +0800

    batch upload sfo and transfer service end
    # Conflicts:
    #	hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/service/datasubmission/impl/TransferInOutCycleUploadServiceImpl.java

--- hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/PatientDelegator.java
+++ hcsa-licence-fe/src/main/java/com/ecquaria/cloud/moh/iais/action/datasubmission/PatientDelegator.java
@@ -3,7 +3,7 @@ package com.ecquaria.cloud.moh.iais.action.datasubmission;
 import com.ecquaria.cloud.annotation.Delegator;
 import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.dataSubmission.DataSubmissionConsts;
-import com.ecquaria.cloud.moh.iais.common.constant.intranetUser.IntranetUserConstant;
+import com.ecquaria.cloud.moh.iais.common.constant.intranet.user.IntranetUserConstant;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.ArSuperDataSubmissionDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.HusbandDto;
 import com.ecquaria.cloud.moh.iais.common.dto.hcsa.dataSubmission.PatientDto;
