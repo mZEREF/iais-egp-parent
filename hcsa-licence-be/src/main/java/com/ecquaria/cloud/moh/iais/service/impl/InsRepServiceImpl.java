7999d91bd8e164560e77e6d25fdbdd354bac5c7b
junyu <junyu@ecquaria.com>
2023-03-06 16:26:31 +0800

    88384

--- hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InsRepServiceImpl.java
+++ hcsa-licence-be/src/main/java/com/ecquaria/cloud/moh/iais/service/impl/InsRepServiceImpl.java
@@ -754,9 +754,6 @@ public class InsRepServiceImpl implements InsRepService {
             createAppPremisesRoutingHistory(applicationNo, status, taskKey, appPremisesRecommendationDto.getProcessRemarks(), InspectionConstants.INSPECTION_STATUS_PROCESSING_DECISION_REPLY, RoleConsts.USER_ROLE_INSPECTIOR, groupId1, subStage);
         }
         ApplicationDto updateApplicationDto = updateApplicaitonStatus(applicationDto, ApplicationConsts.APPLICATION_STATUS_PENDING_INSPECTION_REPORT_REVIEW);
-        if(taskDtos.get(0).getUserId()==null){
-            updateApplicationDto.setStatus(ApplicationConsts.APPLICATION_STATUS_PENDING_TASK_ASSIGNMENT);
-        }
         createAppPremisesRoutingHistory(applicationNo, updateApplicationDto.getStatus(), taskKey, null, null, RoleConsts.USER_ROLE_INSPECTIOR, groupId1, subStage);
     }
 
@@ -791,6 +788,7 @@ public class InsRepServiceImpl implements InsRepService {
             taskDtos.forEach(t -> {
                 t.setUserId(aoId);
                 t.setDateAssigned(new Date());
+                appPremisesRoutingHistoryDtos.get(0).setAppStatus(applicationDto.getStatus());
             });
         }
         createHistoryList(appPremisesRoutingHistoryDtos);
