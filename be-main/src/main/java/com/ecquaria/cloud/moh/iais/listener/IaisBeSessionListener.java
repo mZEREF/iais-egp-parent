2fb2a6742e9274b9c4512ca9bf8b0a4a7cf1b08a
Jinhua <jinhua@ecquaria.com>
2023-03-07 10:46:10 +0800

    For session count

--- be-main/src/main/java/com/ecquaria/cloud/moh/iais/listener/IaisBeSessionListener.java
+++ be-main/src/main/java/com/ecquaria/cloud/moh/iais/listener/IaisBeSessionListener.java
@@ -2,11 +2,15 @@ package com.ecquaria.cloud.moh.iais.listener;
 
 import com.ecquaria.cloud.moh.iais.common.constant.AppConsts;
 import com.ecquaria.cloud.moh.iais.common.constant.AuditTrailConsts;
+import com.ecquaria.cloud.moh.iais.common.constant.RedisNameSpaceConstant;
 import com.ecquaria.cloud.moh.iais.common.dto.AuditTrailDto;
+import com.ecquaria.cloud.moh.iais.common.helper.RedisCacheHelper;
 import com.ecquaria.cloud.moh.iais.common.utils.StringUtil;
 import com.ecquaria.cloud.moh.iais.dto.LoginContext;
 import com.ecquaria.cloud.moh.iais.helper.AuditTrailHelper;
 import com.ecquaria.cloud.moh.iais.service.client.AuditTrailMainBeClient;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -14,6 +18,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.session.events.SessionCreatedEvent;
 import org.springframework.session.events.SessionDeletedEvent;
 import org.springframework.session.events.SessionExpiredEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * IaisBeSessionListener
@@ -21,15 +26,26 @@ import org.springframework.session.events.SessionExpiredEvent;
  * @author Jinhua
  * @date 2020/9/29 14:50
  */
+@Component
 @Slf4j
 public class IaisBeSessionListener {
     @Autowired
     private AuditTrailMainBeClient auditTrailMainClient;
 
+    @Autowired
+    private RedisCacheHelper redisHelper;
+
     @EventListener(SessionCreatedEvent.class)
     @Async
     public void sessionCreatedEvent(SessionCreatedEvent sessionEvent) {
-        log.info("SessionCreatedEvent : -> {}", StringUtil.isEmpty(sessionEvent));
+        String sessionId = sessionEvent.getSessionId();
+        log.info("SessionCreatedEvent : -> {}", sessionId);
+        Set<String> set = redisHelper.get(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet");
+        if (set == null) {
+            set =  new CopyOnWriteArraySet<>();
+        }
+        set.add(sessionId);
+        redisHelper.set(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet", set);
     }
 
     @EventListener(SessionExpiredEvent.class)
@@ -42,10 +58,20 @@ public class IaisBeSessionListener {
                 AuditTrailHelper.callSaveSessionDuration(sessionEvent.getSession().getId(), 30);
             }
         }
+        Set<String> set = redisHelper.get(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet");
+        if (set != null) {
+            set.remove(sessionEvent.getSessionId());
+            redisHelper.set(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet", set);
+        }
     }
 
     @EventListener(SessionDeletedEvent.class)
     public void sessionDeletedEvent(SessionDeletedEvent sessionEvent) {
         log.info("SessionCreatedEvent : -> {}", StringUtil.isEmpty(sessionEvent));
+        Set<String> set = redisHelper.get(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet");
+        if (set != null) {
+            set.remove(sessionEvent.getSessionId());
+            redisHelper.set(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, "activeSessionSet", set);
+        }
     }
 }
