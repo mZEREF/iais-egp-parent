4c3a3494e85b576cb9637b7c2b946a7e17a89adc
Jinhua <jinhua@ecquaria.com>
2023-03-10 16:34:52 +0800

    For active session count

--- backbone/src/main/java/com/ecquaria/cloud/moh/iais/filter/ActiveSessionFilter.java
+++ backbone/src/main/java/com/ecquaria/cloud/moh/iais/filter/ActiveSessionFilter.java
@@ -4,8 +4,9 @@ import com.ecquaria.cloud.helper.SpringContextHelper;
 import com.ecquaria.cloud.moh.iais.common.config.SystemParamConfig;
 import com.ecquaria.cloud.moh.iais.common.constant.RedisNameSpaceConstant;
 import com.ecquaria.cloud.moh.iais.common.helper.RedisCacheHelper;
+import com.ecquaria.cloud.moh.iais.helper.AccessUtil;
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.Date;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -36,14 +37,18 @@ public class ActiveSessionFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (servletRequest instanceof HttpServletRequest) {
             HttpServletRequest request = (HttpServletRequest) servletRequest;
-            Enumeration<String> headerNames = request.getHeaderNames();
+            String headerAgent = request.getHeader("User-Agent");
+            String sessionId = request.getSession().getId();
+            log.info("The request user-agent => {} for session Id => {}", headerAgent, sessionId);
             // Filter session count
             RedisCacheHelper redisHelper = SpringContextHelper.getContext().getBean(RedisCacheHelper.class);
             SystemParamConfig systemParamConfig = SpringContextHelper.getContext().getBean(SystemParamConfig.class);
-//            Object value = redisHelper.get(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, request.getSession().getId());
-            int asCount = redisHelper.keyNumbers(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET);
-            if (asCount > systemParamConfig.getMostActiveSessions()) {
+            if (!headerAgent.startsWith("curl")) {
+                int asCount = redisHelper.keyNumbers(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET);
+                if (asCount > systemParamConfig.getMostActiveSessions()) {
 //                IaisEGPHelper.redirectUrl((HttpServletResponse) response, homeUrl + "/403-error.jsp");
+                }
+                redisHelper.set(RedisNameSpaceConstant.CACHE_NAME_ACTIVE_SESSION_SET, sessionId, new Date());
             }
         }
         chain.doFilter(servletRequest, response);
